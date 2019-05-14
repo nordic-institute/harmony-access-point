90c689b059ad51c45f40dc53b6e27b390e809e9a
ion.perpegel <ion.perpegel@gmail.com>
2019-05-14 10:01:59 +0300

    [EDELIVERY-4626] fix client validation of duplicates; add back-end validation of duplicates; refactor the code-

--- Domibus-MSH/src/main/java/eu/domibus/plugin/routing/RoutingService.java
+++ Domibus-MSH/src/main/java/eu/domibus/plugin/routing/RoutingService.java
@@ -1,6 +1,8 @@
 package eu.domibus.plugin.routing;
 
 import eu.domibus.api.routing.BackendFilter;
+import eu.domibus.api.routing.RoutingCriteria;
+import eu.domibus.common.exception.ConfigurationException;
 import eu.domibus.core.converter.DomainCoreConverter;
 import eu.domibus.plugin.NotificationListener;
 import eu.domibus.plugin.routing.dao.BackendFilterDao;
@@ -62,11 +64,36 @@ public class RoutingService {
     @CacheEvict(value = "backendFilterCache", allEntries = true)
     @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_AP_ADMIN')")
     public void updateBackendFilters(final List<BackendFilter> filters) {
+
+        validateFilters(filters);
+
         List<BackendFilterEntity> backendFilterEntities = coreConverter.convert(filters, BackendFilterEntity.class);
         List<BackendFilterEntity> allBackendFilterEntities = backendFilterDao.findAll();
         List<BackendFilterEntity> backendFilterEntityListToDelete = backendFiltersToDelete(allBackendFilterEntities, backendFilterEntities);
         backendFilterDao.deleteAll(backendFilterEntityListToDelete);
         backendFilterDao.update(backendFilterEntities);
+    }
+
+    private void validateFilters(List<BackendFilter> filters) {
+        filters.forEach(filter -> {
+            if (filters.stream().anyMatch(f -> f != filter
+                    && f.getBackendName().equals(filter.getBackendName())
+                    && areEqual(f.getRoutingCriterias(), filter.getRoutingCriterias()))) {
+                throw new ConfigurationException("Two message filters cannot have the same criteria.");
+            }
+        });
+    }
+
+    private boolean areEqual(List<RoutingCriteria> c1, List<RoutingCriteria> c2) {
+        if (c1.size() != c2.size()) {
+            return false;
+        }
+        for (RoutingCriteria cr1 : c1) {
+            if (!c2.stream().anyMatch(cr2 -> cr2.getName().equals(cr1.getName()) && cr2.getExpression().equals(cr1.getExpression()))) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private List<BackendFilterEntity> backendFiltersToDelete(final List<BackendFilterEntity> masterData, final List<BackendFilterEntity> newData) {
