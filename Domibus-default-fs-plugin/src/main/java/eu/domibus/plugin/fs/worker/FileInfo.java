60096ba040544d409ca9c6524ff89bd13562e3e6
ion.perpegel <ion.perpegel@gmail.com>
2019-05-22 11:42:33 +0200

    [EDELIVERY-4553] fix sonar issues

--- Domibus-default-fs-plugin/src/main/java/eu/domibus/plugin/fs/worker/FileInfo.java
+++ Domibus-default-fs-plugin/src/main/java/eu/domibus/plugin/fs/worker/FileInfo.java
@@ -1,13 +1,25 @@
 package eu.domibus.plugin.fs.worker;
 
 public class FileInfo {
-    public long size;
-    public long modified;
-    public String domain;
+    private long size;
+    private long modified;
+    private String domain;
 
     public FileInfo(long size, long modified, String domain) {
         this.size = size;
         this.modified = modified;
         this.domain = domain;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public long getModified() {
+        return modified;
+    }
+
+    public String getDomain() {
+        return domain;
     }
 }
