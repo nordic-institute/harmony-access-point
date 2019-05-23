c5f695ca927a66144d7712311550c2f889978be5
ion.perpegel <ion.perpegel@gmail.com>
2019-05-23 13:31:04 +0200

    [EDELIVERY-4405] implement pull request comments

--- Domibus-MSH-angular/src/app/common/row-limiter/row-limiter.component.ts
+++ Domibus-MSH-angular/src/app/common/row-limiter/row-limiter.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-row-limiter',
