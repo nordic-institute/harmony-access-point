54303449f9324ed9f740bdfcaf438e6d6a7f5f4b
ion.perpegel <ion.perpegel@gmail.com>
2019-05-30 10:32:22 +0300

    [EDELIVERY-4507] fix tests

--- Domibus-MSH/src/test/java/eu/domibus/web/rest/LoggingResourceTest.java
+++ Domibus-MSH/src/test/java/eu/domibus/web/rest/LoggingResourceTest.java
@@ -5,6 +5,8 @@ import eu.domibus.core.logging.LoggingEntry;
 import eu.domibus.core.logging.LoggingException;
 import eu.domibus.core.logging.LoggingService;
 import eu.domibus.ext.rest.ErrorRO;
+import eu.domibus.web.rest.error.ErrorHandlerService;
+import eu.domibus.web.rest.ro.LoggingFilterRequestRO;
 import eu.domibus.web.rest.ro.LoggingLevelRO;
 import eu.domibus.web.rest.ro.LoggingLevelResultRO;
 import mockit.*;
@@ -12,6 +14,7 @@ import mockit.integration.junit4.JMockit;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
@@ -34,6 +37,9 @@ public class LoggingResourceTest {
 
     @Injectable
     private LoggingService loggingService;
+
+    @Injectable
+    private ErrorHandlerService errorHandlerService;
 
     @Test
     public void testSetLogLevel(final @Mocked LoggingLevelRO loggingLevelRO) {
@@ -61,43 +67,50 @@ public class LoggingResourceTest {
         }};
     }
 
-//    @Test
-//    public void testGetLogLevel(final @Mocked List<LoggingEntry> loggingEntryList) {
-//        final String name = "eu.domibus";
-//        final boolean showClasses = false;
-//
-//        final List<LoggingLevelRO> loggingLevelROList = new ArrayList<>();
-//        LoggingLevelRO loggingLevelRO1 = new LoggingLevelRO();
-//        loggingLevelRO1.setLevel("INFO");
-//        loggingLevelRO1.setName("eu.domibus");
-//        loggingLevelROList.add(loggingLevelRO1);
-//        LoggingLevelRO loggingLevelRO2 = new LoggingLevelRO();
-//        loggingLevelRO2.setLevel("INFO");
-//        loggingLevelRO2.setName("eu.domibus.common");
-//        loggingLevelROList.add(loggingLevelRO2);
-//        LoggingLevelRO loggingLevelRO3 = new LoggingLevelRO();
-//        loggingLevelRO3.setLevel("INFO");
-//        loggingLevelRO3.setName("eu.domibus.common.model");
-//        loggingLevelROList.add(loggingLevelRO3);
-//
-//        new Expectations(loggingResource) {{
-//            loggingService.getLoggingLevel(name, showClasses);
-//            result = loggingEntryList;
-//
-//            domainConverter.convert(loggingEntryList, LoggingLevelRO.class);
-//            result = loggingLevelROList;
-//
-//        }};
-//
-//        //tested method
+    @Test
+    public void testGetLogLevel(final @Mocked List<LoggingEntry> loggingEntryList) {
+        final String name = "eu.domibus";
+        final boolean showClasses = false;
+
+        final List<LoggingLevelRO> loggingLevelROList = new ArrayList<>();
+        LoggingLevelRO loggingLevelRO1 = new LoggingLevelRO();
+        loggingLevelRO1.setLevel("INFO");
+        loggingLevelRO1.setName("eu.domibus");
+        loggingLevelROList.add(loggingLevelRO1);
+        LoggingLevelRO loggingLevelRO2 = new LoggingLevelRO();
+        loggingLevelRO2.setLevel("INFO");
+        loggingLevelRO2.setName("eu.domibus.common");
+        loggingLevelROList.add(loggingLevelRO2);
+        LoggingLevelRO loggingLevelRO3 = new LoggingLevelRO();
+        loggingLevelRO3.setLevel("INFO");
+        loggingLevelRO3.setName("eu.domibus.common.model");
+        loggingLevelROList.add(loggingLevelRO3);
+
+        new Expectations(loggingResource) {{
+            loggingService.getLoggingLevel(name, showClasses);
+            result = loggingEntryList;
+
+            domainConverter.convert(loggingEntryList, LoggingLevelRO.class);
+            result = loggingLevelROList;
+
+        }};
+
+        //tested method
 //        final ResponseEntity<LoggingLevelResultRO>  result =
 //                loggingResource.getLogLevel(name, showClasses, 0, 2, null, false);
-//
-//        Assert.assertNotNull(result.getBody().getLoggingEntries());
-//        List<LoggingLevelRO> loggingEntries = result.getBody().getLoggingEntries();
-//        Assert.assertTrue(!loggingEntries.isEmpty());
-//        Assert.assertEquals(2, loggingEntries.size());
-//    }
+        final ResponseEntity<LoggingLevelResultRO>  result =
+                loggingResource.getLogLevel(new LoggingFilterRequestRO(){{
+                    setLoggerName(name);
+                    setShowClasses(showClasses);
+                    setPageSize(2);
+                    setAsc(false);
+                }}, null);
+
+        Assert.assertNotNull(result.getBody().getLoggingEntries());
+        List<LoggingLevelRO> loggingEntries = result.getBody().getLoggingEntries();
+        Assert.assertTrue(!loggingEntries.isEmpty());
+        Assert.assertEquals(2, loggingEntries.size());
+    }
 
     @Test
     public void testResetLogging() {
