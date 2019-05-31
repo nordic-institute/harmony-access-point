57126f2a43b432c62524cfba2f47f410e370c671
unknown <grzegorz.kopec.ext.ec@gmail.com>
2019-05-31 15:33:06 +0100

    curl -v added

--- Domibus-MSH-soapui-tests/src/main/soapui/scripting/Domibus.groovy
+++ Domibus-MSH-soapui-tests/src/main/soapui/scripting/Domibus.groovy
@@ -1543,7 +1543,10 @@ def findNumberOfDomain(String inputSite) {
         def commandString = null;
         def commandResult = null;
 
-        commandString = "curl " + urlToDomibus(side, log, context) + "/rest/security/authentication -i -H 'Content-Type: application/json' -X POST -d '{\"username\":\"$userLogin\",\"password\":\"$passwordLogin\"}' -c " + context.expand('${projectDir}') + "\\cookie.txt";
+        commandString = "curl " + urlToDomibus(side, log, context) + 
+		"/rest/security/authentication -v -i -H 'Content-Type:application/json' -X POST -d '{\"username\":\"$userLogin\",\"password\":\"$passwordLogin\"}' -c " + 
+		context.expand('${projectDir}') + File.separator + cookie.txt";
+		
         commandResult = runCurlCommand(commandString, log)
         assert(commandResult[0].contains("XSRF-TOKEN")),"Error:Authenticating user: Error while trying to connect to domibus."
         return commandResult[0];
