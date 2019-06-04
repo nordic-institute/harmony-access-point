d36777317e3f6aa4f06865e1c7fc1c95cf3a596a
unknown <grzegorz.kopec.ext.ec@gmail.com>
2019-06-04 15:23:23 +0100

    d

--- Domibus-MSH-soapui-tests/src/main/soapui/scripting/Domibus.groovy
+++ Domibus-MSH-soapui-tests/src/main/soapui/scripting/Domibus.groovy
@@ -1542,7 +1542,7 @@ def findNumberOfDomain(String inputSite) {
         debugLog("  ====  Calling \"fetchCookieHeader\".", log)
         def commandString = null;
         def commandResult = null;
-		def json = '{\"username\":\"' + "${userLogin}" + '"==\",\"password\":\"' + "${passwordLogin}" + '\"}'
+		def json = '{\"username\":\"' + "${userLogin}" + '\",\"password\":\"' + "${passwordLogin}" + '\"}'
 	
         commandString = ["curl", "-v", "-i", "-H",  "Content-Type:application/json", 
 						"--data-binary", json, "-c", context.expand('${projectDir}') + File.separator + "cookie.txt", 
