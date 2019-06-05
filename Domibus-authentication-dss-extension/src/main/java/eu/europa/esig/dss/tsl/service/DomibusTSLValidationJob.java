219781959afe0fc91dcbb324b0fa4217021f7296
dussath <thomas.dussart@ext.ce.europa.eu>
2019-06-05 09:46:41 +0200

    EDELIVERY-4518: - Code review

--- Domibus-authentication-dss-extension/src/main/java/eu/europa/esig/dss/tsl/service/DomibusTSLValidationJob.java
+++ Domibus-authentication-dss-extension/src/main/java/eu/europa/esig/dss/tsl/service/DomibusTSLValidationJob.java
@@ -55,7 +55,7 @@ import eu.europa.esig.dss.x509.KeyStoreCertificateSource;
  */
 public class DomibusTSLValidationJob {
 
-    private static final Logger LOG = LoggerFactory.getLogger(TSLValidationJob.class);
+    private static final Logger LOG = LoggerFactory.getLogger(DomibusTSLValidationJob.class);
 
     private ExecutorService executorService = Executors.newCachedThreadPool();
 
