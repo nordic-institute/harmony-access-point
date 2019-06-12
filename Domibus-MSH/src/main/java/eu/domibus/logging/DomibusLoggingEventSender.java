4f9fed4bb43a87a718163ad8ea3ff452b43133f7
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2019-06-12 17:25:11 +0200

    [EDELIVER-4459] added custom LogEventSender implementation cxf-endpoint.xml cleanup updated domibus.properties files

--- Domibus-MSH/src/main/java/eu/domibus/logging/DomibusLoggingEventSender.java
+++ Domibus-MSH/src/main/java/eu/domibus/logging/DomibusLoggingEventSender.java
@@ -1,6 +1,5 @@
 package eu.domibus.logging;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.cxf.ext.logging.event.LogEvent;
 import org.apache.cxf.ext.logging.event.LogEventSender;
 import org.apache.cxf.ext.logging.event.LogMessageFormatter;
@@ -8,27 +7,41 @@ import org.apache.cxf.ext.logging.slf4j.Slf4jEventSender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DomibusLoggingEventSender extends Slf4jEventSender implements LogEventSender  {
+/**
+ * This class extends the default {@code Slf4jEventSender} implemented by Apache CXF
+ * <p>
+ * It will implement operations based on {@code LogEvent} like partially printing the payload, etc
+ *
+ * @author Catalin Enache
+ * @since 4.1.1
+ */
+public class DomibusLoggingEventSender extends Slf4jEventSender implements LogEventSender {
 
     private static final Logger LOG = LoggerFactory.getLogger(DomibusLoggingEventSender.class);
 
-    private boolean printPayload;
 
-    public boolean isPrintPayload() {
-        return printPayload;
-    }
+    private static final String CONTENT_TYPE = "Content-Type:";
+
+    private boolean printPayload;
 
     public void setPrintPayload(boolean printPayload) {
         this.printPayload = printPayload;
     }
 
-
     @Override
     protected String getLogMessage(LogEvent event) {
-        String payload = event.getPayload();
-        LOG.info("printPayload={}", printPayload);
+        LOG.debug("printPayload={}", printPayload);
 
-        return printPayload ? LogMessageFormatter.format(event) : StringUtils.EMPTY;
+        //if it's multipart and we want to strip the AS4 payload
+        if (event.isMultipartContent() && !printPayload) {
+            final String payload = event.getPayload();
+            String[] payloadSplits = payload.split(CONTENT_TYPE);
+            //keeping only first 2 Content-Type elements
+            if (payloadSplits.length >= 2) {
+                event.setPayload(payloadSplits[0] + CONTENT_TYPE + payloadSplits[1]);
+            }
+        }
+        return LogMessageFormatter.format(event);
     }
 
 }
