4e147ea20226007ecb74a4c5c3e38e53d7f7f503
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2019-06-13 17:06:51 +0200

    [EDELIVER-4459] added Retrieve Message strip payload

--- Domibus-MSH/src/main/java/eu/domibus/logging/DomibusLoggingEventSender.java
+++ Domibus-MSH/src/main/java/eu/domibus/logging/DomibusLoggingEventSender.java
@@ -1,5 +1,6 @@
 package eu.domibus.logging;
 
+import org.apache.cxf.ext.logging.AbstractLoggingInterceptor;
 import org.apache.cxf.ext.logging.event.LogEvent;
 import org.apache.cxf.ext.logging.event.LogEventSender;
 import org.apache.cxf.ext.logging.event.LogMessageFormatter;
@@ -21,6 +22,9 @@ public class DomibusLoggingEventSender extends Slf4jEventSender implements LogEv
 
 
     static final String CONTENT_TYPE = "Content-Type:";
+    static final String RETRIEVE_MESSAGE_RESPONSE = "retrieveMessageResponse";
+    static final String VALUE_START = "<value>";
+    static final String VALUE_END = "</value>";
 
     private boolean printPayload;
 
@@ -32,16 +36,40 @@ public class DomibusLoggingEventSender extends Slf4jEventSender implements LogEv
     protected String getLogMessage(LogEvent event) {
         LOG.debug("printPayload={}", printPayload);
 
-        //if it's multipart and we want to strip the AS4 payload
-        if (event.isMultipartContent() && !printPayload) {
-            final String payload = event.getPayload();
-            String[] payloadSplits = payload.split(CONTENT_TYPE);
-            //keeping only first 2 Content-Type elements
-            if (payloadSplits.length >= 2) {
-                event.setPayload(payloadSplits[0] + CONTENT_TYPE + payloadSplits[1]);
-            }
+        if (!printPayload) {
+            stripPayload(event);
         }
+
+
         return LogMessageFormatter.format(event);
+    }
+
+    /**
+     * It removes some parts of the payload info
+     *
+     * @param event
+     */
+    private void stripPayload(LogEvent event) {
+        final String payload = event.getPayload();
+
+        //C2 -> C3
+        if (payload.contains(CONTENT_TYPE)) {
+            //if it's multipart and we want to strip the AS4 payload
+            if (event.isMultipartContent()) {
+
+                String[] payloadSplits = payload.split(CONTENT_TYPE);
+                //keeping only first 2 Content-Type elements
+                if (payloadSplits.length >= 2) {
+                    event.setPayload(payloadSplits[0] + CONTENT_TYPE + payloadSplits[1] + AbstractLoggingInterceptor.CONTENT_SUPPRESSED);
+                }
+            }
+
+        } else if (payload.contains(RETRIEVE_MESSAGE_RESPONSE)) {
+            //C4 - C3
+            int indexStart = payload.indexOf(VALUE_START);
+            int indexEnd = payload.indexOf(VALUE_END);
+            event.setPayload(payload.replace(payload.substring(indexStart + 7, indexEnd), AbstractLoggingInterceptor.CONTENT_SUPPRESSED));
+        }
     }
 
 }
