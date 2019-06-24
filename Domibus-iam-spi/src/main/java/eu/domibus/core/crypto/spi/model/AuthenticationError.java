9cb16b5f49b8f2752a496d7e1fe98ac7d942b15b
Thomas DUSSART <Thomas.DUSSART@ext.ec.europa.eu>
2019-06-24 11:58:52 +0200

    EDELIVERY-4766: own review

--- Domibus-iam-spi/src/main/java/eu/domibus/core/crypto/spi/model/AuthenticationError.java
+++ Domibus-iam-spi/src/main/java/eu/domibus/core/crypto/spi/model/AuthenticationError.java
@@ -1,5 +1,11 @@
 package eu.domibus.core.crypto.spi.model;
 
+/**
+ * @author Thomas Dussart
+ * @since 4.1
+ * <p>
+ * Possible returned code from authentication module.
+ */
 public enum AuthenticationError {
     UNKNOWN,
     EBMS_0101
