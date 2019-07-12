242909a7ee0117244a9d16514c0d8275d6500104
ion.perpegel <ion.perpegel@gmail.com>
2019-07-12 14:59:27 +0300

    [EDELIVERY-4813] add a mapping between cron scheduled job and his cron expresiion property

--- Domibus-MSH/src/main/java/eu/domibus/core/property/listeners/CronExpressionChangeListener.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/property/listeners/CronExpressionChangeListener.java
@@ -2,24 +2,23 @@ package eu.domibus.core.property.listeners;
 
 import eu.domibus.api.multitenancy.Domain;
 import eu.domibus.api.multitenancy.DomainService;
-import eu.domibus.core.property.DomibusPropertyProviderImpl;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
-import eu.domibus.messaging.MessageListenerContainerInitializer;
 import eu.domibus.property.DomibusPropertyChangeListener;
-import eu.domibus.quartz.DomainSchedulerFactoryConfiguration;
 import eu.domibus.quartz.DomibusQuartzStarter;
 import org.quartz.SchedulerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Ion Perpegel
  * @since 4.1.1
- *
+ * <p>
  * Handles the change of cron expression properties
  */
 @Service
@@ -33,44 +32,48 @@ public class CronExpressionChangeListener implements DomibusPropertyChangeListen
     @Autowired
     protected ApplicationContext applicationContext;
 
-    private String[] handledProperties = new String[]{
-            "domibus.account.unlock.cron",
-            "domibus.certificate.check.cron",
-            "domibus.plugin.account.unlock.cron",
-            "domibus.passwordPolicies.check.cron",
-            "domibus.plugin_passwordPolicies.check.cron",
-            "domibus.payload.temp.job.retention.cron",
-            "domibus.msh.retry.cron",
-            "domibus.retentionWorker.cronExpression",
-            "domibus.msh.pull.cron",
-            "domibus.pull.retry.cron",
-            "domibus.alert.cleaner.cron",
-            "domibus.alert.retry.cron",
-            //"domibus.alert.super.cleaner.cron",
-            //"domibus.alert.super.retry.cron",
-            "domibus.ui.replication.sync.cron",
-            "domibus.splitAndJoin.receive.expiration.cron",
-    };
+    Map<String, String> propertyToJobMap = Stream.of(new String[][]{
+            {"domibus.account.unlock.cron", "activateSuspendedUserTrigger"}, //todo: handle also the super trigger
+            {"domibus.certificate.check.cron", "activateSuspendedUserTrigger"}, //todo: handle also the super trigger
+            {"domibus.plugin.account.unlock.cron", "activateSuspendedPluginUserTrigger"},
+            {"domibus.passwordPolicies.check.cron", "userPasswordPolicyAlertTrigger"},
+            {"domibus.plugin_passwordPolicies.check.cron", "pluginUserPasswordPolicyAlertTrigger"},
+            {"domibus.payload.temp.job.retention.cron", "temporaryPayloadRetentionTrigger"},
+            {"domibus.msh.retry.cron", "retryWorkerTrigger"},
+            {"domibus.retentionWorker.cronExpression", "retentionWorkerTrigger"},
+            {"domibus.msh.pull.cron", "pullRequestTrigger"},
+            {"domibus.pull.retry.cron", "pullRetryWorkerTrigger"},
+            {"domibus.alert.cleaner.cron", "alertCleanerTrigger"},
+            {"domibus.alert.retry.cron", "alertRetryWorkerTrigger"},
+//            { "domibus.alert.super.cleaner.cron", "alertSuperCleanerTrigger" },
+//            { "domibus.alert.super.retry.cron", "alertRetrySuperWorkerTrigger" },
+            {"domibus.ui.replication.sync.cron", "uiReplicationTrigger"},
+            {"domibus.splitAndJoin.receive.expiration.cron", "splitAndJoinExpirationTrigger"},
+
+    }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
 
     @Override
     public boolean handlesProperty(String propertyName) {
-        return Arrays.stream(handledProperties).anyMatch(p -> p.equalsIgnoreCase(propertyName));
+        return propertyToJobMap.containsKey(propertyName);
     }
 
     @Override
     public void propertyValueChanged(String domainCode, String propertyName, String propertyValue) {
-        DomainSchedulerFactoryConfiguration domainSchedulerFactoryConfiguration = applicationContext.getBean(DomainSchedulerFactoryConfiguration.class);
-        DomibusQuartzStarter domibusQuartzStarter= applicationContext.getBean(DomibusQuartzStarter.class);
+        String jobName = propertyToJobMap.get(propertyName);
+        if (jobName == null) {
+            LOGGER.warn("Could not find the coresponding job for the property [{}]", propertyName);
+            return;
+        }
+
+//        DomainSchedulerFactoryConfiguration schedulerConf = applicationContext.getBean(DomainSchedulerFactoryConfiguration.class);
+        DomibusQuartzStarter domibusQuartzStarter = applicationContext.getBean(DomibusQuartzStarter.class);
         final Domain domain = domainService.getDomain(domainCode);
 
         try {
-            switch (propertyName) {
-                case "domibus.retentionWorker.cronExpression":
-                    domibusQuartzStarter.rescheduleJob(domain, "retentionWorkerJob", propertyValue);
-                    break;
-            }
-        }catch (SchedulerException ex) {
+            domibusQuartzStarter.rescheduleJob(domain, jobName, propertyValue);
+        } catch (SchedulerException ex) {
             LOGGER.error("Could not reschedule [{}] ", propertyName, ex);
         }
     }
+
 }
