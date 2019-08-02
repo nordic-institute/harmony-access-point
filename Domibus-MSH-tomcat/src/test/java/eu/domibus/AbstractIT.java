3eb996819188452ed7c85d9724d77d868c4e780a
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2019-08-02 09:39:35 +0200

    [EDELIVERY-4848] added a random value for activeMQ connectorPort and rmiServerPort

--- Domibus-MSH-tomcat/src/test/java/eu/domibus/AbstractIT.java
+++ Domibus-MSH-tomcat/src/test/java/eu/domibus/AbstractIT.java
@@ -44,6 +44,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.SocketUtils;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
@@ -103,6 +104,13 @@ public abstract class AbstractIT {
 
         FileUtils.deleteDirectory(new File("target/temp"));
         System.setProperty("domibus.config.location", new File("target/test-classes").getAbsolutePath());
+
+        int activeMQConnectorPort = SocketUtils.findAvailableTcpPort(2000, 2100);
+        int activeMQRmiServerPort = SocketUtils.findAvailableTcpPort(1200, 1300);
+        System.setProperty("activeMQ.connectorPort", String.valueOf(activeMQConnectorPort));
+        System.setProperty("activeMQ.rmiServerPort", String.valueOf(activeMQRmiServerPort));
+        LOG.info("activeMQ.connectorPort=[{}]", activeMQConnectorPort);
+        LOG.info("activeMQ.rmiServerPort=[{}]", activeMQRmiServerPort);
 
         SecurityContextHolder.getContext()
                 .setAuthentication(new UsernamePasswordAuthenticationToken(
