c8f57f17779adbc5c0459680d4f0344f29560653
Ioana DRAGUSANU <idragusa@gmail.com>
2019-08-02 09:44:35 +0300

    [EDELIVERY-4830] Use configured truststore to download CRL on https, in addition to cacerts.

--- Domibus-MSH/src/main/java/eu/domibus/core/util/HttpUtilImpl.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/util/HttpUtilImpl.java
@@ -1,7 +1,9 @@
 package eu.domibus.core.util;
 
+import eu.domibus.api.multitenancy.DomainContextProvider;
 import eu.domibus.api.util.HttpUtil;
 import eu.domibus.common.util.ProxyUtil;
+import eu.domibus.core.crypto.api.MultiDomainCryptoService;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
 import eu.domibus.proxy.DomibusProxyService;
@@ -11,14 +13,26 @@ import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 /**
  * Created by Cosmin Baciu on 12-Jul-16.
@@ -28,11 +42,20 @@ public class HttpUtilImpl implements HttpUtil {
 
     private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(HttpUtilImpl.class);
 
+    public static final String DEFAULT_SSL_PROTOCOL = "TLSv1.2";
+
     @Autowired
     DomibusProxyService domibusProxyService;
 
     @Autowired
     ProxyUtil proxyUtil;
+
+    @Autowired
+    protected MultiDomainCryptoService multiDomainCertificateProvider;
+
+    @Autowired
+    protected DomainContextProvider domainProvider;
+
 
     @Override
     public ByteArrayInputStream downloadURL(String url) throws IOException {
@@ -44,8 +67,11 @@ public class HttpUtilImpl implements HttpUtil {
 
     @Override
     public ByteArrayInputStream downloadURLDirect(String url) throws IOException {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
+        CloseableHttpClient httpclient = HttpClients.custom()
+                .setSSLSocketFactory(getSSLConnectionSocketFactory())
+                .build();
         HttpGet httpGet = new HttpGet(url);
+
 
         try {
             LOG.debug("Executing request " + httpGet.getRequestLine() + " directly");
@@ -59,11 +85,11 @@ public class HttpUtilImpl implements HttpUtil {
     public ByteArrayInputStream downloadURLViaProxy(String url) throws IOException {
         HttpClientBuilder httpClientBuilder = HttpClients.custom();
         CredentialsProvider credentialsProvider = proxyUtil.getConfiguredCredentialsProvider();
-        if(credentialsProvider != null) {
+        if (credentialsProvider != null) {
             httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
         }
 
-        try(CloseableHttpClient httpClient = httpClientBuilder.build()) {
+        try (CloseableHttpClient httpClient = httpClientBuilder.setSSLSocketFactory(getSSLConnectionSocketFactory()).build()) {
             HttpHost proxy = proxyUtil.getConfiguredProxy();
 
             RequestConfig config = RequestConfig.custom()
@@ -89,4 +115,76 @@ public class HttpUtilImpl implements HttpUtil {
         }
     }
 
+    protected SSLConnectionSocketFactory getSSLConnectionSocketFactory() {
+        SSLContext sslContext = null;
+        final X509TrustManager defaultTm = getX509TrustManager(true);
+        final X509TrustManager domibusTm = getX509TrustManager(false);
+
+        X509TrustManager customTm = new X509TrustManager() {
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return defaultTm.getAcceptedIssuers();
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] chain,
+                                           String authType) throws CertificateException {
+                try {
+                    domibusTm.checkServerTrusted(chain, authType);
+                } catch (CertificateException e) {
+                    // This will throw another CertificateException if this fails too.
+                    defaultTm.checkServerTrusted(chain, authType);
+                }
+            }
+
+            @Override
+            public void checkClientTrusted(X509Certificate[] chain,
+                                           String authType) throws CertificateException {
+                defaultTm.checkClientTrusted(chain, authType);
+            }
+        };
+        try {
+            sslContext = SSLContext.getInstance(DEFAULT_SSL_PROTOCOL);
+            sslContext.init(null, new TrustManager[]{customTm}, null);
+        } catch (NoSuchAlgorithmException | KeyManagementException exc) {
+            LOG.warn("Could not instanciate sslContext", exc);
+            return null;
+        }
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+                sslContext,
+                null,
+                null,
+                NoopHostnameVerifier.INSTANCE);
+
+        return sslsf;
+    }
+
+    protected X509TrustManager getX509TrustManager(boolean getDefault) {
+        KeyStore trustStore = null;
+        if (getDefault) {
+            LOG.debug("Get default certificates (cacerts)");
+        }
+        if (!getDefault) {
+            LOG.debug("Get custom certificates (truststore)");
+            trustStore = multiDomainCertificateProvider.getTrustStore(domainProvider.getCurrentDomain());
+        }
+        X509TrustManager trustManager = null;
+        try {
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            // Using null here initialises the TMF with the default trust store (cacerts).
+            tmf.init(trustStore);
+
+            for (TrustManager tm : tmf.getTrustManagers()) {
+                if (tm instanceof X509TrustManager) {
+                    trustManager = (X509TrustManager) tm;
+                    break;
+                }
+            }
+        } catch (NoSuchAlgorithmException | KeyStoreException exc) {
+            LOG.warn("Could not load trustManager for ssl exchange. ", exc);
+            return null;
+        }
+
+        return trustManager;
+    }
 }
--- Domibus-MSH/src/test/java/eu/domibus/core/util/HttpUtilImplTest.java
+++ Domibus-MSH/src/test/java/eu/domibus/core/util/HttpUtilImplTest.java
@@ -1,6 +1,9 @@
 package eu.domibus.core.util;
 
+import eu.domibus.api.multitenancy.DomainContextProvider;
+import eu.domibus.api.multitenancy.DomainService;
 import eu.domibus.common.util.ProxyUtil;
+import eu.domibus.core.crypto.api.MultiDomainCryptoService;
 import eu.domibus.proxy.DomibusProxyService;
 import mockit.Injectable;
 import mockit.NonStrictExpectations;
@@ -11,10 +14,15 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.junit.Assert;
 import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.KeyStore;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509CRL;
 
@@ -23,8 +31,12 @@ import java.security.cert.X509CRL;
  * @since 4.1
  */
 @RunWith(JMockit.class)
-@Ignore
 public class HttpUtilImplTest {
+
+    private static final String RESOURCE_PATH = "src/test/resources/eu/domibus/ebms3/receiver/";
+    private static final String TEST_TRUSTSTORE = "gateway_truststore.jks";
+    private static final String TEST_TRUSTSTORE_PASSWD = "test123";
+
     @Tested
     HttpUtilImpl httpUtil;
 
@@ -34,7 +46,14 @@ public class HttpUtilImplTest {
     @Injectable
     DomibusProxyService domibusProxyService;
 
-    //@Test
+    @Injectable
+    MultiDomainCryptoService multiDomainCertificateProvider;
+
+    @Injectable
+    DomainContextProvider domainProvider;
+
+    @Test
+    @Ignore
     public void testDownloadCRLViaProxy() throws Exception {
         new NonStrictExpectations(proxyUtil) {{
             domibusProxyService.useProxy();
@@ -66,5 +85,47 @@ public class HttpUtilImplTest {
                 new UsernamePasswordCredentials("baciuco", "pass"));
 
         return credsProvider;
+    }
+
+    @Test
+    public void testDownloadCRLHttps() throws Exception {
+        String url = "http://onsitecrl.verisign.com/offlineca/NATIONALITANDTELECOMAGENCYPEPPOLRootCA.crl";
+        /* Added this crl file to git so we have it on https and use it for testing */
+        String urlSSL = "https://ec.europa.eu/cefdigital/code/projects/EDELIVERY/repos/domibus/browse/Domibus-MSH-test/src/main/resources/crls/NATIONALITANDTELECOMAGENCYPEPPOLRootCA.crl?at=3c7eae7c00e7425593436a1f469ba269834ece90&raw";
+
+        /* Local SoapUI mock endpoints for http and https, EDELIVERY-4830 */
+        //String url = "https://localhost:8555/crltest";
+        //String urlSSL = "http://localhost:8089/crltest";
+
+        KeyStore trustStore = KeyStore.getInstance("JKS");
+        FileInputStream instream = new FileInputStream(new File(RESOURCE_PATH + TEST_TRUSTSTORE));
+        try {
+            trustStore.load(instream, TEST_TRUSTSTORE_PASSWD.toCharArray());
+        } finally {
+            instream.close();
+        }
+
+        new NonStrictExpectations(proxyUtil) {{
+            domibusProxyService.useProxy();
+            result = false;
+
+            domainProvider.getCurrentDomain();
+            result = DomainService.DEFAULT_DOMAIN;
+
+            multiDomainCertificateProvider.getTrustStore(DomainService.DEFAULT_DOMAIN);
+            result = trustStore;
+        }};
+
+        ByteArrayInputStream inputStream = httpUtil.downloadURL(url);
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        X509CRL x509CRL = (X509CRL) cf.generateCRL(inputStream);
+        System.out.println(x509CRL);
+
+        ByteArrayInputStream inputStreamSSL = httpUtil.downloadURLDirect(urlSSL);
+        CertificateFactory cfSSL = CertificateFactory.getInstance("X.509");
+        X509CRL x509CRLSSL = (X509CRL) cfSSL.generateCRL(inputStreamSSL);
+        System.out.println(x509CRLSSL);
+
+        Assert.assertTrue(x509CRLSSL.equals(x509CRL));
     }
 }
