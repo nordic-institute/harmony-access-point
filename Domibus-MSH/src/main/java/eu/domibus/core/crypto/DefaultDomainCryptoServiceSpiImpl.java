593881d88f15c54d6c3b09a243951c9a1a516067
Ioana DRAGUSANU <idragusa@gmail.com>
2019-08-09 10:19:46 +0300

    [EDELIVERY-4831] Backup truststore when new truststore is uploaded.

--- Domibus-MSH/src/main/java/eu/domibus/core/crypto/DefaultDomainCryptoServiceSpiImpl.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/crypto/DefaultDomainCryptoServiceSpiImpl.java
@@ -30,6 +30,8 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Properties;
 
@@ -149,6 +151,8 @@ public class DefaultDomainCryptoServiceSpiImpl extends Merlin implements DomainC
                 throw new CryptoException("Could not create parent directory for truststore", e);
             }
         }
+        // keep old truststore in case it needs to be restored, truststore_name.backup-yyyy-MM-dd_HH_mm_ss.SSS
+        backupTrustStore(trustStoreFile);
 
         LOG.debug("TrustStoreFile is: [{}]", trustStoreFile.getAbsolutePath());
         try (FileOutputStream fileOutputStream = new FileOutputStream(trustStoreFile)) {
@@ -164,6 +168,23 @@ public class DefaultDomainCryptoServiceSpiImpl extends Merlin implements DomainC
 
         signalTrustStoreUpdate();
     }
+
+    protected String backupTrustStore(File trustStoreFile) throws CryptoException {
+        if(trustStoreFile == null) {
+            LOG.warn("Truststore file was null, nothing to backup!");
+            return null;
+        }
+
+        try {
+            String backupName = trustStoreFile.getAbsolutePath() + ".backup-" + LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+            File backupFile = new File(backupName);
+            FileUtils.copyFile(trustStoreFile, backupFile);
+            return backupName;
+        } catch (IOException e) {
+            throw new CryptoException("Could not create backup file for truststore", e);
+        }
+    }
+
 
     @Override
     @Transactional(noRollbackFor = DomibusCertificateException.class)
