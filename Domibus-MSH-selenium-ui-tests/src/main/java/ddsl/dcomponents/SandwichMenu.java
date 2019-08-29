5ea13f9d0d666131c460f6a687b4dfe9f2613ebf
Rupam <rupam.rupam@ext.ec.europa.eu>
2019-08-29 09:58:14 +0200

    Renamed one variable on SandwichMenu.java

--- Domibus-MSH-selenium-ui-tests/src/main/java/ddsl/dcomponents/SandwichMenu.java
+++ Domibus-MSH-selenium-ui-tests/src/main/java/ddsl/dcomponents/SandwichMenu.java
@@ -100,8 +100,8 @@ public class SandwichMenu extends DComponent {
 	/**This method is implemented to check presence of link in Sandwich menu*/
 	public boolean isPresent() throws Exception {
 		expandMenu();
-		String userIDStr = driver.findElement(ChangePassLnk).getText();
-		boolean toReturn = !StringUtils.equalsIgnoreCase(userIDStr, "Change Password");
+		String chngLnk = driver.findElement(ChangePassLnk).getText();
+		boolean toReturn = !StringUtils.equalsIgnoreCase(chngLnk, "Change Password");
 		log.info("Availability of Change Password link is : " + toReturn);
 
 		return toReturn;
