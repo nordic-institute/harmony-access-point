d7876f0edd1aba76064b39762ecbd856f9e13adc
ion.perpegel <ion.perpegel@gmail.com>
2019-08-30 11:45:28 +0300

    [EDELIVERY-4812] add negative scenario unit test for fsplugin properties

--- Domibus-default-fs-plugin/src/test/java/eu/domibus/plugin/fs/property/FSPluginPropertiesTest.java
+++ Domibus-default-fs-plugin/src/test/java/eu/domibus/plugin/fs/property/FSPluginPropertiesTest.java
@@ -15,6 +15,8 @@ import org.springframework.context.ApplicationContext;
 
 import java.util.*;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * @author Cosmin Baciu
  * @since 4.1
@@ -71,6 +73,34 @@ public class FSPluginPropertiesTest {
         final Boolean isKnownFSProperty = fsPluginProperties.hasKnownProperty(propertyName);
 
         Assert.assertEquals(true, isKnownFSProperty);
+    }
+
+
+    @Test
+    public void testUnknownProperty() {
+        final String propertyName = "fsplugin.messages.location.unknown";
+
+        new Expectations(Collections.class) {{
+            domibusConfigurationExtService.isMultiTenantAware();
+            result = true;
+        }};
+
+        final Boolean isKnownFSProperty = fsPluginProperties.hasKnownProperty(propertyName);
+        Assert.assertEquals(false, isKnownFSProperty);
+
+        try {
+            fsPluginProperties.getKnownPropertyValue("default", propertyName);
+            Assert.fail("Expected exception was not raised!");
+        } catch (IllegalArgumentException e) {
+            assertEquals(true, e.getMessage().contains(propertyName));
+        }
+
+        try {
+            fsPluginProperties.setKnownPropertyValue("default", propertyName, "testValue");
+            Assert.fail("Expected exception was not raised!");
+        } catch (IllegalArgumentException e) {
+            assertEquals(true, e.getMessage().contains(propertyName));
+        }
     }
 
 }
