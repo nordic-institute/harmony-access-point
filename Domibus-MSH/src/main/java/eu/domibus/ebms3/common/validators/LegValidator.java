cc00611a3babc57b1a23a27b410edf2c138999b5
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2019-09-03 10:55:49 +0200

    [EDELIVERY-4972] improved the validator

--- Domibus-MSH/src/main/java/eu/domibus/ebms3/common/validators/LegValidator.java
+++ Domibus-MSH/src/main/java/eu/domibus/ebms3/common/validators/LegValidator.java
@@ -1,16 +1,22 @@
 package eu.domibus.ebms3.common.validators;
 
-import eu.domibus.common.model.configuration.*;
+import eu.domibus.common.model.configuration.BusinessProcesses;
+import eu.domibus.common.model.configuration.Configuration;
+import eu.domibus.common.model.configuration.LegConfiguration;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/** This validator checks everything related to Leg Configuration
+/**
+ * This validator checks everything related to Leg Configuration
+ *
  * @author Catalin Enache
  * @since 4.1.1
  */
@@ -19,6 +25,9 @@ import java.util.List;
 public class LegValidator implements ConfigurationValidator {
     private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(LegValidator.class);
 
+    final String[] attributesToCheck = {"service", "action", "security", "defaultMpc", "receptionAwareness", "reliability",
+            "errorHandling", "compressPayloads"};
+
     @Override
     public List<String> validate(Configuration configuration) {
 
@@ -26,22 +35,38 @@ public class LegValidator implements ConfigurationValidator {
 
         final BusinessProcesses businessProcesses = configuration.getBusinessProcesses();
 
-        for (LegConfiguration legConfiguration: businessProcesses.getLegConfigurations()) {
-               Service service = legConfiguration.getService();
-               String message;
-               if (null == service) {
-                   message = "Invalid service specified for leg configuration [" + legConfiguration.getName() + ']';
-                   issues.add(message);
-                   LOG.debug(message);
-               }
-               Action action = legConfiguration.getAction();
-            if (null == action) {
-                message = "Invalid action specified for leg configuration [" + legConfiguration.getName() + ']';
-                issues.add(message);
-                LOG.debug(message);
+        for (LegConfiguration legConfiguration : businessProcesses.getLegConfigurations()) {
+            String message;
+
+            for (String attribute: attributesToCheck) {
+                message = validateAttributeAgainstNull(legConfiguration, attribute);
+                if (StringUtils.isNotEmpty(message)) {
+                    message += "for leg configuration [" + legConfiguration.getName() + "]";
+                    issues.add(message);
+                    LOG.debug(message);
+                }
             }
         }
 
         return Collections.unmodifiableList(issues);
+    }
+
+    private String validateAttributeAgainstNull(Object object, String fieldName) {
+        if (object == null) {
+            return StringUtils.EMPTY;
+        }
+
+        Class clazz = object.getClass();
+        try {
+            Field field = clazz.getDeclaredField(fieldName);
+            field.setAccessible(true);
+            Object fieldObject = field.get(object);
+            if (null == fieldObject) {
+                return "Invalid " + fieldName + " specified ";
+            }
+        } catch (NoSuchFieldException | IllegalAccessException | SecurityException e) {
+            LOG.debug("Unable to access attribute: " + fieldName);
+        }
+        return StringUtils.EMPTY;
     }
 }
