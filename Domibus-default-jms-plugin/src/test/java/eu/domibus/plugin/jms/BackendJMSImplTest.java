b704b67132c57599c1f70cecb75c0aacf3794f4b
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2019-09-09 11:20:39 +0200

    [EDELIVERY-4824] clean/trim the messageId for JMS plugin

--- Domibus-default-jms-plugin/src/test/java/eu/domibus/plugin/jms/BackendJMSImplTest.java
+++ Domibus-default-jms-plugin/src/test/java/eu/domibus/plugin/jms/BackendJMSImplTest.java
@@ -9,9 +9,9 @@ import eu.domibus.ext.services.DomainContextExtService;
 import eu.domibus.ext.services.DomibusPropertyExtService;
 import eu.domibus.ext.services.JMSExtService;
 import eu.domibus.ext.services.MessageExtService;
+import eu.domibus.plugin.handler.MessagePuller;
 import eu.domibus.plugin.handler.MessageRetriever;
 import eu.domibus.plugin.handler.MessageSubmitter;
-import eu.domibus.plugin.handler.MessagePuller;
 import mockit.*;
 import mockit.integration.junit4.JMockit;
 import org.apache.activemq.command.ActiveMQMapMessage;
@@ -22,6 +22,7 @@ import org.springframework.jms.core.JmsOperations;
 import javax.jms.MapMessage;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 
 /**
@@ -104,6 +105,45 @@ public class BackendJMSImplTest {
 
             assertEquals(capturedMessageId, messageId);
             assertEquals(capturedJmsCorrelationId, jmsCorrelationId);
+        }};
+    }
+
+    @Test
+    public void testReceiveMessage_MessageId_WithEmptySpaces(@Injectable final MapMessage map) throws Exception {
+        final String messageId = " test123 ";
+        final String messageIdTrimmed = "test123";
+        final String jmsCorrelationId = "2";
+        final String messageTypeSubmit = JMSMessageConstants.MESSAGE_TYPE_SUBMIT;
+
+        new Expectations(backendJMS) {{
+            map.getStringProperty(JMSMessageConstants.MESSAGE_ID);
+            result = messageId;
+
+            map.getJMSCorrelationID();
+            result = jmsCorrelationId;
+
+            map.getStringProperty(JMSMessageConstants.JMS_BACKEND_MESSAGE_TYPE_PROPERTY_KEY);
+            result = messageTypeSubmit;
+
+            backendJMS.submit(withAny(new ActiveMQMapMessage()));
+            result = messageIdTrimmed;
+
+            backendJMS.sendReplyMessage(anyString, anyString, jmsCorrelationId);
+        }};
+
+        backendJMS.receiveMessage(map);
+
+        new Verifications() {{
+            backendJMS.submit(map);
+
+            String capturedMessageId = null;
+            String capturedJmsCorrelationId = null;
+            String capturedErrorMessage = null;
+            backendJMS.sendReplyMessage(capturedMessageId = withCapture(), capturedErrorMessage = withCapture(), capturedJmsCorrelationId = withCapture());
+
+            assertEquals(capturedMessageId, messageIdTrimmed);
+            assertEquals(capturedJmsCorrelationId, jmsCorrelationId);
+            assertNull(capturedErrorMessage);
         }};
     }
 
