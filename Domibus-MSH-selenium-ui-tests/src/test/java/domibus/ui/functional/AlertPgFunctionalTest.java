195caed022ac3112cd69f87f1779ecce7cc59872
Rupam <rupam.rupam@ext.ec.europa.eu>
2019-10-11 11:46:21 +0200

    Review comment implemented

--- Domibus-MSH-selenium-ui-tests/src/test/java/domibus/ui/functional/AlertPgFunctionalTest.java
+++ Domibus-MSH-selenium-ui-tests/src/test/java/domibus/ui/functional/AlertPgFunctionalTest.java
@@ -24,26 +24,26 @@ public class AlertPgFunctionalTest extends BaseTest {
     public void SearchBasicFilters() throws Exception {
         SoftAssert soft = new SoftAssert();
         DomibusPage page = new DomibusPage(driver);
-        LoginPage Lpage = new LoginPage(driver);
+        LoginPage lpage = new LoginPage(driver);
         String userName = Generator.randomAlphaNumeric(3);
         rest.createUser(userName, DRoles.USER, data.getDefaultTestPass(), null);
-        Lpage.login(userName, data.getNewTestPass());
-        soft.assertFalse(Lpage.getSandwichMenu().isLoggedIn(), "User not logged in");
-        log.info("Alert Message shown : " + Lpage.getAlertArea().getAlertMessage());
-        Lpage.login(data.getAdminUser());
+        lpage.login(userName, data.getNewTestPass());
+        soft.assertFalse(lpage.getSandwichMenu().isLoggedIn(), "User not logged in");
+        log.info("Alert Message shown : " + lpage.getAlertArea().getAlertMessage());
+        lpage.login(data.getAdminUser());
         page.getSidebar().gGoToPage(PAGES.ALERTS);
-        AlertPage Apage = new AlertPage(driver);
-        log.info("Number of records : " + Apage.grid().getRowsNo());
+        AlertPage apage = new AlertPage(driver);
+        log.info("Number of records : " + apage.grid().getRowsNo());
         log.info("Getting all listed alert info");
-        log.info("Alert type for top row : " + Apage.grid().getRowInfo(0).get("Alert Type"));
-        String beforeSearchalertType = Apage.grid().getRowInfo(0).get("Alert Type");
-        List<HashMap<String, String>> allRowInfo = Apage.grid().getAllRowInfo();
+        log.info("Alert type for top row : " + apage.grid().getRowInfo(0).get("Alert Type"));
+        String beforeSearchalertType = apage.grid().getRowInfo(0).get("Alert Type");
+        List<HashMap<String, String>> allRowInfo = apage.grid().getAllRowInfo();
         HashMap<String, String> fAlert = allRowInfo.get(0);
         log.info("Basic filtering by " + fAlert);
-        Apage.getFilters().basicFilterBy(null, fAlert.get("Alert Type"), fAlert.get("Alert Status"),
+        apage.getFilters().basicFilterBy(null, fAlert.get("Alert Type"), fAlert.get("Alert Status"),
                 fAlert.get("Alert level"), fAlert.get("Creation Time"), null);
-        Apage.grid().waitForRowsToLoad();
-        String afterSearchAlertType = Apage.grid().getRowInfo(0).get("Alert Type");
+        apage.grid().waitForRowsToLoad();
+        String afterSearchAlertType = apage.grid().getRowInfo(0).get("Alert Type");
         soft.assertTrue(beforeSearchalertType.equals(afterSearchAlertType), "After and before search records are same");
         soft.assertAll();
 
@@ -54,27 +54,27 @@ public class AlertPgFunctionalTest extends BaseTest {
     public void SearchAdvanceFilters() throws Exception {
         SoftAssert soft = new SoftAssert();
         DomibusPage page = new DomibusPage(driver);
-        LoginPage Lpage = new LoginPage(driver);
+        LoginPage lpage = new LoginPage(driver);
         String userName = Generator.randomAlphaNumeric(3);
         rest.createUser(userName, DRoles.USER, data.getDefaultTestPass(), null);
-        Lpage.login(userName, data.getNewTestPass());
-        soft.assertFalse(Lpage.getSandwichMenu().isLoggedIn(), "User not logged in");
-        log.info("Alert Message shown : " + Lpage.getAlertArea().getAlertMessage());
-        Lpage.login(data.getAdminUser());
+        lpage.login(userName, data.getNewTestPass());
+        soft.assertFalse(lpage.getSandwichMenu().isLoggedIn(), "User not logged in");
+        log.info("Alert Message shown : " + lpage.getAlertArea().getAlertMessage());
+        lpage.login(data.getAdminUser());
         page.getSidebar().gGoToPage(PAGES.ALERTS);
-        AlertPage Apage = new AlertPage(driver);
-        log.info("Number of records : " + Apage.grid().getRowsNo());
+        AlertPage apage = new AlertPage(driver);
+        log.info("Number of records : " + apage.grid().getRowsNo());
         log.info("Getting all listed alert info");
-        log.info("Alert type for top row : " + Apage.grid().getRowInfo(0).get("Alert Type"));
-        String beforeSearchalertType = Apage.grid().getRowInfo(0).get("Alert Type");
-        List<HashMap<String, String>> allRowInfo = Apage.grid().getAllRowInfo();
+        log.info("Alert type for top row : " + apage.grid().getRowInfo(0).get("Alert Type"));
+        String beforeSearchalertType = apage.grid().getRowInfo(0).get("Alert Type");
+        List<HashMap<String, String>> allRowInfo = apage.grid().getAllRowInfo();
         HashMap<String, String> fAlert = allRowInfo.get(0);
         log.info("Advance filtering by " + fAlert);
-        Apage.getFilters().advancedFilterBy(null, fAlert.get("Alert Type"), fAlert.get("Alert Status")
+        apage.getFilters().advancedFilterBy(null, fAlert.get("Alert Type"), fAlert.get("Alert Status")
                 , null, fAlert.get("Alert Level"), fAlert.get("Creation Time"), null,
                 fAlert.get("Reporting Time"), null);
-        Apage.grid().waitForRowsToLoad();
-        String afterSearchAlertType = Apage.grid().getRowInfo(0).get("Alert Type");
+        apage.grid().waitForRowsToLoad();
+        String afterSearchAlertType = apage.grid().getRowInfo(0).get("Alert Type");
         soft.assertTrue(beforeSearchalertType.equals(afterSearchAlertType), "After and before search records are same");
         soft.assertAll();
     }
@@ -86,12 +86,12 @@ public class AlertPgFunctionalTest extends BaseTest {
         DomibusPage page = new DomibusPage(driver);
         log.info("Login into application and navigate to Alerts page");
         login(data.getAdminUser()).getSidebar().gGoToPage(PAGES.ALERTS);
-        AlertPage Apage = new AlertPage(driver);
+        AlertPage apage = new AlertPage(driver);
         log.info("Search using basic filters");
-        Apage.getFilters().basicFilterBy(null, "PLUGIN_USER_LOGIN_FAILURE", null, null, null, null);
-        Apage.grid().waitForRowsToLoad();
+        apage.getFilters().basicFilterBy(null, "PLUGIN_USER_LOGIN_FAILURE", null, null, null, null);
+        apage.grid().waitForRowsToLoad();
         log.info("Validate grid count as zero");
-        soft.assertTrue(Apage.grid().getPagination().getTotalItems() == 0, "No search result exist");
+        soft.assertTrue(apage.grid().getPagination().getTotalItems() == 0, "No search result exist");
         soft.assertAll();
     }
 
@@ -102,21 +102,21 @@ public class AlertPgFunctionalTest extends BaseTest {
         DomibusPage page = new DomibusPage(driver);
         log.info("Login into application and navigate to Alerts page");
         login(data.getAdminUser()).getSidebar().gGoToPage(PAGES.ALERTS);
-        AlertPage Apage = new AlertPage(driver);
-        Apage.grid().waitForRowsToLoad();
+        AlertPage apage = new AlertPage(driver);
+        apage.grid().waitForRowsToLoad();
         log.info("Search using basic filter");
-        int prevCount = Apage.grid().getPagination().getTotalItems();
+        int prevCount = apage.grid().getPagination().getTotalItems();
         log.info("Previous count of grid rows:" + prevCount);
-        Apage.getFilters().basicFilterBy(null, "PLUGIN_USER_LOGIN_FAILURE", null, null, null, null);
+        apage.getFilters().basicFilterBy(null, "PLUGIN_USER_LOGIN_FAILURE", null, null, null, null);
         log.info("Validate Grid row count as zero ");
-        soft.assertTrue(Apage.grid().getPagination().getTotalItems() == 0, "No search result exist");
+        soft.assertTrue(apage.grid().getPagination().getTotalItems() == 0, "No search result exist");
         log.info("Refresh page");
-        Apage.refreshPage();
+        apage.refreshPage();
         log.info("Wait for grid row to load ");
-        Apage.grid().waitForRowsToLoad();
+        apage.grid().waitForRowsToLoad();
         log.info("Validate actual grid row count ");
-        log.info("Current grid row count:" + Apage.grid().getPagination().getTotalItems());
-        soft.assertTrue(Apage.grid().getPagination().getTotalItems() == prevCount, "All search result exist");
+        log.info("Current grid row count:" + apage.grid().getPagination().getTotalItems());
+        soft.assertTrue(apage.grid().getPagination().getTotalItems() == prevCount, "All search result exist");
         soft.assertAll();
     }
 
@@ -127,7 +127,7 @@ public class AlertPgFunctionalTest extends BaseTest {
         DomibusPage page = new DomibusPage(driver);
         log.info("Login into application with super admin credentials and navigate to Alerts page");
         login(data.getAdminUser()).getSidebar().gGoToPage(PAGES.ALERTS);
-        AlertPage Apage = new AlertPage(driver);
+        AlertPage apage = new AlertPage(driver);
         AlertFilters AFpage = new AlertFilters(driver);
         log.info("Check presence of Show domain checkbox");
         soft.assertTrue(AFpage.getShowDomainCheckbox().isPresent(), "CheckBox is  present in case of Admin User");
@@ -167,23 +167,23 @@ public class AlertPgFunctionalTest extends BaseTest {
         modal.getResendButton().click();
         log.info("Navigate to Alerts page");
         page.getSidebar().gGoToPage(PAGES.ALERTS);
-        AlertPage Apage = new AlertPage(driver);
+        AlertPage apage = new AlertPage(driver);
         log.info("Search data using Msg_status_changed alert type");
-        Apage.getFilters().basicFilterBy(null, "MSG_STATUS_CHANGED", null, null, null, null);
+        apage.getFilters().basicFilterBy(null, "MSG_STATUS_CHANGED", null, null, null, null);
         log.info("Check if Multidomain exists");
         if (data.isIsMultiDomain()) {
             log.info("Click on Show domain checkbox");
-            Apage.getFilters().getShowDomainCheckbox().click();
+            apage.getFilters().getShowDomainCheckbox().click();
             log.info("Click on search button");
-            Apage.getFilters().getSearchButton().click();
+            apage.getFilters().getSearchButton().click();
         }
         log.info("Validate data for given message id,status ,alert type ,alert status and level");
-        soft.assertTrue(Apage.grid().getRowInfo(0).get("Alert Type").contains("MSG_STATUS_CHANGED"), "Top row contains alert type as Msg_Status_Changed");
-        soft.assertTrue(Apage.grid().getRowInfo(0).get("Alert Level").contains("HIGH"), "Top row contains alert level as High");
-        soft.assertTrue(Apage.grid().getRowInfo(0).get("Alert Status").contains("SUCCESS"), "Top row contains alert Status as Success");
-        soft.assertTrue(Apage.grid().getRowInfo(0).get("Parameters").contains(messID), "Top row contains alert for message status changed for :" + messID);
-        soft.assertTrue(Apage.grid().getRowInfo(0).get("Parameters").contains("SEND_FAILURE"), "Top row contains alert for message status as Send_failure");
-        soft.assertTrue(Apage.grid().getRowInfo(0).get("Parameters").contains("SEND_ENQUEUED"), "Top row contains alert for message status as Send_Enqueued");
+        soft.assertTrue(apage.grid().getRowInfo(0).get("Alert Type").contains("MSG_STATUS_CHANGED"), "Top row contains alert type as Msg_Status_Changed");
+        soft.assertTrue(apage.grid().getRowInfo(0).get("Alert Level").contains("HIGH"), "Top row contains alert level as High");
+        soft.assertTrue(apage.grid().getRowInfo(0).get("Alert Status").contains("SUCCESS"), "Top row contains alert Status as Success");
+        soft.assertTrue(apage.grid().getRowInfo(0).get("Parameters").contains(messID), "Top row contains alert for message status changed for :" + messID);
+        soft.assertTrue(apage.grid().getRowInfo(0).get("Parameters").contains("SEND_FAILURE"), "Top row contains alert for message status as Send_failure");
+        soft.assertTrue(apage.grid().getRowInfo(0).get("Parameters").contains("SEND_ENQUEUED"), "Top row contains alert for message status as Send_Enqueued");
         soft.assertAll();
 
     }
@@ -193,32 +193,32 @@ public class AlertPgFunctionalTest extends BaseTest {
     public void UserLoginFailureAlert() throws Exception {
         SoftAssert soft = new SoftAssert();
         DomibusPage page = new DomibusPage(driver);
-        LoginPage Lpage = new LoginPage(driver);
+        LoginPage lpage = new LoginPage(driver);
         String userName = Generator.randomAlphaNumeric(3);
         log.info("Create user");
         rest.createUser(userName, DRoles.USER, data.getDefaultTestPass(), null);
         log.info("Try to login with user with wrong password");
-        Lpage.login(userName, data.getNewTestPass());
+        lpage.login(userName, data.getNewTestPass());
         log.info("Alert message shown :" + page.getAlertArea().getAlertMessage());
         log.info("Login into application");
-        Lpage.login(data.getAdminUser());
+        lpage.login(data.getAdminUser());
         log.info("Navigate to Alerts page");
         page.getSidebar().gGoToPage(PAGES.ALERTS);
-        AlertPage Apage = new AlertPage(driver);
+        AlertPage apage = new AlertPage(driver);
         log.info("Search data using basic filter for user_login_failure alert type");
-        Apage.getFilters().basicFilterBy(null, "USER_LOGIN_FAILURE", null, null, null, null);
+        apage.getFilters().basicFilterBy(null, "USER_LOGIN_FAILURE", null, null, null, null);
         log.info("Check if multidomain exists");
         if (data.isIsMultiDomain()) {
             log.info("Select show domain check box");
-            Apage.getFilters().getShowDomainCheckbox().click();
+            apage.getFilters().getShowDomainCheckbox().click();
             log.info("Click on search button");
-            Apage.getFilters().getSearchButton().click();
+            apage.getFilters().getSearchButton().click();
         }
         log.info("Validate pressence of alert data for user_login_failure alert type for given user");
-        soft.assertTrue(Apage.grid().getRowInfo(0).get("Alert Type").contains("USER_LOGIN_FAILURE"), "Top row contains alert type as USER_LOGIN_FAILURE");
-        soft.assertTrue(Apage.grid().getRowInfo(0).get("Alert Level").contains("LOW"), "Top row contains alert level as low");
-        soft.assertTrue(Apage.grid().getRowInfo(0).get("Alert Status").contains("SUCCESS"), "Top row contains alert status as success");
-        soft.assertTrue(Apage.grid().getRowInfo(0).get("Parameters").contains(userName), "Top row contains alert type as USER_LOGIN_FAILURE");
+        soft.assertTrue(apage.grid().getRowInfo(0).get("Alert Type").contains("USER_LOGIN_FAILURE"), "Top row contains alert type as USER_LOGIN_FAILURE");
+        soft.assertTrue(apage.grid().getRowInfo(0).get("Alert Level").contains("LOW"), "Top row contains alert level as low");
+        soft.assertTrue(apage.grid().getRowInfo(0).get("Alert Status").contains("SUCCESS"), "Top row contains alert status as success");
+        soft.assertTrue(apage.grid().getRowInfo(0).get("Parameters").contains(userName), "Top row contains alert type as USER_LOGIN_FAILURE");
         soft.assertAll();
     }
 
@@ -230,37 +230,38 @@ public class AlertPgFunctionalTest extends BaseTest {
         String userName = Generator.randomAlphaNumeric(3);
         log.info("Create user");
         rest.createUser(userName, DRoles.USER, data.getDefaultTestPass(), null);
-        LoginPage Lpage = new LoginPage(driver);
+        LoginPage lpage = new LoginPage(driver);
         log.info("Try to login with wrong password for 5 times so that user account gets disabled");
         for (int i = 0; i <= 5; i++) {
-            Lpage.login(userName, "abc");
-            log.info("Alert Message shown : " + Lpage.getAlertArea().getAlertMessage());
+            lpage.login(userName, "abc");
+            log.info("Alert Message shown : " + lpage.getAlertArea().getAlertMessage());
         }
         log.info("Login with Super/admin user");
-        Lpage.login(data.getAdminUser());
+        lpage.login(data.getAdminUser());
         log.info("Navigate to Alerts page");
         page.getSidebar().gGoToPage(PAGES.ALERTS);
-        AlertPage Apage = new AlertPage(driver);
+        AlertPage apage = new AlertPage(driver);
         log.info("Search by basic filter for alert type : user account disabled");
-        Apage.getFilters().basicFilterBy(null, "USER_ACCOUNT_DISABLED", null, null, null, null);
+        apage.getFilters().basicFilterBy(null, "USER_ACCOUNT_DISABLED", null, null, null, null);
         log.info("Check if multi domain exists");
         if (data.isIsMultiDomain()) {
             log.info("Check show domain alert checkbox");
-            Apage.getFilters().getShowDomainCheckbox().click();
+            apage.getFilters().getShowDomainCheckbox().click();
             log.info("Click on search button");
-            Apage.getFilters().getSearchButton().click();
+            apage.getFilters().getSearchButton().click();
         }
         log.info("Validate top row for user account disabled alert type for given user");
-        soft.assertTrue(Apage.grid().getRowInfo(0).get("Alert Type").contains("USER_ACCOUNT_DISABLED"), "Alert for disabled account is shown ");
-        soft.assertTrue(Apage.grid().getRowInfo(0).get("Alert Level").contains("HIGH"), "Disable account alert is of High level");
-        soft.assertTrue(Apage.grid().getRowInfo(0).get("Alert Status").contains("SUCCESS"), "Account disabled alert has Success status");
-        soft.assertTrue(Apage.grid().getRowInfo(0).get("Parameters").contains(userName), "Alert for user :" + userName + "disabled account is shown here");
+        soft.assertTrue(apage.grid().getRowInfo(0).get("Alert Type").contains("USER_ACCOUNT_DISABLED"), "Alert for disabled account is shown ");
+        soft.assertTrue(apage.grid().getRowInfo(0).get("Alert Level").contains("HIGH"), "Disable account alert is of High level");
+        soft.assertTrue(apage.grid().getRowInfo(0).get("Alert Status").contains("SUCCESS"), "Account disabled alert has Success status");
+        soft.assertTrue(apage.grid().getRowInfo(0).get("Parameters").contains(userName), "Alert for user :" + userName + "disabled account is shown here");
         soft.assertAll();
 
     }
 
+    }
 
-}
+
 
 
 
