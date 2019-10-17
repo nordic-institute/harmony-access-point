3d16b2d417fc72be044b592d070cf3bfeb802bf2
comanca <Catalin.Comanici@ec.europa.eu>
2019-10-17 14:04:29 +0200

    Minor fix

--- Domibus-MSH-selenium-ui-tests/src/main/java/ddsl/dobjects/DWait.java
+++ Domibus-MSH-selenium-ui-tests/src/main/java/ddsl/dobjects/DWait.java
@@ -17,13 +17,11 @@ import utils.TestRunData;
 public class DWait {
 
 	public final WebDriverWait defaultWait;
-	public final WebDriverWait longWait;
 	private TestRunData data = new TestRunData();
 
 
 	public DWait(WebDriver driver) {
 		this.defaultWait = new WebDriverWait(driver, data.getTIMEOUT());
-		this.longWait = new WebDriverWait(driver, data.getLongWait());
 	}
 
 	public void forXMillis(Integer millis) {
@@ -91,16 +89,6 @@ public class DWait {
 		defaultWait.until(ExpectedConditions.textToBePresentInElement(element, text));
 	}
 
-	public void longWaitforElementToBe(WebElement element) {
-
-		longWait.until(new ExpectedCondition<Boolean>() {
-			@Override
-			public Boolean apply(WebDriver driver) {
-				return element.getLocation() != null;
-			}
-		});
-//		defaultWait.until(ExpectedConditions.)
-	}
 
 
 }
