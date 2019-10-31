f86a42ed89f11ec70c99bfef45fa5a15f4b89206
baciuco <cosmin.baciu@ext.ec.europa.eu>
2019-10-31 16:07:15 +0100

    [EDELIVERY-2221] - Documented API for password encryption

--- Domibus-plugin-api/src/main/java/eu/domibus/plugin/encryption/PluginPropertyEncryptionListener.java
+++ Domibus-plugin-api/src/main/java/eu/domibus/plugin/encryption/PluginPropertyEncryptionListener.java
@@ -2,7 +2,7 @@ package eu.domibus.plugin.encryption;
 
 /**
  * This interface should be implemented by plugins that want to support password encryption. <p/>
- * If password encryption is active Domibus will generate the secret keys that are used to encrypt the password and notify the subscribed listeners to encrypt their plugin passwords.
+ * If password encryption is active, Domibus will generate the secret keys that are used to encrypt the passwords and notify afterwards the subscribed plugin listeners to encrypt their own passwords.
  * Plugins must use the password encryption exposed services to handle the password encryption.
  *
  * @author Cosmin Baciu
