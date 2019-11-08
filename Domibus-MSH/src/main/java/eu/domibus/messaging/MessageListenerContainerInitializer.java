9d4e340ce10df5046720e255c587f3ad2023d5e1
Ioana DRAGUSANU <idragusa@gmail.com>
2019-11-08 09:53:56 +0200

    [EDELIVERY-5440] Review comments

--- Domibus-MSH/src/main/java/eu/domibus/messaging/MessageListenerContainerInitializer.java
+++ Domibus-MSH/src/main/java/eu/domibus/messaging/MessageListenerContainerInitializer.java
@@ -65,7 +65,6 @@ public class MessageListenerContainerInitializer {
         LOG.info("Shutting down MessageListenerContainer instances");
 
         for (MessageListenerContainer instance : instances) {
-            LOG.trace("Shutting down MessageListenerContainer instances [{}]", instance);
             try {
                 ((AbstractJmsListeningContainer) instance).shutdown();
             } catch (Exception e) {
