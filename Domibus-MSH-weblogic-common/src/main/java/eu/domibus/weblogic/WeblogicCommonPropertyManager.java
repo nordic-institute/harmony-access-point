c02b87f1a718ac0da2cc97f4ca738c4371a1d323
ion.perpegel <ion.perpegel@gmail.com>
2019-11-13 14:56:34 +0200

    [EDELIVERY-5543] reuse a common clas as property manager

--- Domibus-MSH-weblogic-common/src/main/java/eu/domibus/weblogic/WeblogicCommonPropertyManager.java
+++ Domibus-MSH-weblogic-common/src/main/java/eu/domibus/weblogic/WeblogicCommonPropertyManager.java
@@ -3,23 +3,24 @@ package eu.domibus.weblogic;
 import eu.domibus.ext.domain.DomibusPropertyMetadataDTO;
 import eu.domibus.ext.domain.Module;
 import eu.domibus.ext.services.DomibusPropertyExtServiceDelegateAbstract;
-import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static eu.domibus.api.property.DomibusPropertyMetadataManager.*;
+
 /**
  * @author Ion Perpegel
  * @since 4.2
  * <p>
- * Property manager for the Weblogic ECAS specific properties.
+ * Common property manager for the Weblogic servers/implementations specific properties.
+ * Weblogic and ECAS managers derive from this
  */
-@Service
-public class ECASPropertyManager extends DomibusPropertyExtServiceDelegateAbstract {
+public class WeblogicCommonPropertyManager extends DomibusPropertyExtServiceDelegateAbstract {
 
     private Map<String, DomibusPropertyMetadataDTO> knownProperties = Arrays.stream(new String[]{
-            DOMIBUS_CLUSTER_COMMAND_CRON_EXPRESSION,
+            DOMIBUS_CLUSTER_COMMAND_CRON_EXPRESSION, //present just in domibus.properties but seems not to be used anywhere!!
             DOMIBUS_JMX_USER,
             DOMIBUS_JMX_PASSWORD,
             DOMIBUS_SECURITY_EXT_AUTH_PROVIDER_ENABLED
--- Domibus-MSH-weblogic/src/main/java/eu/domibus/weblogic/WeblogicPropertyManager.java
+++ Domibus-MSH-weblogic/src/main/java/eu/domibus/weblogic/WeblogicPropertyManager.java
@@ -16,20 +16,6 @@ import java.util.stream.Collectors;
  * Property manager for the Weblogic ECAS specific properties.
  */
 @Service
-public class WeblogicPropertyManager extends DomibusPropertyExtServiceDelegateAbstract {
-
-    private Map<String, DomibusPropertyMetadataDTO> knownProperties = Arrays.stream(new String[]{
-            DOMIBUS_CLUSTER_COMMAND_CRON_EXPRESSION,
-            DOMIBUS_JMX_USER,
-            DOMIBUS_JMX_PASSWORD,
-            DOMIBUS_SECURITY_EXT_AUTH_PROVIDER_ENABLED
-    })
-            .map(name -> DomibusPropertyMetadataDTO.getReadOnlyGlobalProperty(name, Module.WEBLOGIC_ECAS))
-            .collect(Collectors.toMap(x -> x.getName(), x -> x));
-
-    @Override
-    public Map<String, DomibusPropertyMetadataDTO> getKnownProperties() {
-        return knownProperties;
-    }
+public class WeblogicPropertyManager extends WeblogicCommonPropertyManager {
 
 }
