8d0629201cd54d5cd93c7d7eb63f5a2003e6de32
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2019-11-25 13:42:10 +0100

    [EDELIVERY-5576] logging no more necessary

--- Domibus-MSH/src/main/java/eu/domibus/common/metrics/MetricsServletContextListener.java
+++ Domibus-MSH/src/main/java/eu/domibus/common/metrics/MetricsServletContextListener.java
@@ -3,8 +3,6 @@ package eu.domibus.common.metrics;
 
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.servlets.MetricsServlet;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author Thomas Dussart
@@ -12,11 +10,8 @@ import org.slf4j.LoggerFactory;
  */
 public class MetricsServletContextListener extends MetricsServlet.ContextListener {
 
-    protected static final Logger LOG = LoggerFactory.getLogger(MetricsServletContextListener.class);
-
     @Override
     protected MetricRegistry getMetricRegistry() {
-        LOG.debug("returning MetricRegistry...");
         return MetricsHelper.getMetricRegistry();
     }
 }
