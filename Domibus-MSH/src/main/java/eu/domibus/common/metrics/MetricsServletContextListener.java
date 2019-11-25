9ca6591c115ddd526c141a6194a7dcbf03793f50
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2019-11-25 11:35:28 +0100

    [EDELIVERY-5576] added debug

--- Domibus-MSH/src/main/java/eu/domibus/common/metrics/MetricsServletContextListener.java
+++ Domibus-MSH/src/main/java/eu/domibus/common/metrics/MetricsServletContextListener.java
@@ -3,6 +3,8 @@ package eu.domibus.common.metrics;
 
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.servlets.MetricsServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Thomas Dussart
@@ -10,8 +12,11 @@ import com.codahale.metrics.servlets.MetricsServlet;
  */
 public class MetricsServletContextListener extends MetricsServlet.ContextListener {
 
+    protected static final Logger LOG = LoggerFactory.getLogger(MetricsServletContextListener.class);
+
     @Override
     protected MetricRegistry getMetricRegistry() {
+        LOG.debug("returning MetricRegistry...");
         return MetricsHelper.getMetricRegistry();
     }
 }
