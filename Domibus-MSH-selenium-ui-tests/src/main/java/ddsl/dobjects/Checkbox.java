42f6ad3ad03cd768dc58b97d9792aa207a3bb16e
ion.perpegel <ion.perpegel@gmail.com>
2019-12-04 14:29:15 +0100

    [EDELIVERY-5574] adapt selenium testst to the changes of 2337

--- Domibus-MSH-selenium-ui-tests/src/main/java/ddsl/dobjects/Checkbox.java
+++ Domibus-MSH-selenium-ui-tests/src/main/java/ddsl/dobjects/Checkbox.java
@@ -1,46 +1,64 @@
 package ddsl.dobjects;
 
+import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 
 /**
  * @author Catalin Comanici
-
  * @version 4.1
  */
 
 
 public class Checkbox extends DObject {
 
+	WebElement labelElement;
+
 	public Checkbox(WebDriver driver, WebElement element) {
-		super(driver, element);
+		this(driver, element, null);
 	}
 
-	public boolean isChecked() throws Exception {
-		if (isPresent()) {
-			return (null != element.getAttribute("checked"));
-		}
-		throw new DObjectNotPresentException();
-	}
+    public Checkbox(WebDriver driver, WebElement element, WebElement labelElement) {
+        super(driver, element);
 
-	public void check() throws Exception {
-		if (isChecked()) return;
-		if (isEnabled()) {
-			element.click();
-			return;
-		}
-		throw new Exception("Checkbox is not enabled");
-	}
+        this.labelElement = labelElement;
+    }
 
-	public void uncheck() throws Exception {
-		if (!isChecked()) return;
-		if (isEnabled()) {
-			element.click();
-			return;
+    public boolean isChecked() throws Exception {
+        if (isPresent()) {
+            return (null != element.getAttribute("checked"));
+        }
+        throw new DObjectNotPresentException();
+    }
+
+    public void check() throws Exception {
+        if (isChecked()) return;
+        if (isEnabled()) {
+			clickCheckbox();
+            return;
+        }
+        throw new Exception("Checkbox is not enabled");
+    }
+
+    public void uncheck() throws Exception {
+        if (!isChecked()) return;
+        if (isEnabled()) {
+			clickCheckbox();
+            return;
+        }
+        throw new Exception("Checkbox is not enabled");
+    }
+
+    private void clickCheckbox() {
+        try {
+            element.click();
+        } catch (ElementNotInteractableException ex) {
+        	// in mat-checkbox the input is actually hidden, and the user has to click on the label to interact with it
+            if (this.labelElement != null)
+				this.labelElement.click();
 		}
-		throw new Exception("Checkbox is not enabled");
-	}
+    }
 
 
 }
