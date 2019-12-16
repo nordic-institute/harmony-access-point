35c8c2fd77f53ac9ada5ab34c25a323370a6fc30
hykiukira <hykiukira@gmail.com>
2019-12-16 14:47:41 +0100

    EDELIVERY:5573 change the transaction scope

--- Domibus-MSH/src/main/java/eu/domibus/submission/RawMessageService.java
+++ Domibus-MSH/src/main/java/eu/domibus/submission/RawMessageService.java
@@ -1,11 +1,13 @@
 package eu.domibus.submission;
 
+import com.codahale.metrics.Counter;
 import com.codahale.metrics.MetricRegistry;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
 import eu.domibus.web.rest.testdb.TestDB;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -21,13 +23,25 @@ public class RawMessageService {
     RawMessageDao rawMessageDao;
 
     public void updateRawMessage(RawMessage rawMessage, String type) {
-        rawMessage.setRawEnvelope(rawMessage.getRawEnvelope().replace("HOLDER", "HOLDER" + type+System.currentTimeMillis()));
+        rawMessage.setRawEnvelope(rawMessage.getRawEnvelope().replace("HOLDER", "HOLDER" + type + System.currentTimeMillis()));
     }
 
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void saveSync(RawMessage rawMessage, String name) {
-        com.codahale.metrics.Timer.Context saveSyncTimer = metricRegistry.timer(MetricRegistry.name(TestDB.class, name)).time();
-        rawMessageDao.create(rawMessage);
-        saveSyncTimer.stop();
+        Counter counter = null;
+        com.codahale.metrics.Timer.Context saveSyncTimerContext = null;
+        saveSyncTimerContext = metricRegistry.timer(MetricRegistry.name(TestDB.class, name)).time();
+        counter = metricRegistry.counter(MetricRegistry.name(TestDB.class, name + "_counter"));
+        counter.inc();
+        try {
+            rawMessageDao.create(rawMessage);
+        } finally {
+            if (counter != null) {
+                counter.dec();
+            }
+            if (saveSyncTimerContext != null) {
+                saveSyncTimerContext.stop();
+            }
+        }
     }
 }
