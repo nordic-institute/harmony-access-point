02db64267edcff26328a273b3199d39c295f535b
hykiukira <hykiukira@gmail.com>
2019-12-17 12:36:36 +0100

    EDELIVERY:5573 add metrics

--- Domibus-MSH/src/main/java/eu/domibus/web/rest/testdb/TestConnection.java
+++ Domibus-MSH/src/main/java/eu/domibus/web/rest/testdb/TestConnection.java
@@ -1,5 +1,7 @@
 package eu.domibus.web.rest.testdb;
 
+import eu.domibus.common.metrics.Counter;
+import eu.domibus.common.metrics.Timer;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -13,18 +15,23 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.ws.rs.QueryParam;
 import java.io.IOException;
 
+import static eu.domibus.common.metrics.MetricNames.SERVLET_INCOMING_USER_MESSAGE;
+import static eu.domibus.common.metrics.MetricNames.TEST_CN;
+
 @RestController
 @RequestMapping(value = "/rest/testconnection")
+
 public class TestConnection {
     private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(TestConnection.class);
 
     @RequestMapping(path = "cn", method = RequestMethod.POST)
     @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    @Timer(value = TEST_CN)
+    @Counter(TEST_CN)
     public ResponseEntity<String> testCn(
             @RequestPart("myFile") MultipartFile messageXML,
             @QueryParam("count") int count) {
         LOG.info("Testing the connection, count: [{}]", count);
-
         byte[] payload = null;
         try {
             payload = messageXML.getBytes();
