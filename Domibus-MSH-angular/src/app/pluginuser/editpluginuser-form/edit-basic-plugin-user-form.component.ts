8d9f5a5172225fd4b76944844c16af4f40d72563
ion.perpegel <ion.perpegel@gmail.com>
2019-12-27 17:57:04 +0200

    [EDELIVERY-6094] remove commented code

--- Domibus-MSH-angular/src/app/pluginuser/editpluginuser-form/edit-basic-plugin-user-form.component.ts
+++ Domibus-MSH-angular/src/app/pluginuser/editpluginuser-form/edit-basic-plugin-user-form.component.ts
@@ -1,46 +1,24 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {NgControl, NgForm} from '@angular/forms';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
-import {UserValidatorService} from '../../user/support/uservalidator.service';
-import {PluginUserRO} from '../support/pluginuser';
-import {PluginUserService} from '../support/pluginuser.service';
 import {SecurityService} from '../../security/security.service';
-import {UserState} from '../../user/support/user';
 import {EditPluginUserFormBaseComponent} from './edit-plugin-user-form-base.component';
-
-const NEW_MODE = 'New Plugin User';
-const EDIT_MODE = 'Plugin User Edit';
 
 @Component({
   selector: 'editbasicpluginuser-form',
   templateUrl: './edit-basic-plugin-user-form.component.html',
-  providers: [UserValidatorService]
 })
 
 export class EditBasicPluginUserFormComponent extends EditPluginUserFormBaseComponent implements OnInit {
 
-  // existingRoles = [];
-  // editMode: boolean;
-  // formTitle: string;
-  // user: PluginUserRO;
-
   passwordConfirmation: string;
   public passwordPattern: string;
   public passwordValidationMessage: string;
-
-  // public originalUserPattern = PluginUserService.originalUserPattern;
-  // public originalUserMessage = PluginUserService.originalUserMessage;
 
   constructor(public dialogRef: MatDialogRef<EditBasicPluginUserFormComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any,
               private securityService: SecurityService) {
 
     super(dialogRef, data);
-
-    // this.existingRoles = data.userroles;
-    // this.user = data.user;
-    // this.editMode = this.user.status !== UserState[UserState.NEW];
-    // this.formTitle = this.editMode ? EDIT_MODE : NEW_MODE;
 
     this.passwordConfirmation = data.user.password;
   }
@@ -50,20 +28,5 @@ export class EditBasicPluginUserFormComponent extends EditPluginUserFormBaseComp
     this.passwordPattern = passwordPolicy.pattern;
     this.passwordValidationMessage = passwordPolicy.validationMessage;
   }
-
-  // submitForm(userForm: NgForm) {
-  //   if (userForm.invalid) {
-  //     return;
-  //   }
-  //   this.dialogRef.close(true);
-  // }
-  //
-  // shouldShowErrors(field: NgControl | NgForm): boolean {
-  //   return (field.touched || field.dirty) && !!field.errors;
-  // }
-  //
-  // isFormDisabled(form: NgForm) {
-  //   return form.invalid || !form.dirty;
-  // }
 
 }
--- Domibus-MSH-angular/src/app/pluginuser/editpluginuser-form/edit-certificate-plugin-user-form.component.ts
+++ Domibus-MSH-angular/src/app/pluginuser/editpluginuser-form/edit-certificate-plugin-user-form.component.ts
@@ -1,14 +1,8 @@
 import {Component, Inject} from '@angular/core';
-import {NgControl, NgForm} from '@angular/forms';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 import {UserValidatorService} from '../../user/support/uservalidator.service';
-import {PluginUserRO} from '../support/pluginuser';
 import {PluginUserService} from '../support/pluginuser.service';
-import {UserState} from '../../user/support/user';
 import {EditPluginUserFormBaseComponent} from './edit-plugin-user-form-base.component';
-
-const NEW_MODE = 'New PluginUser';
-const EDIT_MODE = 'Plugin User Edit';
 
 @Component({
   selector: 'editcertificatepluginuser-form',
@@ -17,40 +11,12 @@ const EDIT_MODE = 'Plugin User Edit';
 })
 export class EditCertificatePluginUserFormComponent extends EditPluginUserFormBaseComponent {
 
-  // existingRoles = [];
-  // editMode: boolean;
-  // formTitle: string;
-  // user: PluginUserRO;
-
   public certificateIdPattern = PluginUserService.certificateIdPattern;
   public certificateIdMessage = PluginUserService.certificateIdMessage;
 
-  // public originalUserPattern = PluginUserService.originalUserPattern;
-  // public originalUserMessage = PluginUserService.originalUserMessage;
-
   constructor(public dialogRef: MatDialogRef<EditCertificatePluginUserFormComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any) {
-
     super(dialogRef, data);
-
-    // this.existingRoles = data.userroles;
-    // this.user = data.user;
-    // this.editMode = this.user.status !== UserState[UserState.NEW];
-    // this.formTitle = this.editMode ? EDIT_MODE : NEW_MODE;
   }
 
-  // submitForm(userForm: NgForm) {
-  //   if (userForm.invalid) {
-  //     return;
-  //   }
-  //   this.dialogRef.close(true);
-  // }
-  //
-  // shouldShowErrors(field: NgControl | NgForm): boolean {
-  //   return (field.touched || field.dirty) && !!field.errors;
-  // }
-  //
-  // isFormDisabled(form: NgForm) {
-  //   return form.invalid || !form.dirty;
-  // }
 }
