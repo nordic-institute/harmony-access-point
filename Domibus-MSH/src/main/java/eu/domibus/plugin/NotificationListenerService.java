1a50a9b86450a7a473a2b235776f1a59b8d03b41
hykiukira <hykiukira@gmail.com>
2020-01-16 21:01:40 +0100

    EDELIVERY:5573 add stat to InternalJmsMessager

--- Domibus-MSH/src/main/java/eu/domibus/plugin/NotificationListenerService.java
+++ Domibus-MSH/src/main/java/eu/domibus/plugin/NotificationListenerService.java
@@ -143,17 +143,25 @@ public class NotificationListenerService implements MessageListener, JmsListener
             switch (notificationType) {
                 case MESSAGE_RECEIVED:
                     com.codahale.metrics.Timer.Context deliverMessageMetric = metricRegistry.timer(MetricRegistry.name(NotificationListenerService.class, "on_message.RECEIVED.deliverMessage")).time();
+                    com.codahale.metrics.Counter c = metricRegistry.counter(MetricRegistry.name(NotificationListenerService.class, "on_message.MESSAGE_RECEIVED"));
                     backendConnector.deliverMessage(messageId);
+                    c.dec();
                     deliverMessageMetric.stop();
                     break;
                 case MESSAGE_SEND_FAILURE:
+                    c = metricRegistry.counter(MetricRegistry.name(NotificationListenerService.class, "on_message.MESSAGE_SEND_FAILURE"));
                     backendConnector.messageSendFailed(messageId);
+                    c.dec();
                     break;
                 case MESSAGE_SEND_SUCCESS:
+                    c = metricRegistry.counter(MetricRegistry.name(NotificationListenerService.class, "on_message.MESSAGE_SEND_SUCCESS"));
                     backendConnector.messageSendSuccess(messageId);
+                    c.dec();
                     break;
                 case MESSAGE_RECEIVED_FAILURE:
+                    c = metricRegistry.counter(MetricRegistry.name(NotificationListenerService.class, "on_message.MESSAGE_RECEIVED_FAILURE"));
                     doMessageReceiveFailure(message);
+                    c.dec();
                     break;
                /* case MESSAGE_STATUS_CHANGE:
                     doMessageStatusChange(message);
