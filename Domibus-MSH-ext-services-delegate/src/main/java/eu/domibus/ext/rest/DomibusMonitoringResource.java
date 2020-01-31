ac817b40e9d3c82f11f116d4afb0393dccfec476
soumya <Soumya.AZHIKODE-CHANDRAN@ext.ec.europa.eu>
2020-01-31 09:31:26 +0100

    [EDELIVERY-3531]- Adding swagger.json

--- Domibus-MSH-ext-services-delegate/src/main/java/eu/domibus/ext/rest/DomibusMonitoringResource.java
+++ Domibus-MSH-ext-services-delegate/src/main/java/eu/domibus/ext/rest/DomibusMonitoringResource.java
@@ -16,6 +16,7 @@ import java.util.List;
  * Monitoring Domibus.
  * Checking status of DB, JMS Broker and Quartz Triggers)
  * @author Soumya Chandran
+ * @since 4.2
  */
 @RestController
 @RequestMapping(value = "/ext/monitoring/application")
@@ -32,7 +33,7 @@ public class DomibusMonitoringResource {
      * @return MonitoringInfoDTO with the status of monitoring services specific to the filter
      * @throws DomibusMonitoringExtException Raised in case an exception occurs while trying to get the monitoring details {@link DomibusMonitoringExtException}
      */
-    @ApiOperation(value = "Check Domibus is Alive ", notes = "Show the accessibility and status of Domibus Database, JMS broker and Quartz Trigger",
+    @ApiOperation(value = "Check Domibus is Alive ", notes = "Shows the accessibility and status of Domibus Database, JMS broker and Quartz Trigger",
             authorizations = @Authorization(value = "basicAuth"), tags = "status")
     @RequestMapping(path = "status", method = RequestMethod.GET)
     public MonitoringInfoDTO getMonitoringDetails(@RequestParam(value = "filter", defaultValue = "all") List<String> filters) throws DomibusMonitoringExtException {
