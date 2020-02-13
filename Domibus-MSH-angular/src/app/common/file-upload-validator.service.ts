c7862b37ecb5912fe95ede836fb7a8f90d3255fd
ion.perpegel <ion.perpegel@gmail.com>
2020-02-13 17:52:37 +0200

    [EDELIVERY-4417] read maxSize property globally and rely on server validation if cannot read it

--- Domibus-MSH-angular/src/app/common/file-upload-validator.service.ts
+++ Domibus-MSH-angular/src/app/common/file-upload-validator.service.ts
@@ -8,14 +8,18 @@ export class FileUploadValidatorService {
   }
 
   public async validateSize(file: Blob): Promise<any> {
-    const limit = await this.getUploadSizeLimit();
-    if (file.size > limit) {
-      throw new Error('The file exceeds the maximum size limit.');
+    try {
+      const limit = await this.getUploadSizeLimit();
+      if (file.size > limit) {
+        throw new Error('The file exceeds the maximum size limit.');
+      }
+    } catch (e) {
+      //no error in case we cannot read property because the server will validate
     }
   }
 
   private async getUploadSizeLimit(): Promise<number> {
-    const res = await this.propertiesService.getProperties('domibus.file.upload.maxSize', true, 1, 0)
+    const res = await this.propertiesService.getProperties('domibus.file.upload.maxSize', false, 1, 0)
     return +res.items[0].value;
   }
 
