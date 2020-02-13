d489c110b8b99b922b5196a4f1bffdd438ae4823
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2020-02-13 16:48:45 +0100

    [EDELIVERY-6200] partial commit of changes

--- Domibus-MSH-logging/src/main/java/eu/domibus/logging/DomibusLoggingEventSender.java
+++ Domibus-MSH-logging/src/main/java/eu/domibus/logging/DomibusLoggingEventSender.java
@@ -1,12 +1,12 @@
 package eu.domibus.logging;
 
-import org.apache.cxf.ext.logging.AbstractLoggingInterceptor;
 import org.apache.cxf.ext.logging.event.LogEvent;
 import org.apache.cxf.ext.logging.event.LogEventSender;
 import org.apache.cxf.ext.logging.event.LogMessageFormatter;
 import org.apache.cxf.ext.logging.slf4j.Slf4jEventSender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * This class extends the default {@code Slf4jEventSender} implemented by Apache CXF
@@ -19,8 +19,6 @@ import org.slf4j.LoggerFactory;
 public class DomibusLoggingEventSender extends Slf4jEventSender implements LogEventSender {
 
     private static final Logger LOG = LoggerFactory.getLogger(DomibusLoggingEventSender.class);
-
-    static final String CONTENT_TYPE = "Content-Type:";
     private static final String ORG_APACHE_CXF_CATEGORY = "org.apache.cxf";
 
     private boolean printPayload;
@@ -29,44 +27,30 @@ public class DomibusLoggingEventSender extends Slf4jEventSender implements LogEv
         this.printPayload = printPayload;
     }
 
+    @Autowired
+    DomibusLoggingEventHelper domibusLoggingEventHelper;
+
     @Override
     protected String getLogMessage(LogEvent event) {
-        LOG.debug("printPayload=[{}]", printPayload);
-
-        boolean isCxfLoggingInfoEnabled = LoggerFactory.getLogger(ORG_APACHE_CXF_CATEGORY).isInfoEnabled();
-        LOG.debug("[{}] set to INFO=[{}]", ORG_APACHE_CXF_CATEGORY, isCxfLoggingInfoEnabled);
-
-        if (isCxfLoggingInfoEnabled && !printPayload) {
+        if (checkIfStripPayloadPossible()) {
             try {
-                stripPayload(event);
+                domibusLoggingEventHelper.stripPayload(event);
             } catch (RuntimeException e) {
                 LOG.error("Exception while stripping the payload: ", e);
             }
         }
-
         return LogMessageFormatter.format(event);
     }
 
-    /**
-     * It removes some parts of the payload info
-     *
-     * @param event
-     */
-    protected void stripPayload(LogEvent event) {
-        final String payload = event.getPayload();
-
-        //C2 -> C3
-        if (payload.contains(CONTENT_TYPE)) {
-            //if it's multipart and we want to strip the AS4 payload
-            if (event.isMultipartContent()) {
-
-                String[] payloadSplits = payload.split(CONTENT_TYPE);
-                //keeping only first 2 Content-Type elements
-                if (payloadSplits.length >= 2) {
-                    event.setPayload(payloadSplits[0] + CONTENT_TYPE + payloadSplits[1] + AbstractLoggingInterceptor.CONTENT_SUPPRESSED);
-                }
-            }
+    private boolean checkIfStripPayloadPossible() {
+        LOG.debug("printPayload=[{}]", printPayload);
+        if (printPayload) {
+            return false;
         }
-    }
 
+        boolean isCxfLoggingInfoEnabled = LoggerFactory.getLogger(ORG_APACHE_CXF_CATEGORY).isInfoEnabled();
+        LOG.debug("[{}] is set to INFO=[{}]", ORG_APACHE_CXF_CATEGORY, isCxfLoggingInfoEnabled);
+
+        return isCxfLoggingInfoEnabled;
+    }
 }
