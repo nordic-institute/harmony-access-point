4bfec51fd144ebf36a94d92a2ae033473b9449e4
Cosmin Baciu <cosmin.baciu@ext.ec.europa.eu>
2020-03-06 15:34:45 +0100

    [EDELIVERY-5457] - Migrate from Spring XML config to Java config using annotations

--- Domibus-MSH-jms-wildfly-artemis/src/test/java/eu/domibus/jms/wildfly/InternalJMSManagerWildFlyArtemisTest.java
+++ Domibus-MSH-jms-wildfly-artemis/src/test/java/eu/domibus/jms/wildfly/InternalJMSManagerWildFlyArtemisTest.java
@@ -24,7 +24,7 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static eu.domibus.jms.wildfly.InternalJMSManagerWildFlyArtemis.JMS_BROKER_PROPERTY;
+import static eu.domibus.api.property.DomibusPropertyMetadataManager.ACTIVE_MQ_ARTEMIS_BROKER;
 import static org.apache.activemq.artemis.api.core.SimpleString.toSimpleString;
 import static org.junit.Assert.assertEquals;
 
@@ -142,7 +142,7 @@ public class InternalJMSManagerWildFlyArtemisTest {
         };
 
         new Expectations(jmsManager) {{
-            domibusPropertyProvider.getProperty(JMS_BROKER_PROPERTY);
+            domibusPropertyProvider.getProperty(ACTIVE_MQ_ARTEMIS_BROKER);
             result = "localhost";
             activeMQServerControl.getAddressNames();
             result = addressNames;
@@ -204,7 +204,7 @@ public class InternalJMSManagerWildFlyArtemisTest {
         };
 
         new Expectations(jmsManager) {{
-            domibusPropertyProvider.getProperty(JMS_BROKER_PROPERTY);
+            domibusPropertyProvider.getProperty(ACTIVE_MQ_ARTEMIS_BROKER);
             result = "localhost";
             activeMQServerControl.getAddressNames();
             result = addressNames;
@@ -263,7 +263,7 @@ public class InternalJMSManagerWildFlyArtemisTest {
         };
 
         new Expectations(jmsManager) {{
-            domibusPropertyProvider.getProperty(JMS_BROKER_PROPERTY);
+            domibusPropertyProvider.getProperty(ACTIVE_MQ_ARTEMIS_BROKER);
             result = "localhost";
             activeMQServerControl.getAddressNames();
             result = addressNames;
