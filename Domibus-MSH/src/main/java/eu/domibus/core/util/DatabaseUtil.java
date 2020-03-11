cbd88c8bf259bab32b81b746e0f660ff227a2f3c
Cosmin Baciu <cosmin.baciu@ext.ec.europa.eu>
2020-03-11 09:36:40 +0100

    [EDELIVERY-5457] - Migrate from Spring XML config to Java config using annotations

--- Domibus-MSH/src/main/java/eu/domibus/core/util/DatabaseUtil.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/util/DatabaseUtil.java
@@ -4,6 +4,7 @@ import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -17,6 +18,7 @@ import java.sql.SQLException;
  * @author Sebastian-Ion TINCU
  * @since 4.2
  */
+@Lazy
 @Service
 public class DatabaseUtil {
 
