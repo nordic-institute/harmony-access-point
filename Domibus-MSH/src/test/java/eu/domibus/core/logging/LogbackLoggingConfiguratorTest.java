1a7bd84a0191a95093ed4029a22c27f683238eef
Cosmin Baciu <cosmin.baciu@ext.ec.europa.eu>
2020-03-19 14:47:29 +0100

    [EDELIVERY-5440] - Reverted logback configuration for tests

--- Domibus-MSH/src/test/java/eu/domibus/core/logging/LogbackLoggingConfiguratorTest.java
+++ Domibus-MSH/src/test/java/eu/domibus/core/logging/LogbackLoggingConfiguratorTest.java
@@ -35,7 +35,7 @@ public class LogbackLoggingConfiguratorTest {
     public void testConfigureLoggingWithCustomFile(@Mocked System mock) throws Exception {
         new Expectations(logbackLoggingConfigurator) {{
             logbackLoggingConfigurator.getDefaultLogbackConfigurationFile();
-            result = "/user/logback.xml";
+            result = "/user/logback-test.xml";
 
             System.getProperty(anyString);
             result = "/user/mylogback.xml";
@@ -56,7 +56,7 @@ public class LogbackLoggingConfiguratorTest {
     public void testConfigureLoggingWithTheDefaultLogbackConfigurationFile(@Mocked System mock) throws Exception {
         new Expectations(logbackLoggingConfigurator) {{
             logbackLoggingConfigurator.getDefaultLogbackConfigurationFile();
-            result = "/user/logback.xml";
+            result = "/user/logback-test.xml";
 
             System.getProperty(anyString);
             result = null;
@@ -69,7 +69,7 @@ public class LogbackLoggingConfiguratorTest {
             logbackLoggingConfigurator.configureLogging(fileLocation = withCapture());
             times = 1;
 
-            Assert.assertEquals("/user/logback.xml", fileLocation);
+            Assert.assertEquals("/user/logback-test.xml", fileLocation);
         }};
     }
 
@@ -90,7 +90,7 @@ public class LogbackLoggingConfiguratorTest {
             result = false;
         }};
 
-        logbackLoggingConfigurator.configureLogging("/user/logback.xml");
+        logbackLoggingConfigurator.configureLogging("/user/logback-test.xml");
 
         new Verifications() {{
             logbackLoggingConfigurator.configureLogback(anyString);
@@ -108,7 +108,7 @@ public class LogbackLoggingConfiguratorTest {
             result = null;
         }};
 
-        logbackLoggingConfigurator.configureLogging("/user/logback.xml");
+        logbackLoggingConfigurator.configureLogging("/user/logback-test.xml");
 
         new Verifications() {{
             logbackLoggingConfigurator.configureLogback(anyString);
