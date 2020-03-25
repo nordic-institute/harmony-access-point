6708c35d20f0c1fb26c5d291120c402623f012f4
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2020-03-25 17:09:26 +0100

    [EDELIVERY-6341] added In and Out interceptors for msh service

---
+++ Domibus-MSH/src/main/java/eu/domibus/core/ebms3/sender/interceptor/HttpHeaderInInterceptor.java
@@ -0,0 +1,23 @@
+package eu.domibus.core.ebms3.sender.interceptor;
+
+import eu.domibus.logging.DomibusLogger;
+import eu.domibus.logging.DomibusLoggerFactory;
+import org.apache.cxf.ext.logging.LoggingInInterceptor;
+import org.apache.cxf.phase.Phase;
+
+/**
+ * @since 4.2
+ * @author Catalin Enache
+ */
+public class HttpHeaderInInterceptor extends HttpHeaderOutInterceptor {
+
+    public HttpHeaderInInterceptor() {
+        super(Phase.PRE_INVOKE);
+        addBefore(LoggingInInterceptor.class.getName());
+    }
+
+    @Override
+    protected DomibusLogger getLogger(){
+        return DomibusLoggerFactory.getLogger(HttpHeaderInInterceptor.class);
+    }
+}
---
+++ Domibus-MSH/src/main/java/eu/domibus/core/ebms3/sender/interceptor/HttpHeaderOutInterceptor.java
@@ -0,0 +1,61 @@
+package eu.domibus.core.ebms3.sender.interceptor;
+
+import eu.domibus.logging.DomibusLogger;
+import eu.domibus.logging.DomibusLoggerFactory;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.cxf.ext.logging.LoggingOutInterceptor;
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * It will remove user-agent entry from Http headers if contains Apache CXF version
+ *
+ * @author Catalin Enache
+ * @since 4.2
+ */
+public class HttpHeaderOutInterceptor extends AbstractPhaseInterceptor<Message> {
+
+    public static final String USER_AGENT_HTTP_HEADER_KEY = "user-agent";
+    public static final String USER_AGENT_HTTP_HEADER_VALUE_APACHE_CXF = "Apache-CXF";
+
+
+    public HttpHeaderOutInterceptor() {
+        super(Phase.PRE_STREAM);
+        addBefore(LoggingOutInterceptor.class.getName());
+    }
+
+    public HttpHeaderOutInterceptor(String phase) {
+        super(phase);
+    }
+
+    @Override
+    public void handleMessage(Message message) throws Fault {
+        //get the headers
+        Map<String, List<String>> headers = (Map<String, List<String>>) message.get(Message.PROTOCOL_HEADERS);
+
+        if (headers == null) {
+            getLogger().debug("no http headers to intercept");
+            return;
+        }
+
+        boolean removed = headers.entrySet()
+                .removeIf(e -> USER_AGENT_HTTP_HEADER_KEY.equalsIgnoreCase(e.getKey())
+                        && StringUtils.containsIgnoreCase(Arrays.deepToString(e.getValue().toArray()), USER_AGENT_HTTP_HEADER_VALUE_APACHE_CXF)
+                );
+
+        getLogger().debug("httpHeader=[{}] {}", USER_AGENT_HTTP_HEADER_KEY, (removed ? " was successfully removed" : " not present or value not removed"));
+
+        //logging of the remaining headers
+        headers.forEach((key, value) -> getLogger().debug("httpHeader=[{}] value=[{}]", key, Arrays.deepToString(value.toArray())));
+    }
+
+    protected DomibusLogger getLogger(){
+        return DomibusLoggerFactory.getLogger(HttpHeaderOutInterceptor.class);
+    }
+}
