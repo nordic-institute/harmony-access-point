571ac78f0edf2224e251e184e268de62fc5f685f
Ion PERPEGEL <ion.perpegel@gmail.com>
2020-03-30 12:55:44 +0300

    [EDELIVERY-5467] add documentation

--- Domibus-MSH-angular/src/app/common/application.service.ts
+++ Domibus-MSH-angular/src/app/common/application.service.ts
@@ -1,6 +1,12 @@
 import {Injectable} from '@angular/core';
 import 'rxjs/add/operator/map';
 
+/**
+ * @author Ion Perpegel
+ * @since 4.2
+ *
+ * Service meant to keep the current component in the outlet(set by the framework)
+ */
 @Injectable()
 export class ApplicationService {
   private currentComponent: any;
