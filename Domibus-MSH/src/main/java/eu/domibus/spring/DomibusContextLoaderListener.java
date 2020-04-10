d3cc085b8fe24ab7bffdeb8f57184a98f50f10b0
Sebastian-Ion TINCU <Sebastian-Ion.TINCU@ext.ec.europa.eu>
2020-04-10 15:10:03 +0200

    EDELIVERY-6380 Domibus 4.1.2 and 4.1.3 does not use BouncyCastle provider in WebLogic
    Add configuration for setting up the BouncyCastleProvider at startup.

--- Domibus-MSH/src/main/java/eu/domibus/spring/DomibusContextLoaderListener.java
+++ Domibus-MSH/src/main/java/eu/domibus/spring/DomibusContextLoaderListener.java
@@ -8,13 +8,18 @@ import eu.domibus.logging.DomibusLoggerFactory;
 import eu.domibus.plugin.classloader.PluginClassLoader;
 import eu.domibus.core.property.PropertyResolver;
 import org.apache.commons.lang3.StringUtils;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.web.context.ContextLoaderListener;
 
+import javax.crypto.Cipher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+import java.util.Arrays;
 import java.util.Set;
 
 /**
@@ -24,10 +29,20 @@ public class DomibusContextLoaderListener extends ContextLoaderListener {
 
     private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(DomibusContextLoaderListener.class);
 
+    // As recommended, make sure that the Sun security provider remains at a higher preference (i.e. index 2 on Weblogic)
+    private static final int HIGHEST_RECOMMENDED_POSITION_IN_ORDER_OF_PREFERENCES = 3;
+
+    private static final int LIMITED_STRENGTH_MAX_KEY_LENGTH = 128;
+
+    private static final int UNLIMITED_STRENGTH_MAX_KEY_LENGTH = Integer.MAX_VALUE;
+
     PluginClassLoader pluginClassLoader = null;
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
+        registerBouncyCastle();
+        checkStrengthJurisdictionPolicyLevel();
+
         ServletContext servletContext = servletContextEvent.getServletContext();
         String pluginsLocation = servletContext.getInitParameter("pluginsLocation");
         String extensionsLocation = servletContext.getInitParameter("extensionsLocation");
@@ -52,6 +67,30 @@ public class DomibusContextLoaderListener extends ContextLoaderListener {
         super.contextInitialized(servletContextEvent);
     }
 
+    protected void registerBouncyCastle() {
+        LOG.info("Registering the Bouncy Castle provider as the third highest preferred security provider");
+        try {
+            Security.insertProviderAt(new BouncyCastleProvider(), HIGHEST_RECOMMENDED_POSITION_IN_ORDER_OF_PREFERENCES);
+            LOG.debug("Security providers in order of preferences [{}]", Arrays.toString(Security.getProviders()));
+        } catch (SecurityException e) {
+            LOG.error("An error registering Bouncy Castle provider as the second highest preferred security provider", e);
+        }
+    }
+
+    protected void checkStrengthJurisdictionPolicyLevel() {
+        int maxKeyLen = 0;
+        try {
+            maxKeyLen = Cipher.getMaxAllowedKeyLength("AES");
+        } catch (NoSuchAlgorithmException e) { /*ignore*/ }
+
+        LOG.info("Using {} strength jurisdiction policy: maxKeyLen=[{}]",
+                maxKeyLen == LIMITED_STRENGTH_MAX_KEY_LENGTH
+                        ? "Limited"
+                        : maxKeyLen == UNLIMITED_STRENGTH_MAX_KEY_LENGTH
+                        ? "Unlimited"
+                        : "Unknown",
+                maxKeyLen);
+    }
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
         super.contextDestroyed(servletContextEvent);
