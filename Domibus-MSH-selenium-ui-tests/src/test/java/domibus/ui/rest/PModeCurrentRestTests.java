7139d66d9e60553fa6be7531b22d332aff2b9388
comanca <Catalin.Comanici@ec.europa.eu>
2020-04-21 17:10:28 +0200

    tmp to store changes 2

---
+++ Domibus-MSH-selenium-ui-tests/src/test/java/domibus/ui/rest/PModeCurrentRestTests.java
@@ -0,0 +1,22 @@
+package domibus.ui.rest;
+
+import com.sun.jersey.api.client.ClientResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import org.testng.collections.Lists;
+import rest.RestServicePaths;
+import utils.TestUtils;
+
+import java.io.IOException;
+import java.util.*;
+
+public class PModeCurrentRestTests extends RestTest {
+
+
+
+}
