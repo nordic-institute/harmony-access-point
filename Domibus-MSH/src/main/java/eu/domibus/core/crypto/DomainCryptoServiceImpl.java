c7cc6d9a0770fd2dc2ce172aeadd7c0b280ded53
François Gautier <francois.gauti3r@gmail.com>
2020-05-05 16:39:44 +0200

    [EDELIVERY-6490] JMS event in DLQ Code review: make the Transaction with propagation=SUPPORT

--- Domibus-MSH/src/main/java/eu/domibus/core/crypto/DomainCryptoServiceImpl.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/crypto/DomainCryptoServiceImpl.java
@@ -20,6 +20,7 @@ import eu.domibus.logging.DomibusLoggerFactory;
 import org.apache.wss4j.common.crypto.CryptoType;
 import org.apache.wss4j.common.ext.WSSecurityException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
@@ -193,7 +194,7 @@ public class DomainCryptoServiceImpl implements DomainCryptoService {
 
 
     @Override
-    @Transactional(noRollbackFor = DomibusCertificateException.class)
+    @Transactional(noRollbackFor = DomibusCertificateException.class, propagation = Propagation.SUPPORTS)
     public boolean isCertificateChainValid(String alias) throws DomibusCertificateException {
         return iamProvider.isCertificateChainValid(alias);
     }
