041db62b2ad95eaeb8e3eda5a7fc8c1e79680520
comanca <Catalin.Comanici@ec.europa.eu>
2020-05-08 16:08:24 +0200

    connection monitor 1st draft

---
+++ Domibus-MSH-selenium-ui-tests/src/test/java/domibus/ui/rest/ConnectionMonitoringRestTests.java
@@ -0,0 +1,29 @@
+package domibus.ui.rest;
+
+import com.sun.jersey.api.client.ClientResponse;
+import org.json.JSONArray;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import org.w3c.dom.Document;
+import rest.RestServicePaths;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ConnectionMonitoringRestTests extends RestTest {
+
+//	@Test //(dataProvider = "readInvalidStrings")
+//	public void getPartiesTest() throws Exception {
+//		SoftAssert soft = new SoftAssert();
+//
+//		rest.pmode().uploadPMode("/pmodes/pmode-blue.xml", null);
+//
+//
+//		soft.assertAll();
+//	}
+//
+//	private List<String> getPartiesFromPMODE(String pmodeFilePath){
+//		Document
+//	}
+
+}
