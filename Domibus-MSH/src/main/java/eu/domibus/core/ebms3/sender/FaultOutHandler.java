65b302b20139ba12401ddbcfe3cd4d63233af7b0
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2020-06-02 17:15:56 +0200

    [EDELIVERY-3551] first version of the code

--- Domibus-MSH/src/main/java/eu/domibus/core/ebms3/sender/FaultOutHandler.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/ebms3/sender/FaultOutHandler.java
@@ -1,15 +1,19 @@
 package eu.domibus.core.ebms3.sender;
 
 import eu.domibus.common.MSHRole;
+import eu.domibus.core.ebms3.ws.handler.AbstractFaultHandler;
 import eu.domibus.core.error.ErrorLogDao;
 import eu.domibus.core.error.ErrorLogEntry;
-import eu.domibus.core.ebms3.ws.handler.AbstractFaultHandler;
+import eu.domibus.core.util.SoapUtil;
 import eu.domibus.ebms3.common.model.Messaging;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.transform.TransformerException;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
 import java.util.Collections;
@@ -26,6 +30,9 @@ public class FaultOutHandler extends AbstractFaultHandler {
 
     @Autowired
     private ErrorLogDao errorLogDao;
+
+    @Autowired
+    private SoapUtil soapUtil;
 
     @Override
     public Set<QName> getHeaders() {
@@ -45,9 +52,25 @@ public class FaultOutHandler extends AbstractFaultHandler {
     @Override
     public boolean handleFault(final SOAPMessageContext context) {
 
-        final Messaging messaging = this.extractMessaging(context.getMessage());
 
-        FaultOutHandler.LOG.debug("An ebMS3 error was received for message with ebMS3 messageId:" + messaging.getSignalMessage().getMessageInfo().getMessageId() + ". Please check the database for more detailed information.");
+        final SOAPMessage soapMessage = context.getMessage();
+        final Messaging messaging = this.extractMessaging(soapMessage);
+        final String messageId = messaging.getSignalMessage().getMessageInfo().getMessageId();
+
+        if (LOG.isErrorEnabled()) {
+            String xmlMessage = null;
+            try {
+                xmlMessage = soapUtil.getRawXMLMessage(soapMessage);
+            } catch (TransformerException e) {
+                LOG.warn("Unable to extract the raw message XML due to: ", e);
+            }
+            if (StringUtils.isNotBlank( xmlMessage)) {
+                LOG.error("An ebMS3 error was received: {}", System.lineSeparator() + xmlMessage);
+            }
+        }
+
+        //save to database
+        LOG.debug("An ebMS3 error was received for message with ebMS3 messageId [{}]. Please check the database for more detailed information.", messageId);
         this.errorLogDao.create(ErrorLogEntry.parse(messaging, MSHRole.SENDING));
 
         return true;
