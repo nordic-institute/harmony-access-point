6fffa335208463d8141c325e0a6ed2fa97439466
ion.perpegel <ion.perpegel@gmail.com>
2020-06-09 17:20:19 +0300

    [EDELIVERY-3901] pmode validation - add warnings for these situations : mep=oneway and binding=pushAndPush or pushAndPull or pullAndPush (doesn't make sense) ; mep=twoway and binding=pushAndPush or pushAndPull or pullAndPush (not implemented in Domibus); mep=twoway and binding=push or pull (doesn't make sense)

--- Domibus-MSH/src/main/java/eu/domibus/core/pmode/validation/validators/OneWayMepValidator.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/pmode/validation/validators/OneWayMepValidator.java
@@ -12,25 +12,21 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static eu.domibus.core.ebms3.Ebms3Constants.TWOWAY_MEP_VALUE;
+import static eu.domibus.core.ebms3.Ebms3Constants.ONEWAY_MEP_VALUE;
 
 /**
  * @author Ion Perpegel
  * @since 4.2
  * <p>
- * Two-Way mep with binding pushAndPush, pullAndPush and pushAndPull are not supported.
- * (These bindings are simulated in Domibus with two One-Way exchanges).
- * <p>
- * Two-Way mep with any other binding is not valid.
- * <p>
- * Both these situations generate warnings, not errors, for backward compatibility.
+ * One-Way mep with binding pushAndPush, pullAndPush and pushAndPull is not valid.
+ * This combination generates a warning, not an error, for backward compatibility.
  * <p>
  */
 @Component
 @Order(6)
-public class TwoWayMepValidator implements PModeValidator {
+public class OneWayMepValidator implements PModeValidator {
 
-    private static final List<String> notSupported = Arrays.asList(
+    private static final List<String> notAccepted = Arrays.asList(
             MessageExchangePattern.TWO_WAY_PUSH_PUSH.getUri(),
             MessageExchangePattern.TWO_WAY_PUSH_PULL.getUri(),
             MessageExchangePattern.TWO_WAY_PULL_PUSH.getUri()
@@ -41,15 +37,11 @@ public class TwoWayMepValidator implements PModeValidator {
         List<ValidationIssue> issues = new ArrayList<>();
 
         configuration.getBusinessProcesses().getProcesses().forEach(process -> {
-            if (process.getMep() != null && TWOWAY_MEP_VALUE.equalsIgnoreCase(process.getMep().getValue())) {
+            if (process.getMep() != null && ONEWAY_MEP_VALUE.equalsIgnoreCase(process.getMep().getValue())) {
                 String binding = process.getMepBinding() == null ? null : process.getMepBinding().getValue();
                 if (binding != null) {
-                    if (notSupported.stream().anyMatch(binding::equalsIgnoreCase)) {
-                        String message = String.format("Two-way mep with binding [%s] is not supported for process [%s], use 2 one-way processes to simulate two-way communication.",
-                                process.getMepBinding().getName(), process.getName());
-                        issues.add(new ValidationIssue(message, ValidationIssue.Level.WARNING));
-                    } else {
-                        String message = String.format("Two-way mep with binding [%s] is invalid for process [%s].",
+                    if (notAccepted.stream().anyMatch(binding::equalsIgnoreCase)) {
+                        String message = String.format("One-way mep with binding [%s] is not valid for process [%s].",
                                 process.getMepBinding().getName(), process.getName());
                         issues.add(new ValidationIssue(message, ValidationIssue.Level.WARNING));
                     }
