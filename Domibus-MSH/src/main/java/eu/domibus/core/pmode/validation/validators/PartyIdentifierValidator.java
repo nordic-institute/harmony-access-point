10ab7615adbee0e37f8cea45eaa61ebf14faef92
soumya <Soumya.AZHIKODE-CHANDRAN@ext.ec.europa.eu>
2020-06-24 17:22:53 +0200

    [EDELIVERY-6649]- review comments fix

--- Domibus-MSH/src/main/java/eu/domibus/core/pmode/validation/validators/PartyIdentifierValidator.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/pmode/validation/validators/PartyIdentifierValidator.java
@@ -58,7 +58,7 @@ public class PartyIdentifierValidator implements PModeValidator {
         Set<Identifier> duplicateIdentifiers = party.getIdentifiers().stream()
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet().stream()
-                .filter(identifier -> identifier.getValue() > 1)
+                .filter(map -> map.getValue() > 1)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toSet());
 
