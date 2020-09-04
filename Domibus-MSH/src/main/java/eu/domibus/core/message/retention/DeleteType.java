8fda19cfed7c8925de7bf483f0aa6a807afce541
Ioana DRAGUSANU <idragusa@gmail.com>
2020-09-04 10:51:45 +0300

    [EDELIVERY-6693] Add author

--- Domibus-MSH/src/main/java/eu/domibus/core/message/retention/DeleteType.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/message/retention/DeleteType.java
@@ -1,5 +1,9 @@
 package eu.domibus.core.message.retention;
 
+/**
+ * @author idragusa
+ * @since 4.2
+ */
 public enum DeleteType {
     DELETE_MESSAGE_ID_SINGLE("DELETE_MESSAGE_ID_SINGLE"),
     DELETE_MESSAGE_ID_MULTI("DELETE_MESSAGE_ID_MULTI");
