de83f3a87e1bc9591f430f73eabdd6f87c6b5f83
Ioana DRAGUSANU <idragusa@gmail.com>
2020-09-08 12:11:42 +0300

    [EDELIVERY-6693] Address review comments 2

--- Domibus-MSH/src/main/java/eu/domibus/core/message/retention/MessageDeleteType.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/message/retention/MessageDeleteType.java
@@ -5,16 +5,6 @@ package eu.domibus.core.message.retention;
  * @since 4.2
  */
 public enum MessageDeleteType {
-    DELETE_MESSAGE_ID_SINGLE("DELETE_MESSAGE_ID_SINGLE"),
-    DELETE_MESSAGE_ID_MULTI("DELETE_MESSAGE_ID_MULTI");
-
-    private String type;
-
-    MessageDeleteType(String type) {
-        this.type = type;
-    }
-
-    public String getType() {
-        return type;
-    }
+    DELETE_MESSAGE_ID_SINGLE,
+    DELETE_MESSAGE_ID_MULTI,
 }
