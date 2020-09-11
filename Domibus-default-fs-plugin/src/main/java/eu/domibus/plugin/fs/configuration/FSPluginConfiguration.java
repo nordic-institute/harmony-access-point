8d86950b89c3f6e7a5e1beeef5246ada76a2f3cf
Cosmin Baciu <cosmin.baciu@ext.ec.europa.eu>
2020-09-11 14:04:45 +0200

    [EDELIVERY-6420] - Fixed circular dependency

--- Domibus-default-fs-plugin/src/main/java/eu/domibus/plugin/fs/configuration/FSPluginConfiguration.java
+++ Domibus-default-fs-plugin/src/main/java/eu/domibus/plugin/fs/configuration/FSPluginConfiguration.java
@@ -1,12 +1,14 @@
 package eu.domibus.plugin.fs.configuration;
 
-import eu.domibus.ext.services.DomibusConfigurationExtService;
+import eu.domibus.common.MessageStatus;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
 import eu.domibus.plugin.environment.DomibusEnvironmentUtil;
-import eu.domibus.plugin.fs.BackendFSImpl;
+import eu.domibus.plugin.fs.*;
+import eu.domibus.plugin.fs.ebms3.ObjectFactory;
 import eu.domibus.plugin.notification.PluginAsyncNotificationConfiguration;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,8 +18,11 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.jms.Queue;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -32,6 +37,9 @@ public class FSPluginConfiguration {
     private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(FSPluginConfiguration.class);
 
     public static final String NOTIFY_BACKEND_QUEUE_JNDI = "jms/domibus.notification.filesystem";
+
+    @Value("file:///${domibus.config.location}/plugins/config/fs-plugin.properties")
+    protected String fsPluginExternalPropertiesFile;
 
     @Bean("backendFSPlugin")
     public BackendFSImpl createFSPlugin() {
@@ -52,7 +60,7 @@ public class FSPluginConfiguration {
     }
 
     @Bean("fsPluginProperties")
-    public PropertiesFactoryBean fsPluginProperties(DomibusConfigurationExtService domibusConfigurationExtService) throws IOException {
+    public PropertiesFactoryBean fsPluginProperties() throws IOException {
         PropertiesFactoryBean result = new PropertiesFactoryBean();
         result.setIgnoreResourceNotFound(true);
 
@@ -60,13 +68,45 @@ public class FSPluginConfiguration {
         resources.add(new ClassPathResource("config/fs-plugin.properties"));
 
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        String domibusConfigLocation = domibusConfigurationExtService.getConfigLocation();
-        String location = "file:///" + domibusConfigLocation + "/plugins/config/fs-plugin.properties";
-        LOG.debug("Resolving resource [{}]", location);
-        Resource domibusProperties = resolver.getResource(location);
+        LOG.debug("Using FSPlugin external properties file [{}]", fsPluginExternalPropertiesFile);
+        Resource domibusProperties = resolver.getResource(fsPluginExternalPropertiesFile);
         resources.add(domibusProperties);
 
         result.setLocations(resources.toArray(new Resource[0]));
+        return result;
+    }
+
+    @Bean("fsPluginJaxbContext")
+    public JAXBContext jaxbContext() throws JAXBException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
+        return jaxbContext;
+    }
+
+    @Bean
+    public FSXMLHelper fsxmlHelper(@Qualifier("fsPluginJaxbContext") JAXBContext jaxbContext) {
+        FSXMLHelperImpl result = new FSXMLHelperImpl(jaxbContext);
+        return result;
+    }
+
+    @Bean
+    public FSMimeTypeHelper fsMimeTypeHelper() {
+        FSMimeTypeHelperImpl result = new FSMimeTypeHelperImpl();
+        return result;
+    }
+
+    @Bean
+    public FSFileNameHelper fsFileNameHelper() {
+        List<String> stateSuffixes = getStateSuffixes();
+        LOG.debug("Using state suffixes [{}]", stateSuffixes);
+        FSFileNameHelper result = new FSFileNameHelper(stateSuffixes);
+        return result;
+    }
+
+    public List<String> getStateSuffixes() {
+        List<String> result = new LinkedList<>();
+        for (MessageStatus status : MessageStatus.values()) {
+            result.add(FSFileNameHelper.EXTENSION_SEPARATOR + status.name());
+        }
         return result;
     }
 
