de3e571e5810f8169f58d77b8abefcce9e2ff394
Ion PERPEGEL <ion.perpegel@gmail.com>
2020-09-14 11:26:22 +0300

    [EDELIVERY-4398] replace @Size constraint validation with a custom one to address the scenario where the field is null( certificate users name)

--- Domibus-MSH/src/main/java/eu/domibus/web/rest/validators/SizeIfPresent.java
+++ Domibus-MSH/src/main/java/eu/domibus/web/rest/validators/SizeIfPresent.java
@@ -1,7 +1,5 @@
 package eu.domibus.web.rest.validators;
 
-import eu.domibus.core.rest.validators.FieldBlacklistValidator;
-
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.Documented;
@@ -9,27 +7,27 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Annotation for custom validator that checks that the value does not contain any char from the blacklist
  *
  * @author Ion Perpegel
- * since 4.1
+ * since 4.2
  */
-@Target({FIELD, PARAMETER})
+@Target({FIELD})
 @Retention(RUNTIME)
-@Constraint(validatedBy = FieldBlacklistValidator.class)
+@Constraint(validatedBy = SizeIfPresentValidator.class)
 @Documented
-public @interface FieldWhiteListed {
+public @interface SizeIfPresent {
 
-    static String MESSAGE = "Forbidden character detected.";
-
-    String message() default MESSAGE;
+    String message() default "{javax.validation.constraints.Size.message}";
 
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
 
+    int min() default 0;
+
+    int max() default 2147483647;
 }
--- Domibus-MSH/src/main/java/eu/domibus/web/rest/validators/SizeIfPresentValidator.java
+++ Domibus-MSH/src/main/java/eu/domibus/web/rest/validators/SizeIfPresentValidator.java
@@ -1,2 +1,30 @@
-package eu.domibus.web.rest.validators;public class SizeIfPresentValidator {
+package eu.domibus.web.rest.validators;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class SizeIfPresentValidator implements ConstraintValidator<SizeIfPresent, String> {
+
+    private SizeIfPresent constraintAnnotation;
+
+    @Override
+    public void initialize(SizeIfPresent constraintAnnotation) {
+        this.constraintAnnotation = constraintAnnotation;
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        if (StringUtils.isEmpty(value)) {
+            return true;
+        }
+        if (value.length() < constraintAnnotation.min()) {
+            return false;
+        }
+        if (value.length() > constraintAnnotation.max()) {
+            return false;
+        }
+        return true;
+    }
 }
