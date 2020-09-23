4228a8d978fbada079e89c07c842818a328a9cdc
Joze RIHTARSIC <joze.rihtarsic@ext.ec.europa.eu>
2020-09-23 18:59:54 +0200

    add audit to update truststore action

--- Domibus-MSH/src/test/java/eu/domibus/core/audit/AuditServiceImplTest.java
+++ Domibus-MSH/src/test/java/eu/domibus/core/audit/AuditServiceImplTest.java
@@ -17,6 +17,7 @@ import org.junit.runner.RunWith;
 import org.mockito.*;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -61,7 +62,7 @@ public class AuditServiceImplTest {
     public void listAuditTarget() throws Exception {
         List<String> targets = auditService.listAuditTarget();
         targets.stream().forEach(System.out::println);
-        assertEquals(7, targets.size());
+        assertEquals(8, targets.size());
         assertTrue(targets.contains("User"));
         assertTrue(targets.contains("Pmode"));
         assertTrue(targets.contains("Pmode Archive"));
@@ -69,6 +70,7 @@ public class AuditServiceImplTest {
         assertTrue(targets.contains("Message filter"));
         assertTrue(targets.contains("Jms message"));
         assertTrue(targets.contains("PluginUser"));
+        assertTrue(targets.contains("Certificate"));
     }
 
     @Test
