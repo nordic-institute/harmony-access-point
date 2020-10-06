a5151843d9dd39caac94c660f802f481af061f87
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2020-10-06 10:24:13 +0200

    [EDELIVERY-7032] added method to print Messaging object

--- Domibus-MSH/src/main/java/eu/domibus/core/message/SoapServiceImpl.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/message/SoapServiceImpl.java
@@ -3,12 +3,12 @@ package eu.domibus.core.message;
 import eu.domibus.common.ErrorCode;
 import eu.domibus.core.ebms3.EbMS3Exception;
 import eu.domibus.core.util.MessageUtil;
+import eu.domibus.core.util.SoapUtil;
 import eu.domibus.ebms3.common.model.Messaging;
 import eu.domibus.ebms3.common.model.ObjectFactory;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
 import org.apache.cxf.binding.soap.SoapMessage;
-import org.apache.cxf.helpers.IOUtils;
 import org.apache.cxf.interceptor.StaxInInterceptor;
 import org.apache.neethi.builders.converters.StaxToDOMConverter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,10 +18,13 @@ import org.w3c.dom.Node;
 
 import javax.xml.soap.SOAPException;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.TransformerException;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+
+import static org.apache.cxf.helpers.IOUtils.copy;
 
 
 /**
@@ -37,11 +40,14 @@ public class SoapServiceImpl implements SoapService {
     @Autowired
     protected MessageUtil messageUtil;
 
+    @Autowired
+    protected SoapUtil soapUtil;
+
 
     public Messaging getMessage(final SoapMessage message) throws IOException, EbMS3Exception {
         final InputStream inputStream = message.getContent(InputStream.class);
         final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        IOUtils.copy(inputStream, byteArrayOutputStream); //FIXME: do not copy the whole byte[], use SequenceInputstream instead
+        copy(inputStream, byteArrayOutputStream); //FIXME: do not copy the whole byte[], use SequenceInputstream instead
         final byte[] data = byteArrayOutputStream.toByteArray();
         message.setContent(InputStream.class, new ByteArrayInputStream(data));
         new StaxInInterceptor().handleMessage(message);
@@ -52,7 +58,6 @@ public class SoapServiceImpl implements SoapService {
         final Element soapEnvelope = new StaxToDOMConverter().convert(xmlStreamReader);
         message.removeContent(XMLStreamReader.class);
         message.setContent(InputStream.class, new ByteArrayInputStream(data));
-        //message.setContent(XMLStreamReader.class, XMLInputFactory.newInstance().createXMLStreamReader(message.getContent(InputStream.class)));
         final Node messagingNode = soapEnvelope.getElementsByTagNameNS(ObjectFactory._Messaging_QNAME.getNamespaceURI(), ObjectFactory._Messaging_QNAME.getLocalPart()).item(0);
         if (messagingNode == null) {
             throw new EbMS3Exception(ErrorCode.EbMS3ErrorCode.EBMS_0009, "Messaging header is empty!", null, null);
@@ -64,6 +69,26 @@ public class SoapServiceImpl implements SoapService {
             LOG.error("Error unmarshalling Messaging header", e);
             throw new EbMS3Exception(ErrorCode.EbMS3ErrorCode.EBMS_0009, "Messaging header is empty!", null, e);
         }
+    }
+
+    public String getMessagingAsRAWXml(final SoapMessage message) throws IOException, EbMS3Exception, TransformerException {
+        final InputStream inputStream = message.getContent(InputStream.class);
+        final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        copy(inputStream, byteArrayOutputStream);
+        final byte[] data = byteArrayOutputStream.toByteArray();
+        message.setContent(InputStream.class, new ByteArrayInputStream(data));
+        new StaxInInterceptor().handleMessage(message);
+        final XMLStreamReader xmlStreamReader = message.getContent(XMLStreamReader.class);
+        if (xmlStreamReader == null) {
+            throw new EbMS3Exception(ErrorCode.EbMS3ErrorCode.EBMS_0009, "Messaging header is missing!", null, null);
+        }
+
+        final Element soapEnvelope = new StaxToDOMConverter().convert(xmlStreamReader);
+        message.removeContent(XMLStreamReader.class);
+        message.setContent(InputStream.class, new ByteArrayInputStream(data));
+        final Node messagingNode = soapEnvelope.getElementsByTagNameNS(ObjectFactory._Messaging_QNAME.getNamespaceURI(), ObjectFactory._Messaging_QNAME.getLocalPart()).item(0);
+
+        return soapUtil.getRawXMLMessage(messagingNode);
     }
 }
 
