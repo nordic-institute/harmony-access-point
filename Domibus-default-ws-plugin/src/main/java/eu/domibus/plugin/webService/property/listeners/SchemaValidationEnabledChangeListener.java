c9ba33b572009e762c48e166dec438e927380083
François Gautier <francois.gauti3r@gmail.com>
2020-10-13 10:28:23 +0200

    [EDELIVERY-7322] Endpoint injection should use qualifier

--- Domibus-default-ws-plugin/src/main/java/eu/domibus/plugin/webService/property/listeners/SchemaValidationEnabledChangeListener.java
+++ Domibus-default-ws-plugin/src/main/java/eu/domibus/plugin/webService/property/listeners/SchemaValidationEnabledChangeListener.java
@@ -4,12 +4,11 @@ import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
 import eu.domibus.plugin.property.PluginPropertyChangeListener;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import javax.xml.ws.Endpoint;
 
-import static eu.domibus.plugin.webService.property.WSPluginPropertyManager.MTOM_ENABLED_PROPERTY;
 import static eu.domibus.plugin.webService.property.WSPluginPropertyManager.SCHEMA_VALIDATION_ENABLED_PROPERTY;
 
 /**
@@ -23,9 +22,9 @@ public class SchemaValidationEnabledChangeListener implements PluginPropertyChan
 
     private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(SchemaValidationEnabledChangeListener.class);
 
-    private Endpoint backendInterfaceEndpoint;
+    private final Endpoint backendInterfaceEndpoint;
 
-    public SchemaValidationEnabledChangeListener(Endpoint backendInterfaceEndpoint) {
+    public SchemaValidationEnabledChangeListener(@Qualifier("backendInterfaceEndpoint") Endpoint backendInterfaceEndpoint) {
         this.backendInterfaceEndpoint = backendInterfaceEndpoint;
     }
 
