6f51a6fc6ad8717ea1f439e301cab3317927380c
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2020-10-14 15:43:49 +0200

    [EDELIVERY-7269] revert all changes

--- Domibus-MSH/src/main/java/eu/domibus/core/spring/DomibusContextRefreshedListener.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/spring/DomibusContextRefreshedListener.java
@@ -1,7 +1,6 @@
 package eu.domibus.core.spring;
 
 import eu.domibus.api.encryption.EncryptionService;
-import eu.domibus.core.plugin.routing.RoutingService;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,9 +23,6 @@ public class DomibusContextRefreshedListener {
     @Autowired
     protected EncryptionService encryptionService;
 
-    @Autowired
-    protected RoutingService routingService;
-
     @Transactional(propagation = Propagation.NOT_SUPPORTED)
     @EventListener
     public void onApplicationEvent(ContextRefreshedEvent event) {
@@ -37,8 +33,6 @@ public class DomibusContextRefreshedListener {
             LOG.info("Skipping event: we are processing only the web application context event");
             return;
         }
-
-        routingService.init();
 
         encryptionService.handleEncryption();
 
