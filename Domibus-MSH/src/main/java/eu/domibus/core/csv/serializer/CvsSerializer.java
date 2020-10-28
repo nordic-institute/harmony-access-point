4c08905be96492a4910b433c475c3946b697074d
ion.perpegel <ion.perpegel@gmail.com>
2020-10-28 08:43:40 +0200

    [EDELIVRY-7144] use default separator from common library

--- Domibus-MSH/src/main/java/eu/domibus/core/csv/serializer/CvsSerializer.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/csv/serializer/CvsSerializer.java
@@ -1,11 +1,13 @@
 package eu.domibus.core.csv.serializer;
 
+import com.opencsv.ICSVWriter;
+
 /**
  * @author François Gautier
  * @since 4.2
  */
 public interface CvsSerializer {
-    static final String LIST_DELIMITER = ",";
+    static final String LIST_DELIMITER = String.valueOf(ICSVWriter.DEFAULT_SEPARATOR);
 
     boolean canHandle(Object fieldValue);
     String serialize(Object fieldValue);
