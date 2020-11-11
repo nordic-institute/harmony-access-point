10ce26a0de047e268888247802e0ef7108bc6542
ion.perpegel <ion.perpegel@gmail.com>
2020-11-11 15:09:41 +0200

    Revert "[EDELIVERY-6182] some trials to introduce spring session with jdbc store and initialize it correctly with rerspect to spring security and domibus bootstrap configuration"
    This reverts commit 449847e7

--- Domibus-MSH/src/main/java/eu/domibus/core/spring/SessionInitializer.java
+++
@@ -1,7 +0,0 @@
-package eu.domibus.core.spring;
-
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-
-public class SessionInitializer extends AbstractHttpSessionApplicationInitializer {
-
-}
