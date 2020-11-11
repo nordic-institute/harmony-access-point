449847e7fb63513f11a902fdeb329fb9fd734374
ion.perpegel <ion.perpegel@gmail.com>
2020-11-11 12:44:45 +0200

    [EDELIVERY-6182] some trials to introduce spring session with jdbc store and initialize it correctly with rerspect to spring security and domibus bootstrap configuration

--- Domibus-MSH/src/main/java/eu/domibus/core/spring/DomibusApplicationInitializer.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/spring/DomibusApplicationInitializer.java
@@ -62,7 +62,7 @@ public class DomibusApplicationInitializer implements WebApplicationInitializer 
         Thread.currentThread().setContextClassLoader(pluginClassLoader);
 
         AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-        rootContext.register(DomibusRootConfiguration.class);
+        rootContext.register(DomibusRootConfiguration.class, SessionConfig.class);
 
         try {
             configurePropertySources(rootContext, domibusConfigLocation);
---
+++ Domibus-MSH/src/main/java/eu/domibus/core/spring/SessionInitializer.java
@@ -0,0 +1,7 @@
+package eu.domibus.core.spring;
+
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+public class SessionInitializer extends AbstractHttpSessionApplicationInitializer {
+
+}
