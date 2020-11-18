0fe8a8b814fe8fa03273adc88cd3fdb93f0c3e79
ion.perpegel <ion.perpegel@gmail.com>
2020-11-18 16:10:02 +0200

    [EDELIVERY-6182] remove unnedeed transaction manager

--- Domibus-MSH/src/main/java/eu/domibus/core/spring/DomibusSessionConfiguration.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/spring/DomibusSessionConfiguration.java
@@ -5,7 +5,6 @@ import eu.domibus.core.property.DomibusPropertyProviderImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -18,6 +17,7 @@ import javax.sql.DataSource;
 
 /**
  * Beans and configuration needed by Spring Session
+ *
  * @author Ion Perpegel
  * @since 5.0
  */
@@ -28,17 +28,15 @@ public class DomibusSessionConfiguration {
     @Autowired
     DomibusPropertyProviderImpl domibusPropertyProvider;
 
+    @Autowired
+    protected PlatformTransactionManager transactionManager;
+
     @Bean
     public EmbeddedDatabase dataSource() {
         return new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
                 .addScript("org/springframework/session/jdbc/schema-h2.sql")
                 .build();
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
     }
 
     @Bean
