3c453ee4bdf807aa7669710543e7e2aa4736095e
ion.perpegel <ion.perpegel@gmail.com>
2020-11-19 15:55:12 +0200

    [EDELIVERY-6182] fix constants

--- Domibus-MSH/src/main/java/eu/domibus/core/spring/DomibusSessionInitializer.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/spring/DomibusSessionInitializer.java
@@ -4,6 +4,8 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
+import static eu.domibus.core.spring.DomibusSessionInitializer.SESSION_INITIALIZER_ORDER;
+
 /**
  * WebApplicationInitializer needed by Spring Session to insert the session filter as the first one;
  * hence the highest precedence, even higher than DomibusApplicationInitializer
@@ -12,9 +14,9 @@ import org.springframework.session.web.context.AbstractHttpSessionApplicationIni
  * @since 5.0
  * Makes sure this is executed first so that the Spring session filter is added before anybody else
  */
-@Order(Ordered.HIGHEST_PRECEDENCE)
+@Order(SESSION_INITIALIZER_ORDER)
 public class DomibusSessionInitializer extends AbstractHttpSessionApplicationInitializer {
     // High precedence but less than the highest possible
     // HIGHEST_PRECEDENCE is negative so adding one actually decreases the precedence
-    public static final int SESSION_INITIALIZER_ORDER = Ordered.HIGHEST_PRECEDENCE + 1;
+    public static final int SESSION_INITIALIZER_ORDER = Ordered.HIGHEST_PRECEDENCE;
 }
