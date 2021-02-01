9eee61681a3a2b61c0882bf33674f6c11e654470
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2021-02-01 11:36:07 +0100

    [EDELIVERY-4808] added IT tests

--- Domibus-default-ws-plugin/src/test/java/eu/domibus/plugin/webService/dao/WSMessageLogDaoIT.java
+++ Domibus-default-ws-plugin/src/test/java/eu/domibus/plugin/webService/dao/WSMessageLogDaoIT.java
@@ -16,6 +16,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
 
@@ -54,6 +57,26 @@ public class WSMessageLogDaoIT {
         entity2.setReceived(DateUtils.addDays(new Date(), -2));
         wsMessageLogDao.create(entity2);
 
+        WSMessageLogEntity entity3 = new WSMessageLogEntity();
+        entity3.setMessageId("messageID_3");
+        entity3.setRefToMessageId("refToMessageID_3");
+        entity3.setConversationId("conversationID_3");
+        entity3.setOriginalSender("urn:oasis:names:tc:ebcore:partyid-type:unregistered:C1");
+        entity3.setFinalRecipient("urn:oasis:names:tc:ebcore:partyid-type:unregistered:C4");
+        entity3.setFromPartyId("domibus-blue2");
+        entity3.setReceived(DateUtils.addDays(new Date(), -1));
+        wsMessageLogDao.create(entity3);
+
+        WSMessageLogEntity entity4 = new WSMessageLogEntity();
+        entity4.setMessageId("messageID_4");
+        entity4.setRefToMessageId("refToMessageID_4");
+        entity4.setConversationId("conversationID_4");
+        entity4.setOriginalSender("urn:oasis:names:tc:ebcore:partyid-type:unregistered:C1");
+        entity4.setFinalRecipient("urn:oasis:names:tc:ebcore:partyid-type:unregistered:C4");
+        entity4.setFromPartyId("domibus-blue2");
+        entity4.setReceived(new Date());
+        wsMessageLogDao.create(entity4);
+
 
     }
 
@@ -91,6 +114,17 @@ public class WSMessageLogDaoIT {
         Assert.assertEquals(1, wsMessageLogEntityList.size());
         Assert.assertEquals("messageID_2", wsMessageLogEntityList.get(0).getMessageId());
 
+        wsMessageLogEntityList =  wsMessageLogDao.findAllWithFilter(null, null, "conversationID_3",
+                null, null, null, null, null, 0);
+        Assert.assertTrue(CollectionUtils.isNotEmpty(wsMessageLogEntityList));
+        Assert.assertEquals(1, wsMessageLogEntityList.size());
+        Assert.assertEquals("messageID_3", wsMessageLogEntityList.get(0).getMessageId());
+
+        LocalDateTime now = LocalDateTime.now(ZoneId.systemDefault());
+        wsMessageLogEntityList =  wsMessageLogDao.findAllWithFilter(null, null, null,
+                null, null, null, now.minus(Period.ofDays(3)), now, 0);
+        Assert.assertTrue(CollectionUtils.isNotEmpty(wsMessageLogEntityList));
+        Assert.assertEquals(3, wsMessageLogEntityList.size());
 
     }
 }
