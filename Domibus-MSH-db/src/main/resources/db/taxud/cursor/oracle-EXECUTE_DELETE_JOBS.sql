cd3802fb0dc5150b05c1e25cd461951efd74534f
Ioana DRAGUSANU <idragusa@gmail.com>
2021-02-05 17:03:29 +0200

    [4.2-Taxud] Second jobs run 1 hour later

--- Domibus-MSH-db/src/main/resources/db/taxud/cursor/oracle-EXECUTE_DELETE_JOBS.sql
+++ Domibus-MSH-db/src/main/resources/db/taxud/cursor/oracle-EXECUTE_DELETE_JOBS.sql
@@ -6,25 +6,6 @@ DECLARE
 BEGIN
   message_number_to_delete := 5000;
   mpc:='urn:fdc:ec.europa.eu:2019:eu_ics2_t2c';
-  start_date:=SYSDATE-30; -- messages will be deleted including start_date
-  end_date:=SYSDATE-4; -- messages will be deleted excluding the end date
-
-  DeleteExpiredDownloadedMessages(
-    mpc=>mpc,
-    start_date=>start_date,
-    end_date=>end_date,
-    message_number_to_delete=>message_number_to_delete
-  );
-END;
-
-DECLARE
-  message_number_to_delete NUMBER;
-  mpc VARCHAR2(255);
-  start_date DATE;
-  end_date DATE;
-BEGIN
-  message_number_to_delete := 5000;
-  mpc:='urn:fdc:ec.europa.eu:2019:eu_ics2_t2c';
   start_date:=SYSDATE-4; -- messages will be deleted including start_date
   end_date:=SYSDATE-3; -- messages will be deleted excluding the end date
 
@@ -45,7 +26,26 @@ BEGIN
   message_number_to_delete := 5000;
   mpc:='urn:fdc:ec.europa.eu:2019:eu_ics2_t2c';
   start_date:=SYSDATE-30; -- messages will be deleted including start_date
-  end_date:=SYSDATE-4; -- messages will be deleted excluding the end date
+  end_date:=SYSDATE-4-1/24; -- messages will be deleted excluding the end date
+
+  DeleteExpiredDownloadedMessages(
+    mpc=>mpc,
+    start_date=>start_date,
+    end_date=>end_date,
+    message_number_to_delete=>message_number_to_delete
+  );
+END;
+
+DECLARE
+  message_number_to_delete NUMBER;
+  mpc VARCHAR2(255);
+  start_date DATE;
+  end_date DATE;
+BEGIN
+  message_number_to_delete := 5000;
+  mpc:='urn:fdc:ec.europa.eu:2019:eu_ics2_t2c';
+  start_date:=SYSDATE-4; -- messages will be deleted including start_date
+  end_date:=SYSDATE-3; -- messages will be deleted excluding the end date
 
   DeleteExpiredSentMessages(
     mpc=>mpc,
@@ -63,8 +63,8 @@ DECLARE
 BEGIN
   message_number_to_delete := 5000;
   mpc:='urn:fdc:ec.europa.eu:2019:eu_ics2_t2c';
-  start_date:=SYSDATE-4; -- messages will be deleted including start_date
-  end_date:=SYSDATE-3; -- messages will be deleted excluding the end date
+  start_date:=SYSDATE-30; -- messages will be deleted including start_date
+  end_date:=SYSDATE-4-1/24; -- messages will be deleted excluding the end date
 
   DeleteExpiredSentMessages(
     mpc=>mpc,
