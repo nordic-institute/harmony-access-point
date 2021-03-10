25c0bb6d3384825e40a586fd2dcb1881b67cec6b
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2021-03-10 17:35:43 +0100

    [EDELIVERY-7877] added TB_D_MPC mechanism

--- Domibus-MSH-db/src/main/resources/db/migration/oracle/db-migration-package.sql
+++ Domibus-MSH-db/src/main/resources/db/migration/oracle/db-migration-package.sql
@@ -1,8 +1,6 @@
 CREATE OR REPLACE PACKAGE MIGRATE_42_TO_50 IS
     TEMP_PREFIX CONSTANT Varchar2(5) := 'TEMP_';
-    BATCH_SIZE CONSTANT NUMBER := 10000;
-
-    PROCEDURE drop_table(tab_name IN VARCHAR2);
+    BATCH_SIZE CONSTANT NUMBER := 3;
 
     PROCEDURE migrate_tb_user_message;
 
@@ -13,6 +11,7 @@ END MIGRATE_42_TO_50;
 
 CREATE OR REPLACE PACKAGE BODY MIGRATE_42_TO_50 IS
 
+    /** -- Helper procedures start -*/
     PROCEDURE drop_table(tab_name IN VARCHAR2) IS
         v_table_exists INT;
     BEGIN
@@ -23,10 +22,41 @@ CREATE OR REPLACE PACKAGE BODY MIGRATE_42_TO_50 IS
         END IF;
     END drop_table;
 
+    PROCEDURE truncate_or_create_table(tab_name IN VARCHAR2, create_sql IN VARCHAR2) IS
+        v_table_exists INT;
+    BEGIN
+        SELECT COUNT(*) INTO v_table_exists FROM USER_TABLES WHERE table_name = UPPER(tab_name);
+        IF v_table_exists > 0 THEN
+            EXECUTE IMMEDIATE 'TRUNCATE TABLE ' || tab_name;
+            DBMS_OUTPUT.PUT_LINE('Table ' || tab_name || ' truncated');
+        ELSE
+            EXECUTE IMMEDIATE create_sql;
+            DBMS_OUTPUT.PUT_LINE('Table ' || tab_name || ' created');
+        END IF;
+    END truncate_or_create_table;
+
+    FUNCTION get_tb_d_mpc_record(mpc_value IN VARCHAR2) RETURN NUMBER IS
+        v_id_pk NUMBER := -1;
+    BEGIN
+        BEGIN
+            SELECT ID_PK into v_id_pk FROM TB_D_MPC WHERE VALUE = mpc_value;
+        EXCEPTION
+            WHEN NO_DATA_FOUND THEN
+                -- create new record
+                DBMS_OUTPUT.PUT_LINE('Add new record into TB_D_MPC: '||mpc_value);
+                v_id_pk := HIBERNATE_SEQUENCE.nextval;
+                EXECUTE IMMEDIATE 'INSERT INTO TB_D_MPC(ID_PK, VALUE) VALUES (' || v_id_pk || ', :1)' USING mpc_value;
+                COMMIT;
+        END;
+        RETURN v_id_pk;
+    END get_tb_d_mpc_record;
+    /** -- Helper procedures end -*/
+
     /**-- TB_USER_MESSAGE migration --*/
     PROCEDURE migrate_tb_user_message IS
         v_sql        VARCHAR2(1000);
         v_temp_table VARCHAR2(30) := TEMP_PREFIX || 'TB_USER_MESSAGE';
+        v_dict_table VARCHAR2(30);
         CURSOR c_user_message IS
             SELECT UM.ID_PK,
                    MI.MESSAGE_ID           MESSAGE_ID,
@@ -34,20 +64,23 @@ CREATE OR REPLACE PACKAGE BODY MIGRATE_42_TO_50 IS
                    UM.COLL_INFO_CONVERS_ID CONVERSATION_ID,
                    ML.SOURCE_MESSAGE       SOURCE_MESSAGE,
                    ML.MESSAGE_FRAGMENT     MESSAGE_FRAGMENT,
-                   MI.TIME_STAMP           EBMS3_TIMESTAMP
+                   MI.TIME_STAMP           EBMS3_TIMESTAMP,
+                   UM.MPC                  MPC
             FROM TB_MESSAGE_LOG ML
                      LEFT OUTER JOIN TB_MESSAGE_INFO MI ON ML.MESSAGE_ID = MI.MESSAGE_ID,
                  TB_USER_MESSAGE UM
             WHERE UM.MESSAGEINFO_ID_PK = MI.ID_PK;
         TYPE T_USER_MESSAGE IS TABLE OF c_user_message%ROWTYPE;
         user_message T_USER_MESSAGE;
-        v_batch_no INT := 1;
+        v_batch_no   INT          := 1;
     BEGIN
-        drop_table(v_temp_table);
         v_sql := 'CREATE TABLE ' || v_temp_table ||
                  '(ID_PK NUMBER(38, 0) NOT NULL, MESSAGE_ID VARCHAR2(255), REF_TO_MESSAGE_ID VARCHAR2(255), CONVERSATION_ID VARCHAR2(255), SPLIT_AND_JOIN NUMBER(1), SOURCE_MESSAGE NUMBER(1), MESSAGE_FRAGMENT NUMBER(1), EBMS3_TIMESTAMP TIMESTAMP, ACTION_ID_FK NUMBER(38, 0), AGREEMENT_ID_FK NUMBER(38, 0), SERVICE_ID_FK NUMBER(38, 0), MPC_ID_FK NUMBER(38, 0), FROM_PARTY_ID_FK NUMBER(38, 0), FROM_ROLE_ID_FK NUMBER(38, 0), TO_PARTY_ID_FK NUMBER(38, 0), TO_ROLE_ID_FK NUMBER(38, 0), MESSAGE_SUBTYPE_ID_FK NUMBER(38, 0), CREATION_TIME TIMESTAMP DEFAULT sysdate NOT NULL, CREATED_BY VARCHAR2(255) DEFAULT user NOT NULL, MODIFICATION_TIME TIMESTAMP, MODIFIED_BY VARCHAR2(255), CONSTRAINT PK_USER_MESSAGE PRIMARY KEY (ID_PK))';
-        EXECUTE IMMEDIATE v_sql;
-        DBMS_OUTPUT.PUT_LINE('Temp table: ' || v_temp_table || ' successfully created.');
+        truncate_or_create_table(v_temp_table, v_sql);
+
+        v_sql := 'CREATE TABLE TB_D_MPC (ID_PK NUMBER(38, 0) NOT NULL, VALUE VARCHAR2(255) NOT NULL, CREATION_TIME TIMESTAMP DEFAULT sysdate NOT NULL, CREATED_BY VARCHAR2(255) DEFAULT user NOT NULL, MODIFICATION_TIME TIMESTAMP, MODIFIED_BY VARCHAR2(255), CONSTRAINT PK_D_MPC PRIMARY KEY (ID_PK))';
+        v_dict_table := 'TB_D_MPC';
+        truncate_or_create_table(v_dict_table, v_sql);
 
         /** migrate old columns */
         DBMS_OUTPUT.PUT_LINE('Start to migrate TB_USER_MESSAGE entries...');
@@ -59,23 +92,24 @@ CREATE OR REPLACE PACKAGE BODY MIGRATE_42_TO_50 IS
             FOR i IN user_message.FIRST .. user_message.LAST
                 LOOP
                     EXECUTE IMMEDIATE 'INSERT INTO ' || v_temp_table ||
-                                      ' (ID_PK, MESSAGE_ID, REF_TO_MESSAGE_ID, CONVERSATION_ID, SOURCE_MESSAGE, MESSAGE_FRAGMENT, EBMS3_TIMESTAMP) ' ||
-                                      'VALUES (:p_1, :p_2, :p_3, :p_4, :p_5, :p_6, :p_7)'
+                                      ' (ID_PK, MESSAGE_ID, REF_TO_MESSAGE_ID, CONVERSATION_ID, SOURCE_MESSAGE, MESSAGE_FRAGMENT, EBMS3_TIMESTAMP, MPC_ID_FK) ' ||
+                                      'VALUES (:p_1, :p_2, :p_3, :p_4, :p_5, :p_6, :p_7, :p_8)'
                         USING user_message(i).ID_PK,
                         user_message(i).MESSAGE_ID,
                         user_message(i).REF_TO_MESSAGE_ID,
                         user_message(i).CONVERSATION_ID,
                         user_message(i).SOURCE_MESSAGE,
                         user_message(i).MESSAGE_FRAGMENT,
-                        user_message(i).EBMS3_TIMESTAMP;
+                        user_message(i).EBMS3_TIMESTAMP,
+                        get_tb_d_mpc_record(user_message(i).MPC);
                     IF i MOD BATCH_SIZE = 0 THEN
                         COMMIT;
-                        DBMS_OUTPUT.PUT_LINE('Commit after' || BATCH_SIZE * v_batch_no || ' records');
+                        DBMS_OUTPUT.PUT_LINE('Commit after ' || BATCH_SIZE * v_batch_no || ' records');
                         v_batch_no := v_batch_no + 1;
                     END IF;
 
                 END LOOP;
-            DBMS_OUTPUT.PUT_LINE('Wrote ' || user_message.COUNT || ' records in total into '||v_temp_table);
+            DBMS_OUTPUT.PUT_LINE('Wrote ' || user_message.COUNT || ' records in total into ' || v_temp_table);
         END LOOP;
 
         COMMIT;
