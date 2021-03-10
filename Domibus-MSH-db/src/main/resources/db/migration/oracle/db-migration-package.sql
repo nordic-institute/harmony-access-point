3c983da3aacae8fa3f32126d7b0cf7f43978a21f
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2021-03-10 16:18:56 +0100

    [EDELIVERY-7877] improved batch commit mechanism

--- Domibus-MSH-db/src/main/resources/db/migration/oracle/db-migration-package.sql
+++ Domibus-MSH-db/src/main/resources/db/migration/oracle/db-migration-package.sql
@@ -1,7 +1,8 @@
 CREATE OR REPLACE PACKAGE MIGRATE_42_TO_50 IS
     TEMP_PREFIX CONSTANT Varchar2(5) := 'TEMP_';
+    BATCH_SIZE CONSTANT NUMBER := 10000;
 
-    PROCEDURE drop_table(table_name IN VARCHAR2);
+    PROCEDURE drop_table(tab_name IN VARCHAR2);
 
     PROCEDURE migrate_tb_user_message;
 
@@ -12,19 +13,19 @@ END MIGRATE_42_TO_50;
 
 CREATE OR REPLACE PACKAGE BODY MIGRATE_42_TO_50 IS
 
-    PROCEDURE drop_table(table_name IN VARCHAR2) IS
+    PROCEDURE drop_table(tab_name IN VARCHAR2) IS
         v_table_exists INT;
     BEGIN
-        SELECT COUNT(*) INTO v_table_exists FROM USER_TABLES WHERE table_name = UPPER(table_name);
+        SELECT COUNT(*) INTO v_table_exists FROM USER_TABLES WHERE table_name = UPPER(tab_name);
         IF v_table_exists > 0 THEN
-            EXECUTE IMMEDIATE 'DROP TABLE ' || table_name;
-            DBMS_OUTPUT.PUT_LINE('Table ' || table_name || ' dropped');
+            EXECUTE IMMEDIATE 'DROP TABLE ' || tab_name;
+            DBMS_OUTPUT.PUT_LINE('Table ' || tab_name || ' dropped');
         END IF;
     END drop_table;
 
     /**-- TB_USER_MESSAGE migration --*/
     PROCEDURE migrate_tb_user_message IS
-        v_sql        LONG;
+        v_sql        VARCHAR2(1000);
         v_temp_table VARCHAR2(30) := TEMP_PREFIX || 'TB_USER_MESSAGE';
         CURSOR c_user_message IS
             SELECT UM.ID_PK,
@@ -40,15 +41,16 @@ CREATE OR REPLACE PACKAGE BODY MIGRATE_42_TO_50 IS
             WHERE UM.MESSAGEINFO_ID_PK = MI.ID_PK;
         TYPE T_USER_MESSAGE IS TABLE OF c_user_message%ROWTYPE;
         user_message T_USER_MESSAGE;
+        v_batch_no INT := 1;
     BEGIN
         drop_table(v_temp_table);
         v_sql := 'CREATE TABLE ' || v_temp_table ||
                  '(ID_PK NUMBER(38, 0) NOT NULL, MESSAGE_ID VARCHAR2(255), REF_TO_MESSAGE_ID VARCHAR2(255), CONVERSATION_ID VARCHAR2(255), SPLIT_AND_JOIN NUMBER(1), SOURCE_MESSAGE NUMBER(1), MESSAGE_FRAGMENT NUMBER(1), EBMS3_TIMESTAMP TIMESTAMP, ACTION_ID_FK NUMBER(38, 0), AGREEMENT_ID_FK NUMBER(38, 0), SERVICE_ID_FK NUMBER(38, 0), MPC_ID_FK NUMBER(38, 0), FROM_PARTY_ID_FK NUMBER(38, 0), FROM_ROLE_ID_FK NUMBER(38, 0), TO_PARTY_ID_FK NUMBER(38, 0), TO_ROLE_ID_FK NUMBER(38, 0), MESSAGE_SUBTYPE_ID_FK NUMBER(38, 0), CREATION_TIME TIMESTAMP DEFAULT sysdate NOT NULL, CREATED_BY VARCHAR2(255) DEFAULT user NOT NULL, MODIFICATION_TIME TIMESTAMP, MODIFIED_BY VARCHAR2(255), CONSTRAINT PK_USER_MESSAGE PRIMARY KEY (ID_PK))';
         EXECUTE IMMEDIATE v_sql;
-        DBMS_OUTPUT.PUT_LINE('New table: ' || v_temp_table || ' successfully created.');
+        DBMS_OUTPUT.PUT_LINE('Temp table: ' || v_temp_table || ' successfully created.');
 
         /** migrate old columns */
-        DBMS_OUTPUT.PUT_LINE('Migrating TB_USER_MESSAGE entries...');
+        DBMS_OUTPUT.PUT_LINE('Start to migrate TB_USER_MESSAGE entries...');
         OPEN c_user_message;
         LOOP
             FETCH c_user_message BULK COLLECT INTO user_message;
@@ -66,9 +68,17 @@ CREATE OR REPLACE PACKAGE BODY MIGRATE_42_TO_50 IS
                         user_message(i).SOURCE_MESSAGE,
                         user_message(i).MESSAGE_FRAGMENT,
                         user_message(i).EBMS3_TIMESTAMP;
+                    IF i MOD BATCH_SIZE = 0 THEN
+                        COMMIT;
+                        DBMS_OUTPUT.PUT_LINE('Commit after' || BATCH_SIZE * v_batch_no || ' records');
+                        v_batch_no := v_batch_no + 1;
+                    END IF;
+
                 END LOOP;
-            DBMS_OUTPUT.PUT_LINE('Wrote ' || user_message.COUNT || ' records');
+            DBMS_OUTPUT.PUT_LINE('Wrote ' || user_message.COUNT || ' records in total into '||v_temp_table);
         END LOOP;
+
+        COMMIT;
         CLOSE c_user_message;
 
 
