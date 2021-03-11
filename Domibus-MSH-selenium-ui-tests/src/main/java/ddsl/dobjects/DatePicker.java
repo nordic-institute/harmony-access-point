7ecc5ce7e041faf5d511397358e1cb995cf848ab
Rupam <rupam.rupam@ext.ec.europa.eu>
2021-03-11 11:33:53 +0100

    Script for err-17, msg23

--- Domibus-MSH-selenium-ui-tests/src/main/java/ddsl/dobjects/DatePicker.java
+++ Domibus-MSH-selenium-ui-tests/src/main/java/ddsl/dobjects/DatePicker.java
@@ -5,12 +5,16 @@ import org.apache.commons.lang3.time.DateUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.testng.asserts.SoftAssert;
+import utils.TestRunData;
 import utils.TestUtils;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -68,8 +72,13 @@ public class DatePicker extends DComponent {
 	@FindBy(css = "div.md2-clock-hours div.md2-clock-cell")
 	private List<WebElement> clockHourItems;
 	@FindBy(css = "div.md2-clock-minutes div.md2-clock-cell")
-	private List<WebElement> clockMinuteItems;
-	
+	public List<WebElement> clockMinuteItems;
+
+	@FindBy (css = ".md2-calendar-body-cell-content.md2-calendar-body-today")
+	public WebElement todayDate;
+	@FindBy(css = "div.md2-clock-hours.active div.md2-clock-cell-selected")
+	public WebElement currentClockHour;
+
 	public DatePicker(WebDriver driver, WebElement container) {
 		super(driver);
 		PageFactory.initElements(new AjaxElementLocatorFactory(driver, data.getTIMEOUT()), this);
@@ -122,6 +131,40 @@ public class DatePicker extends DComponent {
 //	used don't change
 	public void clearSelectedDate() throws Exception {
 		new DInput(driver, getInput()).clear();
+	}
+
+	public int verifyMaxClockValue(SoftAssert soft, Calendar cal){
+		log.debug("Getting Current date & time");
+		Date date = cal.getTime();
+		soft.assertTrue(Integer.parseInt(todayDate.getText()) == cal.get(Calendar.DAY_OF_MONTH), "Current date & Max enable Clock date are same");
+		todayDate.click();
+		soft.assertTrue(Integer.parseInt(currentClockHour.getText()) == cal.get(Calendar.HOUR_OF_DAY), "Current hour & Max Enable Clock hour are same");
+
+		Actions act = new Actions(driver);
+		act.moveToElement(currentClockHour).click().perform();
+		int size = clockMinuteItems.size();
+		log.debug("Check all enable minutes for selection are smaller than Current minute");
+
+		for (int i = 0; i < size; i++) {
+			if (clockMinuteItems.get(i).getAttribute("class").contains("disabled")) {
+				soft.assertTrue(Integer.parseInt(clockMinuteItems.get(i).getText()) > cal.get(Calendar.MINUTE), "Disabled clock minutes are greater than current minute");
+
+			} else {
+				soft.assertTrue(Integer.parseInt(clockMinuteItems.get(i).getText()) < cal.get(Calendar.MINUTE), "Enabled clock minutes are smaller than current Minute");
+			}
+
+		}
+		act.moveToElement(clockMinuteItems.get(size - 1)).click().perform();
+		log.debug("Current System date & time : " + getSelectedDate());
+		log.debug("Selected Date & time in Field : " + TestRunData.DATEWIDGET_DATE_FORMAT.format(date));
+		if(getSelectedDate().compareTo(TestRunData.DATEWIDGET_DATE_FORMAT.format(date)) >= 0){
+			return 1 ;
+		}
+		else{
+			return -1;
+		}
+
+
 	}
 
 	
