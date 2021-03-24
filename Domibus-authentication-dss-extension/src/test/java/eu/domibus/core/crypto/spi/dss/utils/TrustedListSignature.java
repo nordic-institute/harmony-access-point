e9754aea0bea0243d536fa425c7939190622d143
dussath <thomas.dussart@ext.ec.europa.eu>
2021-03-24 16:06:30 +0100

    EDELIVERY-7848: Upgrade dss 5.8

---
+++ Domibus-authentication-dss-extension/src/test/java/eu/domibus/core/crypto/spi/dss/utils/TrustedListSignature.java
@@ -0,0 +1,12 @@
+package eu.domibus.core.crypto.spi.dss.utils;
+
+/**
+ * @author Thomas Dussart
+ * @since 4.2
+ */
+public class TrustedListSignature {
+
+    public TrustedListSignature() {
+
+    }
+}
