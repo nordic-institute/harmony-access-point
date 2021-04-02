59eb35073db45fd1c2f9156dc6d946e68f5ddc60
Sebastian-Ion TINCU <Sebastian-Ion.TINCU@ext.ec.europa.eu>
2021-04-02 12:24:36 +0200

    EDELIVERY-7822 Provide a solution to tackle the timezone issues detected
    Fix unit tests.

--- Domibus-default-ws-plugin/src/test/java/eu/domibus/plugin/ws/WSPluginDaoTestConfig.java
+++ Domibus-default-ws-plugin/src/test/java/eu/domibus/plugin/ws/WSPluginDaoTestConfig.java
@@ -6,12 +6,19 @@ import eu.domibus.plugin.ws.message.WSMessageLogDao;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Date;
+
 /**
  * @author François Gautier
  * @since 5.0
  */
 @Configuration
 public class WSPluginDaoTestConfig {
+
+    @Bean
+    public DateExtService dateExtService() {
+        return () -> new Date();
+    }
 
     @Bean
     public WSBackendMessageLogDao wsBackendMessageLogDao(DateExtService dateExtService){
