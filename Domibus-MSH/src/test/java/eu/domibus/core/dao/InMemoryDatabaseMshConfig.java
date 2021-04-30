da905e8969128ed3fb7afb8005de29667d23a420
soumya <Soumya.AZHIKODE-CHANDRAN@ext.ec.europa.eu>
2021-04-30 09:10:37 +0200

    [EDELIVERY-8011]- Use UTC for now property defined in the H2 SQL scripts

--- Domibus-MSH/src/test/java/eu/domibus/core/dao/InMemoryDatabaseMshConfig.java
+++ Domibus-MSH/src/test/java/eu/domibus/core/dao/InMemoryDatabaseMshConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.TimeZone;
 
 /**
  * @author Thomas Dussart
@@ -14,8 +15,10 @@ import javax.sql.DataSource;
 public class InMemoryDatabaseMshConfig extends AbstractDatabaseMshConfig {
 
     public DataSource dataSource() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         return getH2DataSource();
     }
+
 
     @Override
     public String getSpecificDatabaseDialect() {
