9d09e3726c595aa23e948c9c45b3d57a6f3af6d1
ion.perpegel <ion.perpegel@gmail.com>
2021-05-08 08:07:23 +0300

    [EDELIVERY-7931] fix int overflow when computing the message expiration date

--- Domibus-MSH/src/test/java/eu/domibus/core/ebms3/sender/retry/UpdateRetryLoggingServiceTest.java
+++ Domibus-MSH/src/test/java/eu/domibus/core/ebms3/sender/retry/UpdateRetryLoggingServiceTest.java
@@ -354,17 +354,16 @@ public class UpdateRetryLoggingServiceTest {
 
     @Test
     public void testMessageExpirationDate(@Injectable final UserMessageLog userMessageLog,
-                                          @Injectable final LegConfiguration legConfiguration,
-                                          @Injectable ReceptionAwareness receptionAwareness) throws InterruptedException {
-        final int timeOut = 10;
-        final long timeOutInMillis = 60000 * timeOut;
-        final long restoredTime = System.currentTimeMillis();// - (timeOutInMillis + delay);
+                                          @Injectable final LegConfiguration legConfiguration) throws InterruptedException {
+        final int timeOutInMin = 10; // in minutes
+        final long timeOutInMillis = 60000L * timeOutInMin;
+        final long restoredTime = System.currentTimeMillis();
         final Date expectedDate = new Date(restoredTime + timeOutInMillis);
 
 
         new Expectations(updateRetryLoggingService) {{
             legConfiguration.getReceptionAwareness().getRetryTimeout();
-            result = timeOut;
+            result = timeOutInMin;
 
             updateRetryLoggingService.getScheduledStartTime(userMessageLog);
             result = restoredTime;
@@ -376,20 +375,33 @@ public class UpdateRetryLoggingServiceTest {
     }
 
     @Test
-    public void testIsExpired(@Injectable final UserMessageLog userMessageLog,
-                              @Injectable final LegConfiguration legConfiguration,
-                              @Injectable ReceptionAwareness receptionAwareness) throws InterruptedException {
-
-        int delay = 10;
-
-        final int timeOut = 10;
-        final long timeOutInMillis = 60000 * timeOut;
-        final long restoredTime = System.currentTimeMillis();// - (timeOutInMillis + delay);
+    public void testMessageExpirationDateInTheFarFuture(@Injectable final UserMessageLog userMessageLog,
+                              @Injectable final LegConfiguration legConfiguration) throws InterruptedException {
+        final int timeOutInMin = 90 * 24 * 60; // 90 days in minutes
+        final long timeOutInMillis = 60000L * timeOutInMin;
+        final long restoredTime = System.currentTimeMillis();
         final Date expectedDate = new Date(restoredTime + timeOutInMillis);
 
+        new Expectations(updateRetryLoggingService) {{
+            legConfiguration.getReceptionAwareness().getRetryTimeout();
+            result = timeOutInMin;
+
+            updateRetryLoggingService.getScheduledStartTime(userMessageLog);
+            result = restoredTime;
+        }};
+        Date messageExpirationDate = updateRetryLoggingService.getMessageExpirationDate(userMessageLog, legConfiguration);
+
+        assertEquals(expectedDate, messageExpirationDate);
+    }
+
+    @Test
+    public void testIsExpired(@Injectable final UserMessageLog userMessageLog,
+                              @Injectable final LegConfiguration legConfiguration) throws InterruptedException {
+
+        long delay = 10;
 
         new Expectations(updateRetryLoggingService) {{
-            domibusPropertyProvider.getIntegerProperty(UpdateRetryLoggingService.MESSAGE_EXPIRATION_DELAY);
+            domibusPropertyProvider.getLongProperty(UpdateRetryLoggingService.MESSAGE_EXPIRATION_DELAY);
             result = delay;
 
             updateRetryLoggingService.getMessageExpirationDate(userMessageLog, legConfiguration);
