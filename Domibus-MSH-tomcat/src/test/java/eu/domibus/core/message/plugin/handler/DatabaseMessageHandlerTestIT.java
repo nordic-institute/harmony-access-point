d76ce05962439d93d0f4797d4e7345ddea593463
Cosmin Baciu <cosmin.baciu@ext.ec.europa.eu>
2021-06-07 11:37:16 +0200

    EDELIVERY-8137 - fix bamboo plan

--- Domibus-MSH-tomcat/src/test/java/eu/domibus/core/message/plugin/handler/DatabaseMessageHandlerTestIT.java
+++ Domibus-MSH-tomcat/src/test/java/eu/domibus/core/message/plugin/handler/DatabaseMessageHandlerTestIT.java
@@ -5,13 +5,17 @@ import eu.domibus.api.model.MessageType;
 import eu.domibus.api.model.UserMessageLog;
 import eu.domibus.core.message.MessagesLogServiceImpl;
 import eu.domibus.core.message.UserMessageLogDao;
-import eu.domibus.test.common.SubmissionUtil;
 import eu.domibus.core.plugin.BackendConnectorProvider;
 import eu.domibus.core.plugin.handler.DatabaseMessageHandler;
 import eu.domibus.messaging.MessagingProcessingException;
 import eu.domibus.plugin.BackendConnector;
 import eu.domibus.plugin.Submission;
+import eu.domibus.test.common.SubmissionUtil;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -52,11 +56,17 @@ public class DatabaseMessageHandlerTestIT extends AbstractIT {
     @Autowired
     UserMessageLogDao userMessageLogDao;
 
-    @Test
-    public void submit() throws MessagingProcessingException, IOException {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void before() {
         BackendConnector backendConnector = Mockito.mock(BackendConnector.class);
         Mockito.when(backendConnectorProvider.getBackendConnector(Mockito.any(String.class))).thenReturn(backendConnector);
+    }
 
+    @Test
+    public void submit() throws MessagingProcessingException, IOException {
         Submission submission = submissionUtil.createSubmission();
         uploadPmode();
         final String messageId = databaseMessageHandler.submit(submission, "mybackend");
@@ -68,7 +78,36 @@ public class DatabaseMessageHandlerTestIT extends AbstractIT {
         filters.put("receivedTo", new Date());
         filters.put("messageSubtype", null);
         messagesLogService.countAndFindPaged(MessageType.USER_MESSAGE, 0, 10, "received", false, filters);
+    }
 
+    @Test
+    public void submitWithNoFromPartyId() throws MessagingProcessingException, IOException {
+        Submission submission = submissionUtil.createSubmission();
+        submission.getFromParties().clear();
+
+        uploadPmode();
+        try {
+            databaseMessageHandler.submit(submission, "mybackend");
+            Assert.fail("Messaging exception should have been thrown");
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof MessagingProcessingException);
+            Assert.assertTrue(e.getMessage().contains("Mandatory field From PartyId is not provided"));
+        }
+    }
+
+    @Test
+    public void submitWithNoToPartyId() throws MessagingProcessingException, IOException {
+        Submission submission = submissionUtil.createSubmission();
+        submission.getToParties().clear();
+
+        uploadPmode();
+        try {
+            databaseMessageHandler.submit(submission, "mybackend");
+            Assert.fail("Messaging exception should have been thrown");
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof MessagingProcessingException);
+            Assert.assertTrue(e.getMessage().contains("ValueInconsistent detail: Mandatory field To PartyId is not provided"));
+        }
     }
 
 
