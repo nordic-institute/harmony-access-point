5cb3b4f772c12d2730a0f920c090b09f794e954d
Ioana DRAGUSANU <idragusa@gmail.com>
2021-07-06 11:53:25 +0300

    [EDELIVERY-8237] Refactor sql functions for new id_pk

--- Domibus-MSH-db/src/main/resources/db/scripts/oracle-sequence-functions.sql
+++ Domibus-MSH-db/src/main/resources/db/scripts/oracle-sequence-functions.sql
@@ -1,25 +1,34 @@
-
--- This function migrates and old ID_PK to the new sequence id format
-
-CREATE OR REPLACE FUNCTION migrate_seq(id_pk IN NUMBER,
-                                       creation_time IN DATE)
-    RETURN NUMBER IS
-    seq_id NUMBER;
-BEGIN
-    select to_number(to_char(creation_time, 'YYMMDDHH') || to_char(id_pk, 'FM0000000000')) into seq_id from dual;
-    RETURN seq_id;
-END;
-
-
--- This function generates a new sequence id, to be used for manual inserts
-
-CREATE OR REPLACE FUNCTION gen_domibus_scalable_sequence
-    RETURN Number
+-- This function generates a sequence id based on DOMIBUS_SCALABLE_SEQUENCE
+CREATE OR REPLACE FUNCTION generate_domibus_scalable_sequence(incr IN NUMBER,
+                                                              creation_time IN DATE)
+    RETURN NUMBER
     IS
     seq_id Number;
 BEGIN
-    select to_number(to_char(SYSDATE, 'YYMMDDHH') || to_char(DOMIBUS_SCALABLE_SEQUENCE.nextval, 'FM0000000000'))
-    into seq_id
-    from dual;
-    RETURN seq_id;
+    DECLARE
+        len CONSTANT         STRING(20) := 'FM0000000000';
+        date_format CONSTANT STRING(10) := 'YYMMDDHH';
+    BEGIN
+        SELECT to_number(to_char(creation_time, date_format) || to_char(incr, len))
+        INTO seq_id
+        FROM dual;
+        RETURN seq_id;
+    END;
+END;
+
+-- This function generates a sequence id based on DOMIBUS_SCALABLE_SEQUENCE for a new entry
+CREATE OR REPLACE FUNCTION generate_id
+    RETURN NUMBER IS
+    seq_id NUMBER;
+BEGIN
+    RETURN generate_domibus_scalable_sequence(DOMIBUS_SCALABLE_SEQUENCE.nextval, SYSDATE);
+END;
+
+-- This function generates a new sequence id based on DOMIBUS_SCALABLE_SEQUENCE for an old entry based on old id_pk and old creation_time
+CREATE OR REPLACE FUNCTION generate_new_id(old_id IN NUMBER,
+                                           creation_time IN DATE)
+    RETURN NUMBER IS
+    seq_id NUMBER;
+BEGIN
+    RETURN generate_domibus_scalable_sequence(old_id, creation_time);
 END;
