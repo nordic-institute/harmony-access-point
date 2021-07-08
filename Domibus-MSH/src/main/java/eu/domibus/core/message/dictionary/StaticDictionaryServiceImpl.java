4a71367aa58d4e5d97c9ecf09bead5cfde7a67db
ion.perpegel <ion.perpegel@gmail.com>
2021-07-08 08:27:34 +0300

    [EDELIVERY-8147] in multi-tenancy, create static dictionary entries at startup for every domain

--- Domibus-MSH/src/main/java/eu/domibus/core/message/dictionary/StaticDictionaryServiceImpl.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/message/dictionary/StaticDictionaryServiceImpl.java
@@ -3,11 +3,19 @@ package eu.domibus.core.message.dictionary;
 import eu.domibus.api.model.MSHRole;
 import eu.domibus.api.model.MessageStatus;
 import eu.domibus.api.model.NotificationStatus;
+import eu.domibus.api.multitenancy.Domain;
+import eu.domibus.api.multitenancy.DomainService;
+import eu.domibus.api.multitenancy.DomainTaskExecutor;
+import eu.domibus.api.property.DomibusConfigurationService;
 import eu.domibus.core.message.MessageStatusDao;
+import eu.domibus.logging.DomibusLogger;
+import eu.domibus.logging.DomibusLoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Ion Perpegel
@@ -16,18 +24,47 @@ import java.util.Arrays;
 @Service
 public class StaticDictionaryServiceImpl implements StaticDictionaryService {
 
+    public static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(StaticDictionaryServiceImpl.class);
+
     protected MessageStatusDao messageStatusDao;
     protected NotificationStatusDao notificationStatusDao;
     protected MshRoleDao mshRoleDao;
+    protected DomibusConfigurationService domibusConfigurationService;
+    protected DomainTaskExecutor domainTaskExecutor;
+    protected DomainService domainService;
 
-    public StaticDictionaryServiceImpl(MessageStatusDao messageStatusDao, NotificationStatusDao notificationStatusDao, MshRoleDao mshRoleDao) {
+    public StaticDictionaryServiceImpl(MessageStatusDao messageStatusDao,
+                                       NotificationStatusDao notificationStatusDao,
+                                       MshRoleDao mshRoleDao,
+                                       DomibusConfigurationService domibusConfigurationService,
+                                       DomainTaskExecutor domainTaskExecutor,
+                                       DomainService domainService) {
         this.messageStatusDao = messageStatusDao;
         this.notificationStatusDao = notificationStatusDao;
         this.mshRoleDao = mshRoleDao;
+        this.domibusConfigurationService = domibusConfigurationService;
+        this.domainTaskExecutor = domainTaskExecutor;
+        this.domainService = domainService;
     }
 
     @Transactional
     public void createStaticDictionaryEntries() {
+        LOG.debug("Start checking and creating static dictionary entries if missing");
+
+        if (domibusConfigurationService.isSingleTenantAware()) {
+            LOG.debug("Start checking and creating static dictionary entries in single tenancy mode");
+            this.createStaticDictionaryEntriesForDomain();
+            return;
+        }
+
+        final List<Domain> domains = domainService.getDomains();
+        for (Domain domain : domains) {
+            LOG.debug("Start checking and creating static dictionary entries for domain [{}]", domain);
+            domainTaskExecutor.submit(() -> this.createStaticDictionaryEntriesForDomain(), domain, true, 1L, TimeUnit.MINUTES);
+        }
+    }
+
+    protected void createStaticDictionaryEntriesForDomain() {
         Arrays.stream(MessageStatus.values()).forEach(messageStatus -> messageStatusDao.findOrCreate(messageStatus));
         Arrays.stream(NotificationStatus.values()).forEach(notificationStatus -> notificationStatusDao.findOrCreate(notificationStatus));
         Arrays.stream(MSHRole.values()).forEach(mshRole -> mshRoleDao.findOrCreate(mshRole));
