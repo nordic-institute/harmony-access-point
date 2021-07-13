8eb3147373938a5fb3f2b17a4f3c1b1d61b5c40a
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2021-07-13 12:01:51 +0200

    [EDELIVERY-7881] Revert replacing interface with final class pattern

--- Domibus-plugin-api/src/main/java/eu/domibus/common/JMSConstants.java
+++ Domibus-plugin-api/src/main/java/eu/domibus/common/JMSConstants.java
@@ -5,9 +5,7 @@ package eu.domibus.common;
  * @author Sebastian-Ion TINCU
  * @since 4.2
  */
-public final class JMSConstants {
-
-    private JMSConstants() {}
+public interface JMSConstants {
 
     /**
      * Bean name for the JMS connection factory that is to be used when working with Spring's
@@ -16,7 +14,7 @@ public final class JMSConstants {
      * <p><b>Note: for performance reasons, the actual bean instance must implement caching of message producers (e.g.
      * {@link org.springframework.jms.connection.CachingConnectionFactory})<b/></p>
      */
-    public static final String DOMIBUS_JMS_CACHING_CONNECTION_FACTORY = "domibusJMS-CachingConnectionFactory";
+    String DOMIBUS_JMS_CACHING_CONNECTION_FACTORY = "domibusJMS-CachingConnectionFactory";
 
     /**
      * Bean name for the JMS connection factory that is to be used when working with Spring
@@ -26,21 +24,21 @@ public final class JMSConstants {
      * themselves and must not be used in conjunction with the
      * {@link org.springframework.jms.connection.CachingConnectionFactory} if dynamic scaling is required.<b/></p>
      */
-    public static final String DOMIBUS_JMS_CONNECTION_FACTORY = "domibusJMS-ConnectionFactory";
+    String DOMIBUS_JMS_CONNECTION_FACTORY = "domibusJMS-ConnectionFactory";
 
     /**
      * Queue names in Domibus
      */
-    public static final String SEND_MESSAGE_QUEUE = "sendMessageQueue";
-    public static final String SEND_LARGE_MESSAGE_QUEUE = "sendLargeMessageQueue";
-    public static final String SPLIT_AND_JOIN_QUEUE = "splitAndJoinQueue";
-    public static final String PULL_MESSAGE_QUEUE = "pullMessageQueue";
-    public static final String SEND_PULL_RECEIPT_QUEUE = "sendPullReceiptQueue";
-    public static final String RETENTION_MESSAGE_QUEUE = "retentionMessageQueue";
-    public static final String ALERT_MESSAGE_QUEUE = "alertMessageQueue";
-    public static final String UI_REPLICATION_QUEUE = "uiReplicationQueue";
-    public static final String NOTIFY_BACKEND_QUEUE = "notifyBackendQueue";
-    public static final String UNKNOWN_RECEIVER_QUEUE = "unknownReceiverQueue";
-    public static final String CLUSTER_COMMAND_TOPIC = "clusterCommandTopic";
+    String SEND_MESSAGE_QUEUE = "sendMessageQueue";
+    String SEND_LARGE_MESSAGE_QUEUE = "sendLargeMessageQueue";
+    String SPLIT_AND_JOIN_QUEUE = "splitAndJoinQueue";
+    String PULL_MESSAGE_QUEUE = "pullMessageQueue";
+    String SEND_PULL_RECEIPT_QUEUE = "sendPullReceiptQueue";
+    String RETENTION_MESSAGE_QUEUE = "retentionMessageQueue";
+    String ALERT_MESSAGE_QUEUE = "alertMessageQueue";
+    String UI_REPLICATION_QUEUE = "uiReplicationQueue";
+    String NOTIFY_BACKEND_QUEUE = "notifyBackendQueue";
+    String UNKNOWN_RECEIVER_QUEUE = "unknownReceiverQueue";
+    String CLUSTER_COMMAND_TOPIC = "clusterCommandTopic";
 
 }
