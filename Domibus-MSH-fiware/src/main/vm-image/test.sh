49b4a335b71c219d23f65aa245ee7491d27fd00a
Cosmin Baciu <cosmin.baciu@ext.ec.europa.eu>
2021-07-26 10:45:50 +0200

    EDELIVERY-8342 - Remove fiware module

--- Domibus-MSH-fiware/src/main/vm-image/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-if ! curl --output /dev/null --silent --head --fail http://localhost:8080/domibus; then
-	echo "Domibus failed .......... [NOT OK]";
-	exit 1;
-fi
-
-echo "Domibus started ....................... [OK]"
-
