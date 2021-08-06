247de052434838f64446733878fe6c0c34a7c607
Ioana DRAGUSANU <idragusa@gmail.com>
2021-08-06 20:11:10 +0300

    [EDELIVERY-7836] Review comments

---
+++ Domibus-MSH-db/src/main/resources/db/common/oracle-create-partitions.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE FUNCTION generate_partition(p_date IN DATE)
+    RETURN NUMBER IS
+    p_id NUMBER;
+BEGIN
+    DECLARE
+        date_format CONSTANT STRING(10) := 'YYMMDDHH24';
+    BEGIN
+        SELECT to_number(to_char(p_date, date_format))
+        INTO p_id
+        FROM dual;
+        RETURN p_id;
+    END;
+END;
+//
+
+set serveroutput on size 30000;
+BEGIN
+    DECLARE
+        p_id   NUMBER;
+        p_name VARCHAR2(20);
+        p_high NUMBER;
+    BEGIN
+        FOR p_number IN 0 .. 24
+            LOOP
+                select generate_partition(sysdate + p_number / 24) into p_id from dual;
+                p_name := 'P' || p_id;
+                p_high := p_id || '0000000000';
+                execute immediate 'ALTER TABLE TB_USER_MESSAGE ADD PARTITION ' || p_name || ' VALUES LESS THAN (' ||
+                                  p_high || ')';
+                -- DBMS_OUTPUT.PUT_LINE(p_name || ' ' || p_high);
+            END LOOP;
+    END;
+END;
+//
