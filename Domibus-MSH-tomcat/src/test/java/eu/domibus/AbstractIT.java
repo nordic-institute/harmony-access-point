40632b32198c4f7c711b8c25279ea497cc4b1ce0
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2021-09-06 16:01:25 +0200

    [EDELIVERY-8454] replaced Xstream with Gson

--- Domibus-MSH-tomcat/src/test/java/eu/domibus/AbstractIT.java
+++ Domibus-MSH-tomcat/src/test/java/eu/domibus/AbstractIT.java
@@ -1,6 +1,6 @@
 package eu.domibus;
 
-import com.thoughtworks.xstream.XStream;
+import com.google.gson.Gson;
 import eu.domibus.api.multitenancy.DomainContextProvider;
 import eu.domibus.api.multitenancy.DomainService;
 import eu.domibus.common.MessageStatus;
@@ -40,6 +40,7 @@ import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -66,6 +67,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
@@ -164,8 +166,10 @@ public abstract class AbstractIT {
     }
 
     protected UserMessage getUserMessageTemplate() throws IOException {
-        XStream xStream = new XStream();
-        return (UserMessage) xStream.fromXML(new ClassPathResource("dataset/messages/UserMessageTemplate.xml").getInputStream());
+        Resource userMessageTemplate = new ClassPathResource("dataset/messages/UserMessageTemplate.json");
+        String jsonStr = new String(IOUtils.toByteArray(userMessageTemplate.getInputStream()), StandardCharsets.UTF_8);
+        UserMessage userMessage = new Gson().fromJson(jsonStr, UserMessage.class);
+        return userMessage;
     }
 
 
