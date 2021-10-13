3942d437839764503b3155e610d4427240fddd94
soumya <Soumya.AZHIKODE-CHANDRAN@ext.ec.europa.eu>
2021-10-13 10:26:38 +0200

    [EDELIVERY-8509]-Review comment fix

--- Domibus-MSH/src/main/java/eu/domibus/core/user/ui/UserRoleDaoImpl.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/user/ui/UserRoleDaoImpl.java
@@ -1,11 +1,8 @@
 package eu.domibus.core.user.ui;
 
 import eu.domibus.core.dao.BasicDao;
-import eu.domibus.logging.DomibusLogger;
-import eu.domibus.logging.DomibusLoggerFactory;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -16,9 +13,6 @@ import java.util.List;
 
 @Repository
 public class UserRoleDaoImpl extends BasicDao<UserRole> implements UserRoleDao {
-
-    private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(UserRoleDaoImpl.class);
-
     public UserRoleDaoImpl() {
         super(UserRole.class);
     }
@@ -33,11 +27,6 @@ public class UserRoleDaoImpl extends BasicDao<UserRole> implements UserRoleDao {
     public UserRole findByName(final String roleName) {
         TypedQuery<UserRole> namedQuery = em.createNamedQuery("UserRole.findByName", UserRole.class);
         namedQuery.setParameter("ROLE_NAME", roleName.trim().toUpperCase());
-        try {
-            return namedQuery.getSingleResult();
-        } catch (NoResultException nrEx) {
-            LOG.debug("Query UserRole.findByName did not find any result for user role with role name [" + roleName + "]");
-            return null;
-        }
+        return namedQuery.getSingleResult();
     }
 }
