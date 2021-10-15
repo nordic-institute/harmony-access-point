ee17b5f04b23dec77b48cbc3c41242e78e3da751
venugar <Arun-Raj.VENUGOPAL@ext.ec.europa.eu>
2021-10-15 12:35:02 +0200

    [EDELIVERY-8502] Correcting test failure

--- Domibus-MSH/src/test/java/eu/domibus/core/plugin/handler/DatabaseMessageHandlerTest.java
+++ Domibus-MSH/src/test/java/eu/domibus/core/plugin/handler/DatabaseMessageHandlerTest.java
@@ -664,6 +664,8 @@ public class DatabaseMessageHandlerTest {
             times = 1;
             backendMessageValidator.validateSubmissionSending(submission);
             times = 1;
+            backendMessageValidator.validateSubmissionPayload(submission);
+            times = 1;
             errorLogService.createErrorLog((EbMS3Exception) any, MSHRole.SENDING, null);
             times = 1;
 
