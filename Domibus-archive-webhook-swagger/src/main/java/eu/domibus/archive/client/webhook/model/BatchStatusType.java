08db2c27c519a7a484f0b2fbf393168eeacec512
Joze RIHTARSIC <joze.RIHTARSIC@ext.ec.europa.eu>
2021-10-28 15:53:02 +0200

    Update statuses

--- Domibus-archive-webhook-swagger/src/main/java/eu/domibus/archive/client/webhook/model/BatchStatusType.java
+++ Domibus-archive-webhook-swagger/src/main/java/eu/domibus/archive/client/webhook/model/BatchStatusType.java
@@ -5,9 +5,43 @@ package eu.domibus.archive.client.webhook.model;
  * @since 5.0
  */
 public enum BatchStatusType {
+    /**
+     * Batch had been created
+     * CHANGE starting to QUEUED
+     */
+    QUEUED,
+    /**
+     * Batch had been picked up by for processing
+     */
+    STARTED,
+    /**
+     * Batch has failed
+     */
+    RETRIED,
+    /**
+     * Batch has failed to export all messages
+     */
+    FAILED,
+    /**
+     * Batch was completed
+     */
     EXPORTED,
-    ARCHIVED,
-    ARCHIVE_FAILED,
+    /**
+     * End status of client does not notify ARCHIVED in time
+     */
     EXPIRED,
-    DELETED;
+    /**
+     * Batch client finished the archived the batch
+     */
+    ARCHIVED,
+    /**
+     * Batch client notified of a failure in the archiving process
+     */
+    ARCHIVE_FAILED,
+    /**
+     * Deleted by the Domibus if the batch is expired, or ARCHIVED or failed to archived by client
+     *
+     * Deletes only filesystem data!
+     */
+    DELETED,
 }
