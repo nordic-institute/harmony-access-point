fe6866e4ae0d5302544b126a4eae395709a0b9fb
ion.perpegel <ion.perpegel@gmail.com>
2021-11-08 14:49:14 +0200

    [EDELIVERY-8615] alert-related unit test fix (new alert type added)

--- Domibus-MSH/src/test/java/eu/domibus/web/rest/AlertResourceTest.java
+++ Domibus-MSH/src/test/java/eu/domibus/web/rest/AlertResourceTest.java
@@ -386,7 +386,7 @@ public class AlertResourceTest {
         }};
 
         List<String> alertTypesAsStrings = alertResource.getAlertTypesAsStrings();
-        assertEquals(15, alertTypesAsStrings.size());
+        assertEquals(16, alertTypesAsStrings.size());
         assertTrue(alertTypesAsStrings.containsAll(AlertResource.forbiddenAlertTypesExtAuthProvider.stream().map(alertType -> alertType.name()).collect(Collectors.toSet())));
     }
 
@@ -398,7 +398,7 @@ public class AlertResourceTest {
         }};
 
         List<String> alertTypesAsStrings = alertResource.getAlertTypesAsStrings();
-        assertEquals(10, alertTypesAsStrings.size());
+        assertEquals(11, alertTypesAsStrings.size());
         assertFalse(alertTypesAsStrings.containsAll(AlertResource.forbiddenAlertTypesExtAuthProvider.stream().map(alertType -> alertType.name()).collect(Collectors.toSet())));
     }
 
