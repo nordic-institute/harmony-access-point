d26eb5acf0f5ebf0a2dc97684918a1041f9d209f
ion.perpegel <ion.perpegel@gmail.com>
2021-11-11 22:01:46 +0200

    [EDELIVERY-8578] add earchiving cleanup job

---
+++ Domibus-MSH/src/main/java/eu/domibus/core/earchive/job/EArchivingRetentionService.java
@@ -0,0 +1,78 @@
+package eu.domibus.core.earchive.job;
+
+import eu.domibus.api.earchive.EArchiveBatchStatus;
+import eu.domibus.api.property.DomibusPropertyProvider;
+import eu.domibus.core.earchive.DomibusEArchiveException;
+import eu.domibus.core.earchive.EArchiveBatchDao;
+import eu.domibus.core.earchive.EArchiveBatchEntity;
+import eu.domibus.core.earchive.storage.EArchiveFileStorageProvider;
+import eu.domibus.logging.DomibusLogger;
+import eu.domibus.logging.DomibusLoggerFactory;
+import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.VFS;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.ZoneOffset;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static eu.domibus.api.property.DomibusPropertyMetadataManagerSPI.DOMIBUS_EARCHIVE_RETENTION_DAYS;
+import static eu.domibus.api.property.DomibusPropertyMetadataManagerSPI.DOMIBUS_EARCHIVE_RETENTION_DELETE_MAX;
+
+/**
+ * @author Ion Perpegel
+ * @since 5.0
+ */
+@Service
+public class EArchivingRetentionService {
+
+    private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(EArchivingRetentionService.class);
+
+    private final EArchiveBatchDao eArchiveBatchDao;
+
+    private final DomibusPropertyProvider domibusPropertyProvider;
+
+    protected final EArchiveFileStorageProvider storageProvider;
+
+    public EArchivingRetentionService(EArchiveBatchDao eArchiveBatchDao, DomibusPropertyProvider domibusPropertyProvider, EArchiveFileStorageProvider storageProvider) {
+        this.eArchiveBatchDao = eArchiveBatchDao;
+        this.domibusPropertyProvider = domibusPropertyProvider;
+        this.storageProvider = storageProvider;
+    }
+
+    @Transactional
+    public void expireBatches() {
+        final Integer retentionInDays = domibusPropertyProvider.getIntegerProperty(DOMIBUS_EARCHIVE_RETENTION_DAYS);
+        final Date limitDate = Date.from(java.time.ZonedDateTime.now(ZoneOffset.UTC).minusDays(retentionInDays).toInstant());
+
+        LOG.debug("Expiring batches older than [{}]", limitDate);
+
+        eArchiveBatchDao.expireBatches(limitDate);
+    }
+
+    @Transactional
+    public void cleanStoredBatches() {
+        final Integer maxBatchesToDelete = domibusPropertyProvider.getIntegerProperty(DOMIBUS_EARCHIVE_RETENTION_DELETE_MAX);
+
+        List<EArchiveBatchStatus> eligibleStatuses = Arrays.asList(EArchiveBatchStatus.EXPIRED, EArchiveBatchStatus.ARCHIVED, EArchiveBatchStatus.ARCHIVE_FAILED);
+        List<EArchiveBatchEntity> batches = eArchiveBatchDao.findBatchesByStatus(eligibleStatuses, maxBatchesToDelete);
+
+        LOG.debug("[{}] batches eligible for deletion found", batches.size());
+
+        batches.stream().forEach(batch -> deleteBatch(batch));
+    }
+
+    protected void deleteBatch(EArchiveBatchEntity batch) {
+        LOG.debug("Deleting earchive structure for batchId [{}]", batch.getBatchId());
+
+        try (FileObject batchDirectory = VFS.getManager().resolveFile(storageProvider.getCurrentStorage().getStorageDirectory(), batch.getBatchId())) {
+            batchDirectory.deleteAll();
+        } catch (Exception e) {
+            throw new DomibusEArchiveException("Could not delete earchive batch [" + batch.getBatchId() + "]", e);
+        }
+
+        batch.setEArchiveBatchStatus(EArchiveBatchStatus.DELETED);
+    }
+}
