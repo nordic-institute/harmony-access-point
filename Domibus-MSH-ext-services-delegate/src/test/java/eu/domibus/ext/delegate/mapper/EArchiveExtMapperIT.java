272cd0c394de10d09d156bff35b13ae26b58cc3d
François <francois.gauti3r@gmail.com>
2021-12-08 16:43:52 +0100

    [EDELIVERY-8715] eArchiving: add batchId to /ext/archive/batches/exported Code review

--- Domibus-MSH-ext-services-delegate/src/test/java/eu/domibus/ext/delegate/mapper/EArchiveExtMapperIT.java
+++ Domibus-MSH-ext-services-delegate/src/test/java/eu/domibus/ext/delegate/mapper/EArchiveExtMapperIT.java
@@ -52,29 +52,6 @@ public class EArchiveExtMapperIT {
     }
 
     @Test
-    public void testArchiveBatchToQueuedBatch_sanitizer() {
-        // given
-        long datetime = 2108080L;
-        EArchiveBatchRequestDTO toConvert = new EArchiveBatchRequestDTO();
-        toConvert.setBatchId(UUID.randomUUID().toString());
-        toConvert.setRequestType("SANITIZER");
-        toConvert.setMessages(Arrays.asList("messageId1", "messageId1"));
-        toConvert.setTimestamp(Calendar.getInstance().getTime());
-        toConvert.setMessageStartId(2108080 * 10000000000L + 1234L);
-        toConvert.setMessageEndId(2108080 * 10000000000L + 12345L);
-        // when
-        final BatchDTO converted = archiveExtMapper.archiveBatchToBatch(toConvert);
-        // then
-        assertEquals(toConvert.getBatchId(), converted.getBatchId());
-        assertArrayEquals(toConvert.getMessages().toArray(), converted.getMessages().toArray());
-        assertEquals(BatchRequestType.CONTINUOUS, converted.getRequestType());
-        assertEquals(toConvert.getTimestamp(), converted.getEnqueuedTimestamp());
-        // extract just dates
-        assertEquals(datetime, converted.getMessageEndDate().longValue());
-        assertEquals(datetime, converted.getMessageStartDate().longValue());
-    }
-
-    @Test
     public void archiveBatchToExportBatch_continuous() {
         // given
         long datetime = 2108080L;
