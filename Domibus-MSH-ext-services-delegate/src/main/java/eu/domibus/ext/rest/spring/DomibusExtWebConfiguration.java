2617c08757b1d393c28883ff5a0d405a08938561
Joze RIHTARSIC <joze.RIHTARSIC@ext.ec.europa.eu>
2021-12-13 07:31:39 +0100

    Update configuration of the ObjectMapper for dedicated service

--- Domibus-MSH-ext-services-delegate/src/main/java/eu/domibus/ext/rest/spring/DomibusExtWebConfiguration.java
+++ Domibus-MSH-ext-services-delegate/src/main/java/eu/domibus/ext/rest/spring/DomibusExtWebConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -33,7 +32,6 @@ public class DomibusExtWebConfiguration implements WebMvcConfigurer {
 
         registry.addInterceptor(authenticationInterceptor())
                 .addPathPatterns("/ext/**");
-
     }
 
     @Bean
@@ -43,14 +41,14 @@ public class DomibusExtWebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.stream().filter(c -> c instanceof MappingJackson2HttpMessageConverter).forEach(c -> {
-            // add JSON mapper with Jdk8Module, JavaTimeModule when creating the ObjectMapper
-            // to property format date as: "2021-12-01T14:52:00Z"
-            ((MappingJackson2HttpMessageConverter) c).setObjectMapper(new ObjectMapper() {{
-                registerModule(new JavaTimeModule());
-                registerModule(new Jdk8Module());
-                disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-            }});
-        });
+        // Configure Object Mapper with format date as: "2021-12-01T14:52:00Z"
+        // for objects in package: eu.domibus.ext.domain.archive
+        DomibusExtMappingConverter converter = new DomibusExtMappingConverter("eu.domibus.ext.domain.archive");
+        converter.setObjectMapper(new ObjectMapper() {{
+            registerModule(new JavaTimeModule());
+            registerModule(new Jdk8Module());
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        }});
+        converters.add(0, converter);
     }
 }
