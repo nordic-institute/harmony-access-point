03e61adc0981c9cb257cf0dbb6227a4f9dd9d48e
ionperpegel <ion.perpegel@gmail.com>
2021-12-24 12:51:56 +0200

    [EDELIVERY-8777] add tests to multiDomainCryptoService class

--- Domibus-MSH-tomcat/src/test/java/eu/domibus/core/MultiDomainCryptoServiceIT.java
+++ Domibus-MSH-tomcat/src/test/java/eu/domibus/core/MultiDomainCryptoServiceIT.java
@@ -3,8 +3,11 @@ package eu.domibus.core;
 import eu.domibus.AbstractIT;
 import eu.domibus.api.multitenancy.Domain;
 import eu.domibus.api.multitenancy.DomainService;
+import eu.domibus.api.pki.CertificateEntry;
 import eu.domibus.api.pki.CertificateInitValueType;
 import eu.domibus.api.pki.CertificateService;
+import eu.domibus.api.property.DomibusConfigurationService;
+import eu.domibus.api.security.TrustStoreEntry;
 import eu.domibus.core.crypto.DefaultDomainCryptoServiceSpiImpl;
 import eu.domibus.core.crypto.DomainCryptoServiceFactory;
 import eu.domibus.core.crypto.MultiDomainCryptoServiceImpl;
@@ -16,7 +19,14 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.cert.X509Certificate;
 import java.util.Arrays;
+import java.util.Base64;
+import java.util.List;
 
 import static eu.domibus.core.crypto.MultiDomainCryptoServiceImpl.DOMIBUS_TRUSTSTORE_NAME;
 
@@ -45,6 +55,9 @@ public class MultiDomainCryptoServiceIT extends AbstractIT {
     @Autowired
     CertificateService certificateService;
 
+    @Autowired
+    DomibusConfigurationService domibusConfigurationService;
+
     @Test
     @Transactional
     public void persistTruststoresIfApplicable() {
@@ -68,4 +81,30 @@ public class MultiDomainCryptoServiceIT extends AbstractIT {
         Assert.assertTrue(isPersisted);
     }
 
+    @Test
+    @Transactional
+    public void getTrustStoreEntries() {
+        multiDomainCryptoService.persistTruststoresIfApplicable();
+        List<TrustStoreEntry> trustStoreEntries = certificateService.getTrustStoreEntries(DOMIBUS_TRUSTSTORE_NAME);
+        Assert.assertTrue(trustStoreEntries.size() == 2);
+    }
+
+    @Test
+    @Transactional
+    public void addCertificate() throws IOException {
+        multiDomainCryptoService.persistTruststoresIfApplicable();
+
+        List<TrustStoreEntry> trustStoreEntries = certificateService.getTrustStoreEntries(DOMIBUS_TRUSTSTORE_NAME);
+        Assert.assertTrue(trustStoreEntries.size() == 2);
+
+        Path path = Paths.get(domibusConfigurationService.getConfigLocation(), "keystores", "green_gw.cer");
+        byte[] content = Files.readAllBytes(path);
+        String green_gw = "green_gw";
+        X509Certificate x509Certificate = certificateService.loadCertificateFromString(Base64.getEncoder().encodeToString(content));
+        multiDomainCryptoService.addCertificate(domainContextProvider.getCurrentDomain(), Arrays.asList(new CertificateEntry(green_gw, x509Certificate)), true);
+
+        trustStoreEntries = certificateService.getTrustStoreEntries(DOMIBUS_TRUSTSTORE_NAME);
+        Assert.assertTrue(trustStoreEntries.size() == 3);
+        Assert.assertTrue(trustStoreEntries.stream().anyMatch(entry -> entry.getName().equals(green_gw)));
+    }
 }
