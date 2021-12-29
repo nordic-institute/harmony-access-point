274a9509e29e50b2039042ceb5a09b6ddb0a802c
ionperpegel <ion.perpegel@gmail.com>
2021-12-29 13:37:20 +0200

    [EDELIVERY-8777] improve test

--- Domibus-MSH-tomcat/src/test/java/eu/domibus/core/message/MessageExchangeServiceIT.java
+++ Domibus-MSH-tomcat/src/test/java/eu/domibus/core/message/MessageExchangeServiceIT.java
@@ -3,18 +3,18 @@ package eu.domibus.core.message;
 import eu.domibus.AbstractIT;
 import eu.domibus.api.model.MSHRole;
 import eu.domibus.api.model.MessageStatus;
-import eu.domibus.api.model.MessageType;
 import eu.domibus.api.model.RawEnvelopeDto;
+import eu.domibus.api.model.UserMessageRaw;
 import eu.domibus.common.MessageDaoTestUtil;
 import eu.domibus.core.message.nonrepudiation.UserMessageRawEnvelopeDao;
-import eu.domibus.web.rest.ro.MessageLogRO;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Date;
-import java.util.HashMap;
 
 /**
  * @author Ion Perpegel
@@ -39,13 +39,16 @@ public class MessageExchangeServiceIT extends AbstractIT {
     public void findUserMessageById() {
 
         String messageId = "msg1";
-        messageDaoTestUtil.createUserMessageLog(messageId, new Date(), MSHRole.RECEIVING, MessageStatus.RECEIVED);
-
         String testXMLEnvelope = "testXMLEnvelope";
+
+        messageDaoTestUtil.createUserMessageLog(messageId, new Date(), MSHRole.RECEIVING, MessageStatus.RECEIVED);
         messageExchangeService.saveRawXml(testXMLEnvelope, messageId);
         RawEnvelopeDto rawXmlByMessageId = userMessageRawEnvelopeDao.findRawXmlByMessageId(messageId);
         String rawXmlMessage = rawXmlByMessageId.getRawXmlMessage();
         Assert.assertEquals(testXMLEnvelope, rawXmlMessage);
+
+        UserMessageRaw byReference = userMessageRawEnvelopeDao.findByReference(rawXmlByMessageId.getId());
+        Assert.assertTrue(Arrays.equals(byReference.getRawXML(), testXMLEnvelope.getBytes(StandardCharsets.UTF_8)));
     }
 
 }
