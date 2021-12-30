ac5d0442c462dd702d2ba5585d48a4ada6acf92b
ionperpegel <ion.perpegel@gmail.com>
2021-12-30 10:53:30 +0200

    [EDELIVERY-8777] add test for clean stored batches

--- Domibus-MSH-tomcat/src/test/java/eu/domibus/core/earchive/EArchivingRetentionServiceIT.java
+++ Domibus-MSH-tomcat/src/test/java/eu/domibus/core/earchive/EArchivingRetentionServiceIT.java
@@ -5,20 +5,35 @@ import eu.domibus.api.earchive.EArchiveBatchFilter;
 import eu.domibus.api.earchive.EArchiveBatchRequestDTO;
 import eu.domibus.api.earchive.EArchiveBatchStatus;
 import eu.domibus.api.earchive.EArchiveRequestType;
+import eu.domibus.api.multitenancy.DomainService;
+import eu.domibus.api.property.DomibusPropertyProvider;
+import eu.domibus.core.earchive.eark.FileSystemEArchivePersistenceE2EIT;
 import eu.domibus.core.earchive.job.EArchivingRetentionService;
+import eu.domibus.core.earchive.storage.EArchiveFileStorageProvider;
+import eu.domibus.logging.DomibusLogger;
+import eu.domibus.logging.DomibusLoggerFactory;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.time.DateUtils;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import static eu.domibus.api.earchive.EArchiveBatchStatus.DELETED;
 import static eu.domibus.api.earchive.EArchiveBatchStatus.EXPIRED;
+import static eu.domibus.api.property.DomibusPropertyMetadataManagerSPI.DOMIBUS_EARCHIVE_ACTIVE;
+import static eu.domibus.api.property.DomibusPropertyMetadataManagerSPI.DOMIBUS_EARCHIVE_STORAGE_LOCATION;
 import static java.util.Collections.singletonList;
 
 /**
@@ -27,6 +42,8 @@ import static java.util.Collections.singletonList;
  */
 
 public class EArchivingRetentionServiceIT extends AbstractIT {
+
+    private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(EArchivingRetentionServiceIT.class);
 
     @Autowired
     EArchivingDefaultService eArchivingService;
@@ -37,12 +54,29 @@ public class EArchivingRetentionServiceIT extends AbstractIT {
     @Autowired
     EArchivingRetentionService eArchivingRetentionService;
 
+    @Autowired
+    protected DomibusPropertyProvider domibusPropertyProvider;
+
+    @Autowired
+    protected EArchiveFileStorageProvider storageProvider;
+
     EArchiveBatchEntity batch1, batch2, batch3, batch4;
+    private File temp;
 
     @Before
     public void setUp() throws Exception {
         waitUntilDatabaseIsInitialized();
-        // prepare
+
+        temp = Files.createTempDirectory(Paths.get("target"), "tmpDirPrefix").toFile();
+        LOG.info("temp folder created: [{}]", temp.getAbsolutePath());
+
+        domibusPropertyProvider.setProperty(DomainService.DEFAULT_DOMAIN, DOMIBUS_EARCHIVE_ACTIVE, "true");
+        domibusPropertyProvider.setProperty(DOMIBUS_EARCHIVE_ACTIVE, "true");
+        domibusPropertyProvider.setProperty(DomainService.DEFAULT_DOMAIN, DOMIBUS_EARCHIVE_STORAGE_LOCATION, temp.getAbsolutePath());
+        domibusPropertyProvider.setProperty(DOMIBUS_EARCHIVE_STORAGE_LOCATION, temp.getAbsolutePath());
+
+        storageProvider.getCurrentStorage().reset();
+
         Date currentDate = Calendar.getInstance().getTime();
 
         batch1 = eArchiveBatchDao.merge(EArchiveTestUtils.createEArchiveBatchEntity(
@@ -87,7 +121,12 @@ public class EArchivingRetentionServiceIT extends AbstractIT {
 
     }
 
-
+    @After
+    public void tearDown() throws IOException {
+        FileUtils.deleteDirectory(temp);
+        LOG.info("temp folder deleted: [{}]", temp.getAbsolutePath());
+    }
+    
     @Test
     @Transactional
     public void getBatchListFilterDates() {
@@ -100,4 +139,14 @@ public class EArchivingRetentionServiceIT extends AbstractIT {
         Assert.assertEquals(1, batchRequestsCount.size());
     }
 
+    @Test
+    @Transactional
+    public void cleanStoredBatches() {
+        eArchivingRetentionService.cleanStoredBatches();
+
+        EArchiveBatchFilter filter = new EArchiveBatchFilter(singletonList(DELETED), null, null, null, null, null, null, null, null);
+        List<EArchiveBatchRequestDTO> batchRequestsCount = eArchivingService.getBatchRequestList(filter);
+        // Only one expired
+        Assert.assertEquals(1, batchRequestsCount.size());
+    }
 }
