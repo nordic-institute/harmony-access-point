a4147783f42c63d27d76a9bf7faa20e202f5c3ba
Cosmin Baciu <Cosmin Baciu>
2022-04-11 13:09:02 +0200

    [EDELIVERY-8742] - Fixed setting the cache property for dyn disc

--- Domibus-MSH/src/main/java/eu/domibus/core/cache/DomibusCacheDynamicExpiryPolicy.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/cache/DomibusCacheDynamicExpiryPolicy.java
@@ -1,6 +1,12 @@
 package eu.domibus.core.cache;
 
+import eu.domibus.api.property.DomibusPropertyMetadataManagerSPI;
+import eu.domibus.api.property.DomibusPropertyProvider;
+import eu.domibus.api.spring.SpringContextProvider;
+import eu.domibus.logging.DomibusLogger;
+import eu.domibus.logging.DomibusLoggerFactory;
 import org.ehcache.expiry.ExpiryPolicy;
+import org.springframework.context.ApplicationContext;
 
 import java.time.Duration;
 import java.util.function.Supplier;
@@ -8,7 +14,7 @@ import java.util.function.Supplier;
 /**
  * Class enables dynamic update of the cache TTL for ehcache 3.x provider. The class must be configured in the
  * ehcache configuration. The Default value of the DomibusCacheDynamicExpiryPolicy is 1h.
- *
+ * <p>
  * Example:
  * <pre>
  * &lt;cache alias="lookupInfo">
@@ -16,21 +22,37 @@ import java.util.function.Supplier;
  *     <class>eu.domibus.core.cache.DomibusCacheDynamicExpiryPolicy</class>
  *   &lt;/expiry>
  * &lt;/cache>
- *</pre>
+ * </pre>
  *
  * @author Joze Rihtarsic
+ * @author Cosmin Baciu
  * @since 5.0
  */
 public class DomibusCacheDynamicExpiryPolicy implements ExpiryPolicy {
 
-    // default duration for 1H
-    Duration expiryDuration = Duration.ofSeconds(3600);
+    private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(DomibusCacheDynamicExpiryPolicy.class);
+
+    protected volatile Duration expiryDuration = null;
 
     /**
      * {@inheritDoc}
      */
     @Override
     public Duration getExpiryForCreation(Object key, Object value) {
+        if (expiryDuration == null) {
+            synchronized (DomibusCacheDynamicExpiryPolicy.class) {
+                if (expiryDuration == null) {
+                    LOG.debug("Getting the TTL for dynamic discovery lookup cache");
+
+                    final ApplicationContext applicationContext = SpringContextProvider.getApplicationContext();
+                    final DomibusPropertyProvider domibusPropertyProvider = applicationContext.getBean(DomibusPropertyProvider.SPRING_BEAN_NAME, DomibusPropertyProvider.class);
+                    final Long ttlCache = domibusPropertyProvider.getLongProperty(DomibusPropertyMetadataManagerSPI.DOMIBUS_DYNAMICDISCOVERY_LOOKUP_CACHE_TTL);
+                    expiryDuration = Duration.ofSeconds(ttlCache);
+
+                    LOG.debug("Using the TTL for dynamic discovery lookup cache [{}]", expiryDuration);
+                }
+            }
+        }
         return expiryDuration;
     }
 
@@ -50,12 +72,7 @@ public class DomibusCacheDynamicExpiryPolicy implements ExpiryPolicy {
         return null;
     }
 
-    @Override
-    public String toString() {
-        return "Dynamic discovery expiry configuration ["+expiryDuration+"]";
-    }
-
-    public void setTTLInSeconds(long ttlInSeconds) {
-        this.expiryDuration = Duration.ofSeconds(ttlInSeconds);
+    public void resetTTLInSeconds() {
+        this.expiryDuration = null;
     }
 }
