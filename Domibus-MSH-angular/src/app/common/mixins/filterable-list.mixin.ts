6252104e52e3d87b79ddb5d58e7632271eb1f001
ionperpegel <ion.perpegel@gmail.com>
2022-04-18 11:47:24 +0300

    [EDELIVERY-9082] create custom cloning of search params to fix Date type cloning

--- Domibus-MSH-angular/src/app/common/mixins/filterable-list.mixin.ts
+++ Domibus-MSH-angular/src/app/common/mixins/filterable-list.mixin.ts
@@ -69,7 +69,7 @@ let FilterableListMixin = (superclass: Constructable) => class extends superclas
    */
   public filterData(): Promise<any> {
     if (!this.initialFilter) {
-      this.initialFilter = JSON.parse(JSON.stringify(this.filter));
+      this.initialFilter = this.clone(this.filter);
     }
 
     this.onBeforeFilter();
@@ -175,6 +175,43 @@ let FilterableListMixin = (superclass: Constructable) => class extends superclas
 
   protected onSetFilters() {
   }
+
+  clone(obj) {
+    // Handle the 3 simple types, and null or undefined
+    if (null == obj || 'object' != typeof obj) {
+      return obj;
+    }
+
+    // Handle Date
+    if (obj instanceof Date) {
+      const copy = new Date();
+      copy.setTime(obj.getTime());
+      return copy;
+    }
+
+    // Handle Array
+    if (obj instanceof Array) {
+      const copy = [];
+      for (let i = 0, len = obj.length; i < len; i++) {
+        copy[i] = this.clone(obj[i]);
+      }
+      return copy;
+    }
+
+    // Handle Object
+    if (obj instanceof Object) {
+      const copy = {};
+      for (let attr in obj) {
+        if (obj.hasOwnProperty(attr)) {
+          copy[attr] = this.clone(obj[attr]);
+        }
+      }
+      return copy;
+    }
+
+    throw new Error('Unable to copy obj! Its type isn\'t supported.');
+  }
+
 };
 export default FilterableListMixin;
 
