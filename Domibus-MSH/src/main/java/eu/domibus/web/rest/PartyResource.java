eab59f93974357bd629cd45a291c51a15e802b44
ionperpegel <ion.perpegel@gmail.com>
2022-04-19 16:54:42 +0300

    Revert "[EDELIVERY-8770] fix party identification based on ids"
    This reverts commit 86ed22f4cb54751718b675daba857871c0873f44.

--- Domibus-MSH/src/main/java/eu/domibus/web/rest/PartyResource.java
+++ Domibus-MSH/src/main/java/eu/domibus/web/rest/PartyResource.java
@@ -176,16 +176,20 @@ public class PartyResource extends BaseResource {
                     List<ProcessRo> processesWithPartyAsInitiator = partyResponseRo.getProcessesWithPartyAsInitiator();
                     List<ProcessRo> processesWithPartyAsResponder = partyResponseRo.getProcessesWithPartyAsResponder();
 
-                    List<ProcessRo> processesWithPartyAsInitiatorAndResponder = processesWithPartyAsInitiator.stream()
-                            .filter(init -> processesWithPartyAsResponder.stream().anyMatch(resp -> resp.getEntityId().equals(init.getEntityId())))
+                    List<ProcessRo> processesWithPartyAsInitiatorAndResponder
+                            = processesWithPartyAsInitiator.
+                            stream().
+                            filter(processesWithPartyAsResponder::contains).
+                            collect(Collectors.toList());
+
+                    List<ProcessRo> processWithPartyAsInitiatorOnly = processesWithPartyAsInitiator
+                            .stream()
+                            .filter(processRo -> !processesWithPartyAsInitiatorAndResponder.contains(processRo))
                             .collect(Collectors.toList());
 
-                    List<ProcessRo> processWithPartyAsInitiatorOnly = processesWithPartyAsInitiator.stream()
-                            .filter(init -> !processesWithPartyAsInitiatorAndResponder.stream().anyMatch(initResp -> initResp.getEntityId().equals(init.getEntityId())))
-                            .collect(Collectors.toList());
-
-                    List<ProcessRo> processWithPartyAsResponderOnly = processesWithPartyAsResponder.stream()
-                            .filter(resp -> !processesWithPartyAsInitiatorAndResponder.stream().anyMatch(initResp -> initResp.getEntityId().equals(resp.getEntityId())))
+                    List<ProcessRo> processWithPartyAsResponderOnly = processesWithPartyAsResponder
+                            .stream()
+                            .filter(processRo -> !processesWithPartyAsInitiatorAndResponder.contains(processRo))
                             .collect(Collectors.toList());
 
                     String joinedProcessesWithMeAsInitiatorOnly = processWithPartyAsInitiatorOnly.
