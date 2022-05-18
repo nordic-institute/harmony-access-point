20ceee670ac547c15eb75b3e9ffc0962b02e56fe
ionperpegel <ion.perpegel@gmail.com>
2022-05-18 09:27:07 +0300

    [EDELIVERY-9366] add roles name validation case insensitive

--- Domibus-MSH/src/main/java/eu/domibus/core/pmode/validation/validators/RolesValidator.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/pmode/validation/validators/RolesValidator.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author musatmi
@@ -20,16 +22,24 @@ import java.util.List;
  */
 @Component
 @Order(4)
-public class RolesValidator  implements PModeValidator {
+public class RolesValidator implements PModeValidator {
 
     @Override
     public List<ValidationIssue> validate(Configuration configuration) {
         List<ValidationIssue> issues = new ArrayList<>();
 
         final BusinessProcesses businessProcesses = configuration.getBusinessProcesses();
+
+        List<String> names = businessProcesses.getRoles().stream().map(el -> el.getName().toLowerCase()).collect(Collectors.toList());
+        Set<String> duplicates = names.stream().filter(name -> Collections.frequency(names, name) > 1).collect(Collectors.toSet());
+        if (duplicates.size() > 0) {
+            issues.add(new ValidationIssue("Business process roles contain duplicate names case insensitive:" + duplicates, ValidationIssue.Level.ERROR));
+        }
+
         for (Process process : businessProcesses.getProcesses()) {
             final Role initiatorRole = process.getInitiatorRole();
             final Role responderRole = process.getResponderRole();
+
             if (initiatorRole != null && initiatorRole.equals(responderRole)) {
                 String errorMessage = "For the business process [" + process.getName() + "], the initiator role name and the responder role name are identical [" + initiatorRole.getName() + "]";
                 issues.add(new ValidationIssue(errorMessage, ValidationIssue.Level.WARNING));
