a8a771da4185bde308076f1425cde576f38be87a
idragusa <idragusa@gmail.com>
2022-05-24 15:55:15 +0300

    [EDELIVERY-9410] Fix partition alert exception for disabled alerts

--- Domibus-MSH/src/main/java/eu/domibus/core/alerts/service/EventServiceImpl.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/alerts/service/EventServiceImpl.java
@@ -7,6 +7,7 @@ import eu.domibus.api.model.MessageStatus;
 import eu.domibus.api.model.UserMessage;
 import eu.domibus.api.user.UserEntityBase;
 import eu.domibus.common.model.configuration.Party;
+import eu.domibus.core.alerts.configuration.partitions.PartitionsModuleConfiguration;
 import eu.domibus.core.alerts.configuration.password.PasswordExpirationAlertModuleConfiguration;
 import eu.domibus.core.alerts.dao.EventDao;
 import eu.domibus.core.alerts.model.common.*;
@@ -83,6 +84,9 @@ public class EventServiceImpl implements EventService {
 
     @Autowired
     private JMSManager jmsManager;
+
+    @Autowired
+    private PartitionsModuleConfiguration partitionsModuleConfiguration;
 
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
@@ -168,6 +172,10 @@ public class EventServiceImpl implements EventService {
      */
     @Override
     public void enqueuePartitionCheckEvent(String partitionName) {
+        if (!partitionsModuleConfiguration.isActive()) {
+            LOG.debug("Partitions alerts module is not enabled, no alert will be created");
+            return;
+        }
         Event event = new Event(EventType.PARTITION_CHECK);
         event.addStringKeyValue(PartitionCheckEvent.PARTITION_NAME.name(), partitionName);
         enqueueEvent(PARTITION_CHECK, event);
