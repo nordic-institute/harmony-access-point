03624d9fe863aa69ee0d32582566784dd862fe70
Gabriel Maier <gabriel.maier@ext.ec.europa.eu>
2022-06-14 16:40:38 +0200

    EDELIVERY-4073 change domain logback configurations

--- Core/Domibus-MSH-logging/src/main/java/eu/domibus/logging/DomibusLoggersCache.java
+++ Core/Domibus-MSH-logging/src/main/java/eu/domibus/logging/DomibusLoggersCache.java
@@ -12,10 +12,10 @@ import static eu.domibus.logging.DomibusLogger.MDC_PROPERTY_PREFIX;
 public class DomibusLoggersCache {
     private final static Map<String, DomibusLogger> cache = new ConcurrentHashMap<>();
 
-    public static DomibusLogger getLogger(String name){
+    public static DomibusLogger getLogger(String name) {
         String loggerName = getLoggerName(name);
         DomibusLogger logger = cache.get(loggerName);
-        if(logger==null){
+        if (logger == null) {
             logger = new DomibusLoggerImpl(LoggerFactory.getLogger(loggerName));
             cache.put(loggerName, logger);
         }
@@ -24,7 +24,7 @@ public class DomibusLoggersCache {
 
     public static String getLoggerName(String name) {
         String domainName = MDC.get(MDC_PROPERTY_PREFIX + MDC_DOMAIN);
-        if(domainName == null){
+        if (domainName == null) {
             domainName = "";
         } else {
             domainName = domainName + ".";
