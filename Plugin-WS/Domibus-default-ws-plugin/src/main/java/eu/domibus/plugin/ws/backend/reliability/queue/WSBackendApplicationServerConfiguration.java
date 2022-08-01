543d435ace672b1646ca11ba2f47cb1c599367b1
soumya <Soumya.AZHIKODE-CHANDRAN@ext.ec.europa.eu>
2022-08-01 07:39:42 +0200

    [EDELIVERY-7061]-removed commented code

--- Plugin-WS/Domibus-default-ws-plugin/src/main/java/eu/domibus/plugin/ws/backend/reliability/queue/WSBackendApplicationServerConfiguration.java
+++ Plugin-WS/Domibus-default-ws-plugin/src/main/java/eu/domibus/plugin/ws/backend/reliability/queue/WSBackendApplicationServerConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jndi.JndiObjectFactoryBean;
 
 import javax.jms.Queue;
-import java.io.IOException;
 
 import static eu.domibus.plugin.ws.backend.reliability.queue.WSMessageListenerContainerConfiguration.WS_PLUGIN_SEND_QUEUE;
 
@@ -26,27 +25,10 @@ public class WSBackendApplicationServerConfiguration {
 
     private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(WSBackendApplicationServerConfiguration.class);
 
- /*   @Autowired
-    protected DomibusPropertyProvider domibusPropertyProvider;*/
-
     @Bean(WS_PLUGIN_SEND_QUEUE)
-    public JndiObjectFactoryBean sendMessageQueue(WSPluginPropertyManager wsPluginPropertyManager) throws IOException {
-        // return propertyRetrieveManager.getInternalProperty(DomibusPropertyMetadataManagerSPI.DOMIBUS_CONFIG_LOCATION);
+    public JndiObjectFactoryBean sendMessageQueue(WSPluginPropertyManager wsPluginPropertyManager) {
         String queueName = wsPluginPropertyManager.getKnownPropertyValue(WSPluginPropertyManager.DISPATCHER_SEND_QUEUE_NAME);
-      /*  if(queueName ==null)
-        {
-            LOG.debug("WSPluginPropertyManager.DISPATCHER_SEND_QUEUE_NAME [{}]", WSPluginPropertyManager.DISPATCHER_SEND_QUEUE_NAME);
-            List<Resource> resources = new ArrayList<>();
-            PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-            Resource[] pluginDefaultResourceList = resolver.getResources("classpath*:config/ws-plugin-default.properties");
-            LOG.debug("Adding the following plugin default properties files [{}]", pluginDefaultResourceList);
-            resources.addAll(Arrays.asList(pluginDefaultResourceList));
-
-            String queueName1 =  domibusPropertyProvider.getProperty(WSPluginPropertyManager.DISPATCHER_SEND_QUEUE_NAME);
-            LOG.debug("Using ws plugin send queue name1 [{}]", queueName1);
-        }*/
         LOG.debug("Using ws plugin send queue name [{}]", queueName);
-
         JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
 
         jndiObjectFactoryBean.setJndiName(queueName);
