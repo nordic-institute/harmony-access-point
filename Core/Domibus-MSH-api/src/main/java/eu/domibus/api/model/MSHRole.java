1b788cb6f7f1db9e8ecca7cca7cba0831e9ea14c
ionperpegel <ion.perpegel@gmail.com>
2022-08-24 09:34:17 +0300

    [EDELIVERY-9735] add migration scripts; remove some temp code

--- Core/Domibus-MSH-api/src/main/java/eu/domibus/api/model/MSHRole.java
+++ Core/Domibus-MSH-api/src/main/java/eu/domibus/api/model/MSHRole.java
@@ -4,8 +4,4 @@ package eu.domibus.api.model;
 public enum MSHRole {
     SENDING,
     RECEIVING,
-  	/*
-     * TODO: to remove once the database migration for the self-sending is done.
-     */
-  	TEMPORARY_FIX_ROLE;
 }
