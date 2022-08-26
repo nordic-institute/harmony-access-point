df2ae9b2ffa815f3c8d1bd70f202c3396d4140b8
Gabriel Maier <gabriel.maier@ext.ec.europa.eu>
2022-08-26 14:36:01 +0200

    EDELIVERY-9465 revert the changes to the deprecated WS Plugin related to markAsDownloaded

--- Plugin-WS/Domibus-default-ws-plugin/src/main/java/eu/domibus/plugin/ws/webservice/deprecated/WebServicePluginImpl.java
+++ Plugin-WS/Domibus-default-ws-plugin/src/main/java/eu/domibus/plugin/ws/webservice/deprecated/WebServicePluginImpl.java
@@ -41,9 +41,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.apache.commons.lang3.BooleanUtils.toBooleanDefaultIfNull;
-import static org.apache.commons.lang3.BooleanUtils.toBooleanObject;
-
 /**
  * @deprecated since 5.0 Use instead {@link eu.domibus.plugin.ws.webservice.WebServiceImpl}
  */
@@ -297,44 +294,7 @@ public class WebServicePluginImpl implements BackendInterface {
     }
 
     /**
-     * The message status is updated to downloaded (the message is not removed from the plugin table containing the pending messages so it can be downloaded using retrieveMessage
-     * @param markMessageAsDownloadedRequest
-     * @param markMessageAsDownloadedResponse
-     * @param ebMSHeaderInfo
-     * @throws MarkMessageAsDownloadedFault
-     */
-    @Override
-    @Transactional(propagation = Propagation.REQUIRES_NEW, timeout = 300, rollbackFor = RetrieveMessageFault.class)
-    @MDCKey(value = {DomibusLogger.MDC_MESSAGE_ID, DomibusLogger.MDC_MESSAGE_ROLE, DomibusLogger.MDC_MESSAGE_ENTITY_ID}, cleanOnStart = true)
-    public void markMessageAsDownloaded(MarkMessageAsDownloadedRequest markMessageAsDownloadedRequest,
-                                        Holder<MarkMessageAsDownloadedResponse> markMessageAsDownloadedResponse,
-                                        Holder<Messaging> ebMSHeaderInfo) throws MarkMessageAsDownloadedFault {
-        boolean isMessageIdNotEmpty = StringUtils.isNotEmpty(markMessageAsDownloadedRequest.getMessageID());
-
-        if (!isMessageIdNotEmpty) {
-            LOG.error(MESSAGE_ID_EMPTY);
-            throw new MarkMessageAsDownloadedFault(MESSAGE_ID_EMPTY, webServicePluginExceptionFactory.createFault("MessageId is empty"));
-        }
-
-        String trimmedMessageId = messageExtService.cleanMessageIdentifier(markMessageAsDownloadedRequest.getMessageID());
-        WSMessageLogEntity wsMessageLogEntity = wsMessageLogDao.findByMessageId(trimmedMessageId);
-        if(wsMessageLogEntity == null) {
-            LOG.businessError(DomibusMessageCode.BUS_MSG_NOT_FOUND, trimmedMessageId);
-            throw new MarkMessageAsDownloadedFault(MESSAGE_NOT_FOUND_ID + trimmedMessageId + "]", webServicePluginExceptionFactory.createFault("No message with id [" + trimmedMessageId + "] pending for download"));
-        }
-        markMessageAsDownloaded(trimmedMessageId);
-
-        markMessageAsDownloadedResponse.value =  WEBSERVICE_OF.createMarkMessageAsDownloadedResponse();
-        markMessageAsDownloadedResponse.value.setMessageID(trimmedMessageId);
-    }
-
-    /**
-     * Add support for large files using DataHandler instead of byte[].
-     * If the flag markAsAcknowledged is set to true then
-     * <ul>
-     *     <li>the message status is updated</li>
-     *     <li>after, successfully downloading, the message is removed from the plugin table containing the pending messages</li>
-     * </ul>
+     * Add support for large files using DataHandler instead of byte[]
      *
      * @param retrieveMessageRequest
      * @param retrieveMessageResponse
@@ -355,14 +315,13 @@ public class WebServicePluginImpl implements BackendInterface {
         }
 
         String trimmedMessageId = messageExtService.cleanMessageIdentifier(retrieveMessageRequest.getMessageID());
-        boolean markAsDownloaded = toBooleanDefaultIfNull(toBooleanObject(retrieveMessageRequest.getMarkAsDownloaded()), true);          //workaround jaxws bug
         WSMessageLogEntity wsMessageLogEntity = wsMessageLogDao.findByMessageId(trimmedMessageId);
-        if(markAsDownloaded && wsMessageLogEntity == null) {
+        if (wsMessageLogEntity == null) {
             LOG.businessError(DomibusMessageCode.BUS_MSG_NOT_FOUND, trimmedMessageId);
             throw new RetrieveMessageFault(MESSAGE_NOT_FOUND_ID + trimmedMessageId + "]", webServicePluginExceptionFactory.createFault("No message with id [" + trimmedMessageId + "] pending for download"));
         }
         eu.domibus.plugin.ws.generated.header.common.model.org.oasis_open.docs.ebxml_msg.ebms.v3_0.ns.core._200704.UserMessage userMessage =
-                downloadUserMessage(trimmedMessageId, markAsDownloaded);
+                getUserMessage(retrieveMessageRequest, trimmedMessageId);
         eu.domibus.plugin.ws.generated.header.common.model.org.oasis_open.docs.ebxml_msg.ebms.v3_0.ns.core._200704.Messaging messagingWs =
                 WebServiceImpl.EBMS_OBJECT_FACTORY.createMessaging();
         messagingWs.setUserMessage(userMessage);
@@ -375,46 +334,33 @@ public class WebServicePluginImpl implements BackendInterface {
         ebMSHeaderInfo.value = messagingMapper.messagingFromEntity(messagingWs);
 
         try {
-            messageAcknowledgeExtService.acknowledgeMessageDeliveredWithUnsecureLoginAllowed(trimmedMessageId, new Timestamp(System.currentTimeMillis()), markAsDownloaded);
+            messageAcknowledgeExtService.acknowledgeMessageDeliveredWithUnsecureLoginAllowed(trimmedMessageId, new Timestamp(System.currentTimeMillis()));
         } catch (AuthenticationExtException | MessageAcknowledgeExtException e) {
             //if an error occurs related to the message acknowledgement do not block the download message operation
             LOG.error("Error acknowledging message [" + retrieveMessageRequest.getMessageID() + "]", e);
         }
-        if(markAsDownloaded) {
-            // remove downloaded message from the plugin table containing the pending messages
-            wsMessageLogDao.delete(wsMessageLogEntity);
-        }
+
+        // remove downloaded message from the plugin table containing the pending messages
+        wsMessageLogDao.delete(wsMessageLogEntity);
     }
 
-    private eu.domibus.plugin.ws.generated.header.common.model.org.oasis_open.docs.ebxml_msg.ebms.v3_0.ns.core._200704.UserMessage downloadUserMessage(String trimmedMessageId, boolean markAsDownloaded) throws RetrieveMessageFault {
+    private eu.domibus.plugin.ws.generated.header.common.model.org.oasis_open.docs.ebxml_msg.ebms.v3_0.ns.core._200704.UserMessage getUserMessage(RetrieveMessageRequest retrieveMessageRequest, String trimmedMessageId) throws RetrieveMessageFault {
         eu.domibus.plugin.ws.generated.header.common.model.org.oasis_open.docs.ebxml_msg.ebms.v3_0.ns.core._200704.UserMessage userMessage;
         try {
-            userMessage = wsPlugin.downloadMessage(trimmedMessageId, null, markAsDownloaded);
+            userMessage = wsPlugin.downloadMessage(trimmedMessageId, null);
         } catch (final MessageNotFoundException mnfEx) {
             if (LOG.isDebugEnabled()) {
-                LOG.debug(MESSAGE_NOT_FOUND_ID + trimmedMessageId + "]", mnfEx);
+                LOG.debug(MESSAGE_NOT_FOUND_ID + retrieveMessageRequest.getMessageID() + "]", mnfEx);
             }
-            LOG.error(MESSAGE_NOT_FOUND_ID + trimmedMessageId + "]");
+            LOG.error(MESSAGE_NOT_FOUND_ID + retrieveMessageRequest.getMessageID() + "]");
             throw new RetrieveMessageFault(MESSAGE_NOT_FOUND_ID + trimmedMessageId + "]", webServicePluginExceptionFactory.createDownloadMessageFault(mnfEx));
         }
 
         if (userMessage == null) {
-            LOG.error(MESSAGE_NOT_FOUND_ID + trimmedMessageId + "]");
+            LOG.error(MESSAGE_NOT_FOUND_ID + retrieveMessageRequest.getMessageID() + "]");
             throw new RetrieveMessageFault(MESSAGE_NOT_FOUND_ID + trimmedMessageId + "]", webServicePluginExceptionFactory.createFault("UserMessage not found"));
         }
         return userMessage;
-    }
-
-    private void markMessageAsDownloaded(String trimmedMessageId) throws MarkMessageAsDownloadedFault {
-        try {
-            wsPlugin.markMessageAsDownloaded(trimmedMessageId);
-        } catch (final MessageNotFoundException mnfEx) {
-            if (LOG.isDebugEnabled()) {
-                LOG.debug(MESSAGE_NOT_FOUND_ID + trimmedMessageId + "]", mnfEx);
-            }
-            LOG.error(MESSAGE_NOT_FOUND_ID + trimmedMessageId + "]");
-            throw new MarkMessageAsDownloadedFault(MESSAGE_NOT_FOUND_ID + trimmedMessageId + "]", webServicePluginExceptionFactory.createDownloadMessageFault(mnfEx));
-        }
     }
 
     private void fillInfoPartsForLargeFilesWs(Holder<RetrieveMessageResponse> retrieveMessageResponse, eu.domibus.plugin.ws.generated.header.common.model.org.oasis_open.docs.ebxml_msg.ebms.v3_0.ns.core._200704.Messaging messaging) {
