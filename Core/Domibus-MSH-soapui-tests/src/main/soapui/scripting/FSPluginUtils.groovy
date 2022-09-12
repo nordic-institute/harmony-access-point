93679975f76c47a9a6dac9928292dcd35587a61f
nkopgzeg <Grzegorz.Kopec.ext.ec@gmail.com>
2022-09-12 08:23:25 +0100

    Updated more split and join

--- Core/Domibus-MSH-soapui-tests/src/main/soapui/scripting/FSPluginUtils.groovy
+++ Core/Domibus-MSH-soapui-tests/src/main/soapui/scripting/FSPluginUtils.groovy
@@ -7,8 +7,25 @@ class FSPluginUtils {
     def context = null
     def log = null
 
-    static def FS_DEF_MAP = [FS_DEF_SENDER:"domibus-blue",FS_DEF_P_TYPE:"urn:oasis:names:tc:ebcore:partyid-type:unregistered",FS_DEF_S_ROLE:"http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/initiator",FS_DEF_RECEIVER:"domibus-red",FS_DEF_R_ROLE:"http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/responder",FS_DEF_AGR_TYPE:"DUM",FS_DEF_AGR:"DummyAgr",FS_DEF_SRV_TYPE:"tc20",FS_DEF_SRV:"bdx:noprocess",FS_DEF_ACTION:"TC20Leg1",FS_DEF_CID:"cid:message",FS_DEF_PAY_NAME:"PayloadName.xml",FS_DEF_MIME:"text/xml",FS_DEF_OR_SENDER:"urn:oasis:names:tc:ebcore:partyid-type:unregistered:C1",FS_DEF_FIN_RECEIVER:"urn:oasis:names:tc:ebcore:partyid-type:unregistered:C4",FS_DEF_PROC_TYPE:"PUSH",FS_DEF_MPC:"http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/mpcPushStandard"]
+    static def FS_DEF_MAP = [SENDER: 'domibus-blue',
+                             P_TYPE: "urn:oasis:names:tc:ebcore:partyid-type:unregistered",
+                             S_ROLE: "http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/initiator",
+                             RECEIVER: "domibus-red",
+                             R_ROLE: "http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/responder",
+                             AGR_TYPE: "DUM",
+                             AGR: "DummyAgr",
+                             SRV_TYPE:'(?<=<Service type=")([^"]+)(?=">)',
+                             SRV: "bdx:noprocess",
+                             ACTION: '(?<=<Action>)(.+)(?=</Action>)',
+                             CID: "cid:message",
+                             PAY_NAME: "PayloadName.xml",
+                             MIME: "text/xml",
+                             OR_SENDER: "urn:oasis:names:tc:ebcore:partyid-type:unregistered:C1",
+                             FIN_RECEIVER: "urn:oasis:names:tc:ebcore:partyid-type:unregistered:C4",
+                             PROC_TYPE: "PUSH",
+                             MPC: '(?<=mpc=")([^"]+)(?=")']
     static def PATH_TO_FS_TEST_FILES = "/resources/PModesandKeystoresSpecialTests/fsPlugin/standard/"
+    static def FS_PLUGIN_MESSAGE_LOCATION_PROPERTY_NAME = "fsplugin.messages.location"
     /**
      *
      * @param log
@@ -93,13 +110,13 @@ class FSPluginUtils {
 
         /*def multitenancyOn = getMultitenancyFromSide(side, context, log)
         if(multitenancyOn){
-            messageLocationPropertyName = domain + ".fsplugin.messages.location"
+            messageLocationPropertyName = domain + "." + FS_PLUGIN_MESSAGE_LOCATION_PROPERTY_NAME
         }*/
         messageMetadata = extractSuitableTemplate(log, context, testRunner, configuration)
 
         // Update the targeted values in the template
         parametersMap.each { entry ->
-            messageMetadata = messageMetadata.replace(FS_DEF_MAP["FS_DEF_" + entry.key],entry.value)
+            messageMetadata = messageMetadata.replaceAll(FS_DEF_MAP[entry.key],entry.value)
         }
 
         fspluginPath = getPathToFsPluginSendingLocation(log, context, testRunner, side)
@@ -143,7 +160,35 @@ class FSPluginUtils {
         LogUtils.debugLog("  ====  \"submitFSmessage\" DONE.", log)
 
     }
-
+    /**
+     * Submit split and join message reusing submitFSmessage method
+     * parametersMap keys must be: [SENDER:"...",RECEIVER:"...",AGR_TYPE:"...",AGR:"...",SRV_TYPE:"...",SRV:"...",ACTION:"...",CID:"...",PAY_NAME:"...",MIME:"...",OR_SENDER:"...",FIN_RECEIVER:"..."]
+     * @param side
+     * @param context
+     * @param log
+     * @param testRunner
+     * @param inputFileName
+     * @param parametersMap
+     * @param configuration
+     * @param domain
+     * @param subFolder
+     * @return
+     */
+    def static submitSplitAndJoinMessage(String side, context, log, testRunner, String inputFileName = "Test_file.xml", parametersMap = [], String configuration = "splitstandard", String domain = "default", String subFolder="") {
+        def twoFiles = false
+        def destSuffix = "_" + inputFileName.split(/\./)[0]
+        submitFSmessage(side,
+                context,
+                log,
+                testRunner,
+                configuration,
+                domain,
+                parametersMap,
+                twoFiles,
+                destSuffix,
+                subFolder,
+                inputFileName)
+    }
     /**
      * This functions checks payload is present in IN folder
      * @param side
@@ -163,15 +208,15 @@ class FSPluginUtils {
         LogUtils.debugLog("  ====  Calling \"checkFSpayloadPresentIN\".", log)
         def fsPayloadPath
         def testFile
-        def messageLocationPrpertyName = "fsplugin.messages.location"
+        def messageLocationPropertyName = FS_PLUGIN_MESSAGE_LOCATION_PROPERTY_NAME
 
         /*def multitenancyOn = getMultitenancyFromSide(side, context, log)
         if(multitenancyOn){
-            messageLocationPropertyName = domain + ".fsplugin.messages.location"
+            messageLocationPropertyName = domain + "." + FS_PLUGIN_MESSAGE_LOCATION_PROPERTY_NAME
         }*/
 
 
-        fsPayloadPath = Domibus.getPropertyAtRuntime(side, messageLocationPrpertyName, context, log, domain) + "/IN/" + finalRecipient + "/" + messageID + "/"
+        fsPayloadPath = Domibus.getPropertyAtRuntime(side, messageLocationPropertyName, context, log, domain) + "/IN/" + finalRecipient + "/" + messageID + "/"
         fsPayloadPath = Domibus.formatPathSlashes(fsPayloadPath)
         LogUtils.debugLog("  checkFSpayloadPresentIN  [][]  fsPayloadPath = \"$fsPayloadPath\"", log)
         for(int i = 0;i<payloadName.size;i++){
@@ -205,15 +250,15 @@ class FSPluginUtils {
         def counter = 0
         def FS_WAIT_TIME = 60_000 // Maximum time to wait to check.
         def STEP_TIME = 1_000 // Time to wait before re-checking.
-        def messageLocationPrpertyName = "fsplugin.messages.location"
+        def messageLocationPropertyName = FS_PLUGIN_MESSAGE_LOCATION_PROPERTY_NAME
 
         /*def multitenancyOn = getMultitenancyFromSide(side, context, log)
         if(multitenancyOn){
-            messageLocationPropertyName = domain + ".fsplugin.messages.location"
+            messageLocationPropertyName = domain + "." + FS_PLUGIN_MESSAGE_LOCATION_PROPERTY_NAME
         }*/
 
 
-        def fsPayloadPath = Domibus.getPropertyAtRuntime(side, messageLocationPrpertyName, context, log, domain) + "/OUT"
+        def fsPayloadPath = Domibus.getPropertyAtRuntime(side, messageLocationPropertyName, context, log, domain) + "/OUT"
         fsPayloadPath = Domibus.formatPathSlashes(fsPayloadPath)
         LogUtils.debugLog("  checkFSpayloadPresentOUT  [][]  fsPayloadPath = \"$fsPayloadPath\"", log)
 
@@ -245,11 +290,11 @@ class FSPluginUtils {
         def counter = 0
         def FS_WAIT_TIME = 60_000 // Maximum time to wait to check.
         def STEP_TIME = 1_000 // Time to wait before re-checking.
-        def messageLocationPropertyName = "fsplugin.messages.location"
+        def messageLocationPropertyName = FS_PLUGIN_MESSAGE_LOCATION_PROPERTY_NAME
 
         /*def multitenancyOn = getMultitenancyFromSide(side, context, log)
         if(multitenancyOn){
-            messageLocationPropertyName = domain + ".fsplugin.messages.location"
+            messageLocationPropertyName = domain + "." + FS_PLUGIN_MESSAGE_LOCATION_PROPERTY_NAME
         }*/
 
 
@@ -286,7 +331,7 @@ class FSPluginUtils {
         def fsPayloadPathBase = ""
         /*def multitenancyOn = getMultitenancyFromSide(side, context, log)
         if(multitenancyOn){
-            messageLocationPropertyName = domain + ".fsplugin.messages.location"
+            messageLocationPropertyName = domain + "." + FS_PLUGIN_MESSAGE_LOCATION_PROPERTY_NAME
         }*/
 
         switch (side.toLowerCase()) {
