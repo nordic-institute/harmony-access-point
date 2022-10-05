fd70964177baf99a0d3841c252817a42f1551b44
ionperpegel <ion.perpegel@gmail.com>
2022-10-05 16:28:05 +0300

    [EDELIVERY-10227] adapt tests

--- Domibus-MSH/src/test/java/eu/domibus/core/clustering/ControllerListenerServiceTest.java
+++ Domibus-MSH/src/test/java/eu/domibus/core/clustering/ControllerListenerServiceTest.java
@@ -55,7 +55,7 @@ public class ControllerListenerServiceTest {
         boolean handled = controllerListenerService.handleMessageDomain(message);
         Assert.assertTrue(handled);
         new Verifications() {{
-            domainContextProvider.setCurrentDomain(domainCode);
+            domainContextProvider.setCurrentDomainWithValidation(domainCode);
         }};
     }
 
