cc35e5c809495824da8a301d30e4e1db24187442
ionperpegel <ion.perpegel@gmail.com>
2022-10-11 15:01:52 +0300

    [EDELIVERY-9155] address PR comments: refactor code to make checks

--- Core/Domibus-MSH/src/main/java/eu/domibus/core/plugin/BackendConnectorStateServiceImpl.java
+++ Core/Domibus-MSH/src/main/java/eu/domibus/core/plugin/BackendConnectorStateServiceImpl.java
@@ -9,6 +9,7 @@ import eu.domibus.core.jms.MessageListenerContainerInitializer;
 import eu.domibus.ext.domain.CronJobInfoDTO;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
+import eu.domibus.plugin.BackendConnector;
 import eu.domibus.plugin.EnableAware;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
@@ -47,11 +48,11 @@ public class BackendConnectorStateServiceImpl implements BackendConnectorStateSe
         Domain domain = domainService.getDomain(domainCode);
         messageListenerContainerInitializer.createMessageListenersForPlugin(backendName, domain);
 
-        EnableAware plugin = (EnableAware) backendConnectorProvider.getBackendConnector(backendName);
-        String[] jobNamesToResume = getNamesToResume(plugin);
+        String[] jobNamesToResume = getJobNames(backendName);
         if (jobNamesToResume == null) {
             return;
         }
+
         domibusScheduler.resumeJobs(domain, jobNamesToResume);
     }
 
@@ -69,15 +70,29 @@ public class BackendConnectorStateServiceImpl implements BackendConnectorStateSe
         Domain domain = domainService.getDomain(domainCode);
         messageListenerContainerInitializer.destroyMessageListenersForPlugin(backendName, domain);
 
-        EnableAware plugin = (EnableAware) backendConnectorProvider.getBackendConnector(backendName);
-        String[] jobNamesToResume = getNamesToResume(plugin);
+        String[] jobNamesToResume = getJobNames(backendName);
         if (jobNamesToResume == null) {
             return;
         }
         domibusScheduler.pauseJobs(domain, jobNamesToResume);
     }
 
-    private String[] getNamesToResume(EnableAware plugin) {
+    private EnableAware getEnableAware(String backendName) {
+        BackendConnector<?, ?> backendConnector = backendConnectorProvider.getBackendConnector(backendName);
+        if (!(backendConnector instanceof EnableAware)) {
+            LOG.info("Backend connector [{}] does not implement EnableAware; exiting.", backendName);
+            return null;
+        }
+        EnableAware plugin = (EnableAware) backendConnector;
+        return plugin;
+    }
+
+    private String[] getJobNames(String backendName) {
+        EnableAware plugin = getEnableAware(backendName);
+        if (plugin == null) {
+            return null;
+        }
+
         List<CronJobInfoDTO> jobsInfo = plugin.getJobsInfo();
         if (CollectionUtils.isEmpty(jobsInfo)) {
             LOG.info("No job names returned; nothing to pause.");
