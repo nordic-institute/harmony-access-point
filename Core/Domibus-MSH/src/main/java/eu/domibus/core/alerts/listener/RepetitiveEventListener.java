dd0a0b7b756e6fb8c67b61b7ea8d9ab5f2644389
ionperpegel <ion.perpegel@gmail.com>
2022-11-03 10:34:00 +0200

    [EDELIVERY-9241] add generic event listeners for normal and repetitive events

---
+++ Core/Domibus-MSH/src/main/java/eu/domibus/core/alerts/listener/RepetitiveEventListener.java
@@ -0,0 +1,63 @@
+package eu.domibus.core.alerts.listener;
+
+import eu.domibus.api.multitenancy.DomainContextProvider;
+import eu.domibus.api.util.DatabaseUtil;
+import eu.domibus.core.alerts.dao.EventDao;
+import eu.domibus.core.alerts.model.common.EventType;
+import eu.domibus.core.alerts.model.service.Alert;
+import eu.domibus.core.alerts.model.service.Event;
+import eu.domibus.core.alerts.service.AlertService;
+import eu.domibus.logging.DomibusLogger;
+import eu.domibus.logging.DomibusLoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import static eu.domibus.api.property.DomibusPropertyMetadataManagerSPI.DOMIBUS_JMS_QUEUE_ALERT;
+import static eu.domibus.core.alerts.service.EventServiceImpl.ALERT_JMS_LISTENER_CONTAINER_FACTORY;
+
+/**
+ * @author Ion Perpegel
+ * @since 5.1
+ */
+@Component
+public class RepetitiveEventListener {
+
+    private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(RepetitiveEventListener.class);
+
+    private final AlertService alertService;
+
+    private final DomainContextProvider domainContextProvider;
+
+    private final DatabaseUtil databaseUtil;
+
+    private final EventDao eventDao;
+
+    public RepetitiveEventListener(AlertService alertService, DomainContextProvider domainContextProvider, DatabaseUtil databaseUtil, EventDao eventDao) {
+        this.alertService = alertService;
+        this.domainContextProvider = domainContextProvider;
+        this.databaseUtil = databaseUtil;
+        this.eventDao = eventDao;
+    }
+
+    @JmsListener(containerFactory = ALERT_JMS_LISTENER_CONTAINER_FACTORY, destination = "${"+ DOMIBUS_JMS_QUEUE_ALERT + "}",
+            selector = "selector = 'RepetitiveEventListener'")
+    @Transactional
+    public void onEvent(final Event event, final @Header(name = "DOMAIN", required = false) String domain) {
+        saveEventAndTriggerAlert(event, domain);
+    }
+
+    private void saveEventAndTriggerAlert(Event event, final String domain) {
+        LOG.debug("Event received:[{}]", event);
+        domainContextProvider.setCurrentDomain(domain);
+        LOG.putMDC(DomibusLogger.MDC_USER, databaseUtil.getDatabaseUserName());
+
+        eu.domibus.core.alerts.model.persist.Event entity = eventDao.read(event.getEntityId());
+        if (entity != null) {
+            final Alert alertOnEvent = alertService.createAlertOnEvent(event);
+            alertService.enqueueAlert(alertOnEvent);
+        }
+    }
+
+}
