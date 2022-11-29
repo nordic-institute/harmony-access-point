f9620ab50cc9bbfe3a596b54af0637ac3eaf18eb
ionperpegel <ion.perpegel@gmail.com>
2022-11-29 12:43:08 +0200

    [EDELIVERY-10409] catch earchiving exception and trigger the alert

---
+++ Core/Domibus-MSH-api/src/main/java/eu/domibus/api/earchive/DomibusEArchiveExportException.java
@@ -0,0 +1,32 @@
+package eu.domibus.api.earchive;
+
+import eu.domibus.api.exceptions.DomibusCoreErrorCode;
+import eu.domibus.api.exceptions.DomibusCoreException;
+
+/**
+ * Exception raised when export fails for a message
+ *
+ * @author Ion Perpegel
+ * @since 5.1
+ */
+public class DomibusEArchiveExportException extends DomibusCoreException {
+
+    Long entityId;
+
+    String message;
+
+    public DomibusEArchiveExportException(DomibusCoreErrorCode code, Long entityId, String message) {
+        super(code, message);
+        this.entityId = entityId;
+        this.message = message;
+    }
+
+    public Long getEntityId() {
+        return entityId;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
