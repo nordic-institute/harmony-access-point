7ee553352e20c726398baf35a26b30ec3f11c41b
furcalu <lucian.furca@ext.ec.europa.eu>
2022-12-01 16:46:03 +0200

    [EDELIVERY-10414]-Sending a message with split and join and FS plugin throws unexpected error on C3

--- Core/Domibus-MSH/src/main/java/eu/domibus/core/message/MessageFragmentDao.java
+++ Core/Domibus-MSH/src/main/java/eu/domibus/core/message/MessageFragmentDao.java
@@ -1,6 +1,8 @@
 package eu.domibus.core.message;
 
+import eu.domibus.api.model.UserMessage;
 import eu.domibus.api.model.splitandjoin.MessageFragmentEntity;
+import eu.domibus.api.model.splitandjoin.MessageGroupEntity;
 import eu.domibus.core.dao.BasicDao;
 import org.springframework.stereotype.Repository;
 
@@ -15,5 +17,12 @@ public class MessageFragmentDao extends BasicDao<MessageFragmentEntity> {
         super(MessageFragmentEntity.class);
     }
 
-
+    @Override
+    public void create(MessageFragmentEntity messageFragmentEntity) {
+        if (!em.contains(messageFragmentEntity.getUserMessage())) {
+            UserMessage attachedUserMessage = em.find(UserMessage.class, messageFragmentEntity.getUserMessage().getEntityId());
+            messageFragmentEntity.setUserMessage(attachedUserMessage);
+        }
+        super.create(messageFragmentEntity);
+    }
 }
