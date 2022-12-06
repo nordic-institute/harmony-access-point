e4d21d44117d7da4788887a3a83bb3887679fc8c
Razvan CRETU <razvan.cretu@ext.ec.europa.eu>
2022-12-06 13:24:50 +0200

    EDELIVERY-10294 cleanup + notes for future analysis

--- Tomcat/Domibus-MSH-tomcat/src/test/java/eu/domibus/AbstractIT.java
+++ Tomcat/Domibus-MSH-tomcat/src/test/java/eu/domibus/AbstractIT.java
@@ -125,12 +125,8 @@ public abstract class AbstractIT {
         //we are using randomly available port in order to allow run in parallel
         int activeMQConnectorPort = SocketUtils.findAvailableTcpPort(2000, 3100);
         int activeMQBrokerPort = SocketUtils.findAvailableTcpPort(61616, 62690);
-        System.setProperty(DomibusPropertyMetadataManagerSPI.ACTIVE_MQ_CONNECTOR_PORT, String.valueOf(activeMQConnectorPort));
-        System.setProperty(DomibusPropertyMetadataManagerSPI.ACTIVE_MQ_TRANSPORT_CONNECTOR_URI, "vm://localhost:" + activeMQBrokerPort + "?broker.persistent=false&create=false");
-//        System.setProperty(DomibusPropertyMetadataManagerSPI.ACTIVE_MQ_JMXURL, "service:jmx:rmi:///jndi/rmi://localhost:" + activeMQConnectorPort + "/jmxrmi");
-//        System.setProperty(DomibusPropertyMetadataManagerSPI.ACTIVE_MQ_PERSISTENT, "false");
-//        System.setProperty(DomibusPropertyMetadataManagerSPI.ACTIVE_MQ_USERNAME, "test_user");
-//        System.setProperty(DomibusPropertyMetadataManagerSPI.ACTIVE_MQ_PASSWORD, "test_password");
+        System.setProperty(DomibusPropertyMetadataManagerSPI.ACTIVE_MQ_CONNECTOR_PORT, String.valueOf(activeMQConnectorPort)); // see EDELIVERY-10294 and check if this can be removed
+        System.setProperty(DomibusPropertyMetadataManagerSPI.ACTIVE_MQ_TRANSPORT_CONNECTOR_URI, "vm://localhost:" + activeMQBrokerPort + "?broker.persistent=false&create=false"); // see EDELIVERY-10294 and check if this can be removed
         LOG.info("activeMQBrokerPort=[{}]", activeMQBrokerPort);
         LOG.info("activeMQConnectorPort=[{}]", activeMQConnectorPort);
 
