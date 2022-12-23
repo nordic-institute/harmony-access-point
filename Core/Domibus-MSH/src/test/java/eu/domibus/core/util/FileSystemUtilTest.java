b843e2bb36eed0be2bf53ac3abab5f9ac52afff9
soumya <Soumya.AZHIKODE-CHANDRAN@ext.ec.europa.eu>
2022-12-23 11:36:23 +0100

    [EDELIVERY-10361]-Removed domibus core exception

--- Core/Domibus-MSH/src/test/java/eu/domibus/core/util/FileSystemUtilTest.java
+++ Core/Domibus-MSH/src/test/java/eu/domibus/core/util/FileSystemUtilTest.java
@@ -1,8 +1,6 @@
 package eu.domibus.core.util;
 
-import eu.domibus.api.exceptions.DomibusCoreException;
 import mockit.Tested;
-import org.apache.commons.vfs2.FileSystemException;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -20,10 +18,12 @@ public class FileSystemUtilTest {
     @Tested
     private FileSystemUtil fileSystemUtil;
 
-    @Test(expected = DomibusCoreException.class)
-    public void createLocationWithRelativePath_returnTempFile() throws FileSystemException {
+    @Test
+    public void createLocationWithRelativePath_returnTempFile() {
         final String location = "..\\domibus_blue\\domibus\\earchiving_storage";
-        fileSystemUtil.createLocation(location);
+        Path path = fileSystemUtil.createLocation(location);
+        Assert.assertNotNull(path);
+        assertTrue(Files.exists(path));
     }
 
     @Test
