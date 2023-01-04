cb4d23342dffd3398138e59d018d574ed23095ab
Francois GAUTIER <francois.gauti3r@gmail.com>
2023-01-04 16:54:30 +0100

    [EDELIVERY-10422] [GITB] create first IT test

--- Domibus-MSH-test/src/main/java/eu/domibus/test/PModeUtil.java
+++ Domibus-MSH-test/src/main/java/eu/domibus/test/PModeUtil.java
@@ -3,22 +3,28 @@ package eu.domibus.test;
 import eu.domibus.common.model.configuration.Configuration;
 import eu.domibus.core.pmode.ConfigurationDAO;
 import eu.domibus.core.pmode.provider.PModeProvider;
+import eu.domibus.ext.domain.ValidationIssueDTO;
+import eu.domibus.ext.services.PModeExtService;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
 import eu.domibus.messaging.XmlProcessingException;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class PModeUtil {
 
-    protected static final int SERVICE_PORT = 8892;
+    protected static final int SERVICE_PORT = 9080;
 
     private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(PModeUtil.class);
 
@@ -27,6 +33,9 @@ public class PModeUtil {
 
     @Autowired
     protected ConfigurationDAO configurationDAO;
+
+    @Autowired
+    protected PModeExtService pModeExtService;
 
     public void uploadPmode(Integer redHttpPort) throws IOException, XmlProcessingException {
         final InputStream inputStream = new ClassPathResource("dataset/pmode/PModeTemplate.xml").getInputStream();
@@ -38,6 +47,25 @@ public class PModeUtil {
 
         final Configuration pModeConfiguration = pModeProvider.getPModeConfiguration(pmodeText.getBytes("UTF-8"));
         configurationDAO.updateConfiguration(pModeConfiguration);
+        pModeProvider.refresh();
+    }
+
+    public void updatePModeFile(Integer httpPort, InputStream inputStream) throws IOException {
+        String pmodeText = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        if (httpPort != null) {
+            LOG.info("Using wiremock http port [{}]", httpPort);
+            pmodeText = pmodeText.replace(String.valueOf(SERVICE_PORT), String.valueOf(httpPort));
+        }
+
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+                "PModeTemplate2",
+                "PModeTemplate2.xml",
+                MimeTypeUtils.APPLICATION_XML.toString(),
+                pmodeText.getBytes());
+        List<ValidationIssueDTO> pmodeInitTest = pModeExtService.updatePModeFile(mockMultipartFile, "Pmode init test");
+        for (ValidationIssueDTO validationIssueDTO : pmodeInitTest) {
+            LOG.warn("Validation issue: [{}]", validationIssueDTO);
+        }
         pModeProvider.refresh();
     }
 
