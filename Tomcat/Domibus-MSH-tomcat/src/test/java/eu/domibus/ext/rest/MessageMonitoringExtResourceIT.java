072f37d5649ebadc15cf59d38801eba35c9cb2ea
idragusa <idragusa@gmail.com>
2023-01-27 21:33:07 +0200

    [EDELIVERY_10698] Merge 5.0.3 into development

--- Tomcat/Domibus-MSH-tomcat/src/test/java/eu/domibus/ext/rest/MessageMonitoringExtResourceIT.java
+++ Tomcat/Domibus-MSH-tomcat/src/test/java/eu/domibus/ext/rest/MessageMonitoringExtResourceIT.java
@@ -83,10 +83,9 @@ public class MessageMonitoringExtResourceIT extends AbstractIT {
         // Note here you can not use @Transactional annotation with the following code force commit on data preparation level!!
         Date currentDate = Calendar.getInstance().getTime();
 
-        uml1 = messageDaoTestUtil.createUserMessageLog(UUID.randomUUID().toString(), currentDate, MSHRole.SENDING, MessageStatus.SEND_FAILURE, false, MessageDaoTestUtil.DEFAULT_MPC, new Date());
+        uml1 = messageDaoTestUtil.createUserMessageLog(UUID.randomUUID().toString(), currentDate, MSHRole.SENDING, MessageStatus.SEND_FAILURE, true, MessageDaoTestUtil.DEFAULT_MPC, null);
         uploadPmode(SERVICE_PORT);
     }
-
 
     @Test
     public void getAttempt_notFound() throws Exception {
