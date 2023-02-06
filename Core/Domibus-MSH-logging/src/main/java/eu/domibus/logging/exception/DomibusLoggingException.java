21890cb5ca893fd29a9c3726fd2e66daf2dec063
Gabriel Maier <gabriel.maier@ext.ec.europa.eu>
2023-02-06 18:09:28 +0100

    EDELIVERY-10798 small cleanup

--- Core/Domibus-MSH-logging/src/main/java/eu/domibus/logging/exception/DomibusLoggingException.java
+++ Core/Domibus-MSH-logging/src/main/java/eu/domibus/logging/exception/DomibusLoggingException.java
@@ -1,5 +1,9 @@
 package eu.domibus.logging.exception;
 
+/**
+ * @author Gabriel Maier
+ * @since 5.1
+ */
 public class DomibusLoggingException extends RuntimeException {
     public DomibusLoggingException(String message, Throwable cause) {
         super(message, cause);
