3099bbc2d712b931f9270506ac8027340d76cca0
ionperpegel <ion.perpegel@gmail.com>
2023-02-23 15:07:31 +0200

    [EDELIVERY-10794] fix error handler; add test resources

--- Domibus-MSH/src/main/java/eu/domibus/web/rest/KeystoreResource.java
+++ Domibus-MSH/src/main/java/eu/domibus/web/rest/KeystoreResource.java
@@ -1,6 +1,5 @@
 package eu.domibus.web.rest;
 
-import eu.domibus.api.crypto.CryptoException;
 import eu.domibus.api.exceptions.RequestValidationException;
 import eu.domibus.api.multitenancy.Domain;
 import eu.domibus.api.multitenancy.DomainContextProvider;
@@ -17,10 +16,8 @@ import eu.domibus.core.converter.PartyCoreMapper;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
 import eu.domibus.web.rest.error.ErrorHandlerService;
-import eu.domibus.web.rest.ro.ErrorRO;
 import eu.domibus.web.rest.ro.TrustStoreRO;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -60,11 +57,6 @@ public class KeystoreResource extends TruststoreResourceBase {
         this.multiDomainCertificateProvider = multiDomainCertificateProvider;
         this.domainProvider = domainProvider;
         this.errorHandlerService = errorHandlerService;
-    }
-
-    @ExceptionHandler({CryptoException.class})
-    public ResponseEntity<ErrorRO> handleCryptoException(CryptoException ex) {
-        return errorHandlerService.createResponse(ex, HttpStatus.BAD_REQUEST);
     }
 
     @Override
