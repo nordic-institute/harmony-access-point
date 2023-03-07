b891dba6e21268aa25f8f2accdce6f823c2cdd87
ionperpegel <ion.perpegel@gmail.com>
2023-03-07 12:00:57 +0200

    [EDELIVERY-10918] refactor to reuse code in prop change listener

--- Plugin-WS/Domibus-default-ws-plugin/src/main/java/eu/domibus/plugin/ws/property/listeners/WSPluginMessageNotificationsChangeListener.java
+++ Plugin-WS/Domibus-default-ws-plugin/src/main/java/eu/domibus/plugin/ws/property/listeners/WSPluginMessageNotificationsChangeListener.java
@@ -1,18 +1,12 @@
 package eu.domibus.plugin.ws.property.listeners;
 
-import eu.domibus.common.NotificationType;
-import eu.domibus.ext.services.DomibusPropertyExtService;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
 import eu.domibus.plugin.property.PluginPropertyChangeListener;
 import eu.domibus.plugin.ws.connector.WSPluginImpl;
-import eu.domibus.plugin.ws.property.WSPluginPropertyManager;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 import static eu.domibus.plugin.ws.property.WSPluginPropertyManager.MESSAGE_NOTIFICATIONS;
 
@@ -28,12 +22,8 @@ public class WSPluginMessageNotificationsChangeListener implements PluginPropert
 
     private final WSPluginImpl wsPlugin;
 
-    private final DomibusPropertyExtService domibusPropertyExtService;
-
-    public WSPluginMessageNotificationsChangeListener(@Qualifier(WSPluginImpl.PLUGIN_NAME) WSPluginImpl wsPlugin,
-                                                      DomibusPropertyExtService domibusPropertyExtService) {
+    public WSPluginMessageNotificationsChangeListener(@Qualifier(WSPluginImpl.PLUGIN_NAME) WSPluginImpl wsPlugin) {
         this.wsPlugin = wsPlugin;
-        this.domibusPropertyExtService = domibusPropertyExtService;
     }
 
     @Override
@@ -45,7 +35,6 @@ public class WSPluginMessageNotificationsChangeListener implements PluginPropert
 
     @Override
     public void propertyValueChanged(String domainCode, String propertyName, String propertyValue) {
-        List<NotificationType> messageNotifications = domibusPropertyExtService.getConfiguredNotifications(MESSAGE_NOTIFICATIONS);
-        wsPlugin.setRequiredNotifications(messageNotifications);
+        wsPlugin.setRequiredNotifications();
     }
 }
