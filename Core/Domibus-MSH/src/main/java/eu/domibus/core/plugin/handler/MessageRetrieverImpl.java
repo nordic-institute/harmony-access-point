986e6ed9fe01cf8cc5282b62f0138d2a2adeaa01
soumya <Soumya.AZHIKODE-CHANDRAN@ext.ec.europa.eu>
2023-03-10 16:32:49 +0100

    [EDELIVERY-10778] -review comments fix

--- Core/Domibus-MSH/src/main/java/eu/domibus/core/plugin/handler/MessageRetrieverImpl.java
+++ Core/Domibus-MSH/src/main/java/eu/domibus/core/plugin/handler/MessageRetrieverImpl.java
@@ -19,6 +19,7 @@ import eu.domibus.messaging.DuplicateMessageException;
 import eu.domibus.messaging.MessageNotFoundException;
 import eu.domibus.plugin.Submission;
 import eu.domibus.plugin.handler.MessageRetriever;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -175,10 +176,10 @@ public class MessageRetrieverImpl implements MessageRetriever {
 
         UserMessageLog userMessageLog = userMessageLogService.findByMessageId(messageId, role);
         List<? extends ErrorResult> errorResults = errorLogService.getErrors(messageId, role);
-        if (userMessageLog == null && errorResults.isEmpty()) {
+        if (userMessageLog == null && CollectionUtils.isEmpty(errorResults)) {
             throw new MessageNotFoundException("Message [" + messageId + "] does not exist");
         }
-        return errorLogService.getErrors(messageId, role);
+        return errorResults;
     }
 
     @Override
