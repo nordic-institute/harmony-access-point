96facd2505ac306dd2d56acecfd38665c65f0c6f
Ionut Breaz <ionut.breaz@gmail.com>
2023-09-01 13:44:47 +0300

    EDELIVERY-11927 The maximum upload file size should be per domain

--- Core/Domibus-MSH/src/main/java/eu/domibus/core/property/listeners/PropertyListenersConfiguration.java
+++ Core/Domibus-MSH/src/main/java/eu/domibus/core/property/listeners/PropertyListenersConfiguration.java
@@ -1,6 +1,5 @@
 package eu.domibus.core.property.listeners;
 
-import eu.domibus.api.property.DomibusPropertyProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
@@ -11,13 +10,6 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
  */
 @Configuration
 public class PropertyListenersConfiguration {
-
-    protected DomibusPropertyProvider domibusPropertyProvider;
-
-    public PropertyListenersConfiguration(DomibusPropertyProvider domibusPropertyProvider) {
-        this.domibusPropertyProvider = domibusPropertyProvider;
-    }
-
     @Bean
     public CommonsMultipartResolver multipartResolver() {
         return new CommonsMultipartResolver();
