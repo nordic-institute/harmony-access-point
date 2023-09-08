bb4032913302777617b3eae5218289879125e3e2
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2023-09-08 15:17:03 +0300

    NEDS-110: Fix loading of PKCS12 keystores
    Adding the Bouncy Castle security provider at (arbitrary?) location 3
    breaks loading of PKCS12 keystores when using Java 11.
    
    Changes the behavior so that the provider is added by default at the
    last position. It possible to control the position via a property
    'domibus.security.bc.provider.order' in case it is necessary to restore
    the old behavior (provider at position 3) or otherwise tweak the order.

--- Core/Domibus-MSH/src/main/java/eu/domibus/core/spring/BouncyCastleInitializer.java
+++ Core/Domibus-MSH/src/main/java/eu/domibus/core/spring/BouncyCastleInitializer.java
@@ -5,6 +5,7 @@ import eu.domibus.logging.DomibusLoggerFactory;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import javax.crypto.Cipher;
+
 import java.security.NoSuchAlgorithmException;
 import java.security.Security;
 import java.util.Arrays;
@@ -18,20 +19,21 @@ public class BouncyCastleInitializer {
 
     private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(BouncyCastleInitializer.class);
 
-    // As recommended, make sure that the Sun security provider remains at a higher preference (i.e. index 2 on Weblogic)
-    private static final int HIGHEST_RECOMMENDED_POSITION_IN_ORDER_OF_PREFERENCES = 3;
-
     private static final int LIMITED_STRENGTH_MAX_KEY_LENGTH = 128;
 
     private static final int UNLIMITED_STRENGTH_MAX_KEY_LENGTH = Integer.MAX_VALUE;
 
-    public void registerBouncyCastle() {
-        LOG.info("Registering the Bouncy Castle provider as the third highest preferred security provider");
+    public void registerBouncyCastle(Integer pos) {
+        LOG.info("Registering the Bouncy Castle provider at position {}", pos == null ? "'last'" : pos.toString());
         try {
-            Security.insertProviderAt(new BouncyCastleProvider(), HIGHEST_RECOMMENDED_POSITION_IN_ORDER_OF_PREFERENCES);
+            if (pos == null) {
+                Security.addProvider(new BouncyCastleProvider());
+            } else {
+                Security.insertProviderAt(new BouncyCastleProvider(), pos);
+            }
             LOG.debug("Security providers in order of preferences [{}]", Arrays.toString(Security.getProviders()));
         } catch (SecurityException e) {
-            LOG.error("An error registering Bouncy Castle provider as the second highest preferred security provider", e);
+            LOG.error("An error registering Bouncy Castle provider", e);
         }
     }
 
@@ -45,8 +47,8 @@ public class BouncyCastleInitializer {
                 maxKeyLen == LIMITED_STRENGTH_MAX_KEY_LENGTH
                         ? "Limited"
                         : maxKeyLen == UNLIMITED_STRENGTH_MAX_KEY_LENGTH
-                        ? "Unlimited"
-                        : "Unknown",
+                                ? "Unlimited"
+                                : "Unknown",
                 maxKeyLen);
     }
 
