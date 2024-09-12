08a9c993342102790d94d6c394cd534f9e13d7a3
azhikso <Soumya.AZHIKODE-CHANDRAN@ext.ec.europa.eu>
2024-09-12 17:04:17 +0200

    EDELIVERY-13902 - review comment fix

--- Plugin-WS/Domibus-default-ws-plugin/src/main/java/eu/domibus/plugin/ws/webservice/WebServiceImpl.java
+++ Plugin-WS/Domibus-default-ws-plugin/src/main/java/eu/domibus/plugin/ws/webservice/WebServiceImpl.java
@@ -531,6 +531,12 @@ public class WebServiceImpl implements WebServicePluginInterface {
         boolean markAsDownloaded = toBooleanDefaultIfNull(toBooleanObject(retrieveMessageRequest.getMarkAsDownloaded()), true);  //workaround jaxws bug
         userMessage = downloadUserMessage(trimmedMessageId, markAsDownloaded);
 
+        // To avoid blocking errors during the Header's response validation
+        if (userMessage.getCollaborationInfo().getAgreementRef() != null && StringUtils.isEmpty(userMessage.getCollaborationInfo().getAgreementRef().getValue())) {
+            userMessage.getCollaborationInfo().setAgreementRef(null);
+        }
+
+
         Messaging messaging = new Messaging();
         messaging.setUserMessage(userMessage);
         ebMSHeaderInfo.value = messaging;
--- Plugin-WS/Domibus-default-ws-plugin/src/main/java/eu/domibus/plugin/ws/webservice/deprecated/WebServicePluginImpl.java
+++ Plugin-WS/Domibus-default-ws-plugin/src/main/java/eu/domibus/plugin/ws/webservice/deprecated/WebServicePluginImpl.java
@@ -328,6 +328,10 @@ public class WebServicePluginImpl implements BackendInterface {
         messagingWs.setUserMessage(userMessage);
         retrieveMessageResponse.value = WEBSERVICE_OF.createRetrieveMessageResponse();
         fillInfoPartsForLargeFilesWs(retrieveMessageResponse, messagingWs);
+        // To avoid blocking errors during the Header's response validation
+        if (userMessage.getCollaborationInfo().getAgreementRef() != null && StringUtils.isEmpty(userMessage.getCollaborationInfo().getAgreementRef().getValue())) {
+            userMessage.getCollaborationInfo().setAgreementRef(null);
+        }
 
         ebMSHeaderInfo.value = messagingMapper.messagingFromEntity(messagingWs);
 
