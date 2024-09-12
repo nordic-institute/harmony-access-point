536aca7b3b84647d7a733623d3fb75cf1662676a
azhikso <Soumya.AZHIKODE-CHANDRAN@ext.ec.europa.eu>
2024-09-12 17:22:19 +0200

    EDELIVERY-13902 - review comment fix

--- Core/Domibus-MSH-test/src/main/java/eu/domibus/test/UserMessageService.java
+++ Core/Domibus-MSH-test/src/main/java/eu/domibus/test/UserMessageService.java
@@ -54,7 +54,7 @@ public class UserMessageService {
 
         userMessage.setAction(actionDictionaryService.findOrCreateAction(userMessage.getActionValue()));
         userMessage.setService(serviceDictionaryService.findOrCreateService(userMessage.getService().getValue(), userMessage.getService().getType()));
-        if (userMessage.getAgreementRef() != null) {
+        if (userMessage.getAgreementRef() != null && StringUtils.isNotEmpty(userMessage.getAgreementRef().getValue())) {
             userMessage.setAgreementRef(agreementDictionaryService.findOrCreateAgreement(userMessage.getAgreementRef().getValue(), userMessage.getAgreementRef().getType()));
         }
         userMessage.setMpc(mpcDictionaryService.findOrCreateMpc(StringUtils.isBlank(userMessage.getMpcValue()) ? Ebms3Constants.DEFAULT_MPC : userMessage.getMpcValue()));
