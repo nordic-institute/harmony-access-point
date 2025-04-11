2c280642fbfec4d1b33de6bd51cdebb7525a81ac
Ionut Breaz <ionut-marius.breaz@ext.ec.europa.eu>
2025-04-11 13:21:39 +0300

    EDELIVERY-14859 fix tests

--- Plugin-JMS/Domibus-default-jms-plugin/src/test/java/eu/domibus/plugin/jms/JMSPluginImplTest.java
+++ Plugin-JMS/Domibus-default-jms-plugin/src/test/java/eu/domibus/plugin/jms/JMSPluginImplTest.java
@@ -96,6 +96,9 @@ public class JMSPluginImplTest {
     @Injectable
     DomainExtService domainExtService;
 
+    @Injectable
+    AuthenticationExtService authenticationExtService;
+
     @Tested
     JMSPluginImpl backendJMS;
 
