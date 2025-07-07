90b907e3074a0b9678fb0fddd9e51e513797d21a
Ion Perpegel <Ion.PERPEGEL@ext.ec.europa.eu>
2025-07-07 22:16:30 +0300

    [EDELIVERY-15245] stuck messages sanitizer: log warning if it overlaps with the SendRetryWorker; improve property description

--- Core/Domibus-MSH/src/main/java/eu/domibus/core/message/UnsentMessageSanitizingWorker.java
+++ Core/Domibus-MSH/src/main/java/eu/domibus/core/message/UnsentMessageSanitizingWorker.java
@@ -25,8 +25,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import static eu.domibus.api.property.DomibusPropertyMetadataManagerSPI.DOMIBUS_MESSAGES_STUCK_IGNORE_RECENT_MINUTES;
-import static eu.domibus.api.property.DomibusPropertyMetadataManagerSPI.DOMIBUS_MESSAGES_STUCK_MAX_COUNT;
+import static eu.domibus.api.property.DomibusPropertyMetadataManagerSPI.*;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static org.apache.commons.collections4.CollectionUtils.isEmpty;
 
@@ -91,10 +90,26 @@ public class UnsentMessageSanitizingWorker extends DomibusQuartzJobBean {
         }
 
         long maxEntityId = dateUtil.getMaxEntityId(MINUTES.toSeconds(retryIgnoreMinutes));
+
+        // check the SendRetryWorker properties and log WARN if any apparent overlap
+        int sendRetryTimeoutDelay = domibusPropertyProvider.getIntegerProperty(DOMIBUS_MSH_RETRY_TIMEOUT_DELAY);
+        long minEntityIdOfSendRetryWorker = dateUtil.getMinEntityId(MINUTES.toSeconds(maxRetryTimeout + sendRetryTimeoutDelay));
+        if (minEntityIdOfSendRetryWorker <= maxEntityId) {
+            // minEntityIdOfSendRetryWorker is the min entity id that the SendRetryWorker would process if started at the same moment;
+            // this does not cover the case when the SendRetryWorker is already running (and runs for an unexpectedly long time),
+            // but it is an indication that the two workers may overlap
+            LOG.warn("The [{}] property value [{}] overlaps with the [{}] property value [{}]. " +
+                    "The SendRetryWorker will process messages with entity ids greater than [{}]. " +
+                    "The UnsentMessageSanitizingWorker will process messages with entity ids lower than [{}]. " +
+                    "This may cause messages to be retried by both the SendRetryWorker and the UnsentMessageSanitizingWorker.",
+                    DOMIBUS_MESSAGES_STUCK_IGNORE_RECENT_MINUTES, ignoreMinutes, DOMIBUS_MSH_RETRY_TIMEOUT_DELAY, sendRetryTimeoutDelay,
+                    minEntityIdOfSendRetryWorker, maxEntityId);
+        }
+
         int maxMessageCount = domibusPropertyProvider.getIntegerProperty(DOMIBUS_MESSAGES_STUCK_MAX_COUNT);
         if (maxMessageCount == 0) {
             // no messages will be retried; an alert will be raised instead
-            Long count = userMessageLogDao.countUnsentMessages(minutesAgo, maxEntityId);
+            long count = userMessageLogDao.countUnsentMessages(minutesAgo, maxEntityId);
             if (count == 0) {
                 LOG.debug("No stuck unsent messages found before [{}]", minutesAgo);
                 return;
