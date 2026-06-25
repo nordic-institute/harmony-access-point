a6aa1de04ac09fe08944fa765550c70311d9fce6
Lucian FURCA <Lucian.FURCA@ext.ec.europa.eu>
2026-06-25 11:31:27 +0300

    [EDELIVERY-16954]-Additional testing for the CVE-2026-49875 workaround - fix DomibusContextLoaderListenerTest tests

--- Core/Domibus-MSH/src/test/java/eu/domibus/core/spring/DomibusContextLoaderListenerTest.java
+++ Core/Domibus-MSH/src/test/java/eu/domibus/core/spring/DomibusContextLoaderListenerTest.java
@@ -27,7 +27,7 @@ import java.util.Set;
  * <p>
  * Methods
  * {@link DomibusContextLoaderListener#shutdownPluginClassLoader}
- * {@link ShutdownUtils#shutdownLogger}
+ * {@link DomibusContextLoaderListener#shutdownLogger}
  * are NOT tested separately because it's hard to partially mocked local method
  * AND super method {@link ContextLoaderListener#contextDestroyed}
  */
@@ -61,9 +61,6 @@ public class DomibusContextLoaderListenerTest {
 
             LoggerFactory.getILoggerFactory();
             result = loggerContext;
-
-            loggerContext.getObject("SHUTDOWN_HOOK");
-            result = null;
         }};
 
         domibusContextLoaderListener.contextDestroyed(servletContextEvent);
@@ -85,8 +82,6 @@ public class DomibusContextLoaderListenerTest {
 
             domibusLogger.info("Stop ch.qos.logback.classic.LoggerContext");
             times = 1;
-
-            loggerContext.getObject("SHUTDOWN_HOOK");
 
             loggerContext.stop();
             times = 1;
@@ -125,8 +120,6 @@ public class DomibusContextLoaderListenerTest {
             LoggerFactory.getILoggerFactory();
             result = loggerContext;
 
-            loggerContext.getObject("SHUTDOWN_HOOK");
-            result = null;
         }};
 
         domibusContextLoaderListener.contextDestroyed(servletContextEvent);
@@ -151,8 +144,6 @@ public class DomibusContextLoaderListenerTest {
 
             domibusLogger.info("Stop ch.qos.logback.classic.LoggerContext");
             times = 1;
-
-            loggerContext.getObject("SHUTDOWN_HOOK");
 
             loggerContext.stop();
             times = 1;
